9c3444a73e0c317df0967497c9b8be9c12ed7577
julian <julian@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2011-10-19 10:19:04 +0000

    Fixed: wflow-core, wflow-consoleweb - Fixed problem in property editor where form options may be shown from a different app.
    git-svn-id: http://dev.joget.org/svn/jw-community/trunk@194 1bff935d-2fe2-40b7-b00f-c2aca69a7612

--- wflow-consoleweb/src/main/java/org/joget/apps/app/controller/PropertyJsonController.java
+++ wflow-consoleweb/src/main/java/org/joget/apps/app/controller/PropertyJsonController.java
@@ -1,9 +1,11 @@
 package org.joget.apps.app.controller;
 
+import java.io.IOException;
 import java.io.Writer;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import org.joget.apps.app.service.AppService;
 import org.joget.commons.util.LogUtil;
 import org.joget.plugin.base.Plugin;
 import org.joget.plugin.base.PluginManager;
@@ -19,6 +21,9 @@ public class PropertyJsonController {
 
     @Autowired
     PluginManager pluginManager;
+    
+    @Autowired
+    AppService appService;
 
     @RequestMapping("/property/json/getElements")
     public void getElements(Writer writer, @RequestParam("classname") String className) throws Exception {
@@ -56,4 +61,20 @@ public class PropertyJsonController {
 
         writer.write(json);
     }
+    
+    @RequestMapping("/property/json/(*:appId)/(~:version)/getPropertyOptions")
+    public void getProperties(Writer writer, @RequestParam(value = "appId", required = true) String appId, @RequestParam(value = "version", required = false) String version, @RequestParam("value") String value, @RequestParam(value = "callback", required = false) String callback) throws IOException {
+        if (appId != null && !appId.trim().isEmpty()) {
+            appService.getAppDefinition(appId, version);
+        }
+
+        String json = "";
+        PropertyEditable element = (PropertyEditable) pluginManager.getPlugin(value);
+        if (element != null) {
+            json = element.getPropertyOptions();
+        }
+
+        writer.write(json);        
+    }
+    
 }
