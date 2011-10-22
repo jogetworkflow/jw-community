134a6b9cd3cfed491998841d0bf959eb084d53d2
owen <owen@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2011-10-22 20:44:52 +0000

    Added: wflow-plugin-archetype - To generate a base plugin maven project
    git-svn-id: http://dev.joget.org/svn/jw-community/trunk@203 1bff935d-2fe2-40b7-b00f-c2aca69a7612

---
+++ wflow-plugin-archetype/create-plugin.bat
@@ -0,0 +1,15 @@
+@echo off
+
+if ""%1"" == """" goto usage
+if ""%2"" == """" goto usage
+
+:create
+mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=3.0-BETA -DgroupId=%1 -DartifactId=%2
+goto end
+
+:usage
+echo   Usage: create-plugin (package) (pluginName)
+
+:end
+
+
