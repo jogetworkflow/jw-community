006d2935faafe6dff973758d3e9b070d515478cd
owen <owen@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2011-12-16 06:05:21 +0000

    Fixed: wflow-core - Form Builder - Input data in invisible section still submitted (#428).
    git-svn-id: http://dev.joget.org/svn/jw-community/trunk@253 1bff935d-2fe2-40b7-b00f-c2aca69a7612

--- wflow-core/src/main/resources/resources/js/section.js
+++ wflow-core/src/main/resources/resources/js/section.js
@@ -17,8 +17,10 @@ VisibilityMonitor.prototype.init = function() {
     var match = this.checkValue(controlEl, controlVal);
     if (!match) {
         targetEl.css("display", "none");
+        this.disableInputField(targetEl);
     } else {
         targetEl.css("display", "block");
+        this.enableInputField(targetEl);
     }
     var thisObject = this;
     controlEl.change(function() {
@@ -27,8 +29,10 @@ VisibilityMonitor.prototype.init = function() {
         var match  = thisObject.checkValue(controlEl, controlVal);
         if (match) {
             targetEl.css("display", "block");
+            thisObject.enableInputField(targetEl);
         } else {
             targetEl.css("display", "none");
+            thisObject.disableInputField(targetEl);
         }
     });
 }
@@ -45,4 +49,10 @@ VisibilityMonitor.prototype.checkValue = function(controlEl, controlValue) {
         match = controlEl.val() == controlValue;
     }
     return match;
+}
+VisibilityMonitor.prototype.disableInputField = function(targetEl) {
+    $(targetEl).find('input, select, textarea').attr("disabled", true); 
+}
+VisibilityMonitor.prototype.enableInputField = function(targetEl) {
+    $(targetEl).find('input, select, textarea').removeAttr("disabled"); 
 }
