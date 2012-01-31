151440c74bba7067e9feb8600ab62e287e168ba8
owen <owen@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2012-01-31 08:53:12 +0000

    Modified: wflow-wfengine - Process Hash Variable on activity name when creating activity instance.
    git-svn-id: http://dev.joget.org/svn/jw-community/trunk@296 1bff935d-2fe2-40b7-b00f-c2aca69a7612

--- wflow-wfengine/src/main/java/org/joget/workflow/shark/WorkflowDODSPersistentManager.java
+++ wflow-wfengine/src/main/java/org/joget/workflow/shark/WorkflowDODSPersistentManager.java
@@ -2,14 +2,32 @@ package org.joget.workflow.shark;
 
 import com.lutris.dods.builder.generator.query.QueryBuilder;
 import java.math.BigDecimal;
+import org.enhydra.shark.api.client.wfmc.wapi.WMSessionHandle;
+import org.enhydra.shark.api.internal.instancepersistence.ActivityPersistenceObject;
 import org.enhydra.shark.api.internal.instancepersistence.PersistenceException;
 import org.enhydra.shark.instancepersistence.DODSPersistentManager;
 import org.enhydra.shark.instancepersistence.data.ProcessDO;
 import org.enhydra.shark.instancepersistence.data.ProcessQuery;
 import org.enhydra.shark.instancepersistence.data.ProcessStateDO;
+import org.joget.workflow.model.WorkflowAssignment;
+import org.joget.workflow.util.WorkflowUtil;
 
 public class WorkflowDODSPersistentManager extends DODSPersistentManager {
 
+    @Override
+    public void persist(WMSessionHandle shandle, ActivityPersistenceObject act, boolean isInitialPersistence) throws PersistenceException {
+        if (isInitialPersistence) {
+            WorkflowAssignment mockAss = new WorkflowAssignment();
+            mockAss.setActivityId(act.getId());
+            mockAss.setActivityDefId(act.getActivityDefinitionId());
+            mockAss.setProcessId(act.getProcessId());
+            mockAss.setProcessDefId(act.getProcessMgrName());
+            
+            act.setName(WorkflowUtil.processVariable(act.getName(), null, mockAss));
+        }
+        super.persist(shandle, act, isInitialPersistence);
+    }
+    
     @Override
     protected ProcessDO[] getPersistedProcesses(int type, String sqlWhere, int startAt, int limit) throws PersistenceException {
         ProcessDO[] DOs = null;
