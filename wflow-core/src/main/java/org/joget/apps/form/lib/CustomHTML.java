c652a71f1392255051022931fc67543a184108ad
owen <owen@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2012-07-30 09:01:25 +0000

    Modified: wflow-core/wflow-consoleweb - Custom HTML - support auto populate saved data. (#728)
    git-svn-id: http://dev.joget.org/svn/jw-community/trunk@508 1bff935d-2fe2-40b7-b00f-c2aca69a7612

--- wflow-core/src/main/java/org/joget/apps/form/lib/CustomHTML.java
+++ wflow-core/src/main/java/org/joget/apps/form/lib/CustomHTML.java
@@ -1,5 +1,7 @@
 package org.joget.apps.form.lib;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,6 +14,7 @@ import org.joget.apps.form.model.FormData;
 import org.joget.apps.form.model.FormRow;
 import org.joget.apps.form.model.FormRowSet;
 import org.joget.apps.form.service.FormUtil;
+import org.joget.commons.util.StringUtil;
 
 public class CustomHTML extends Element implements FormBuilderPaletteElement, FormContainer {
 
@@ -29,10 +32,27 @@ public class CustomHTML extends Element implements FormBuilderPaletteElement, Fo
     public String getDescription() {
         return "Custom HTML Element";
     }
+    
+    @Override
+    public Collection<String> getDynamicFieldNames() {
+        Collection<String> fieldNames = new ArrayList<String>();
+        
+        String customHTML = (String) getProperty("value");
+
+        Pattern pattern = Pattern.compile("name=\\\"([^\\\"]*)\\\"");
+        Matcher matcher = pattern.matcher(customHTML);
+
+        while (matcher.find()) {
+            String name = matcher.group(1);
+            fieldNames.add(name);
+        }
+        return fieldNames;
+    }
 
     @Override
     public FormRowSet formatData(FormData formData) {
         FormRowSet rowSet = null;
+        FormRow result = null;
 
         String customHTML = (String) getProperty("value");
 
@@ -57,12 +77,15 @@ public class CustomHTML extends Element implements FormBuilderPaletteElement, Fo
 
                 // formulate values
                 String delimitedValue = FormUtil.generateElementPropertyValues(values);
+                
+                if (rowSet == null) {
+                    rowSet = new FormRowSet();
+                    result = new FormRow();
+                    rowSet.add(result);
+                }
 
                 // set value into Properties and FormRowSet object
-                FormRow result = new FormRow();
                 result.setProperty(name, delimitedValue);
-                rowSet = new FormRowSet();
-                rowSet.add(result);
             }
         }
         return rowSet;
@@ -74,33 +97,153 @@ public class CustomHTML extends Element implements FormBuilderPaletteElement, Fo
 
         //get the list of names inside tag input and textarea
         String customHTML = (String) getProperty("value");
+        if (getPropertyString("autoPopulate") != null && getPropertyString("autoPopulate").equalsIgnoreCase("true")) {
+            //input field
+            Pattern pattern = Pattern.compile("<input[^>]*>");
+            Matcher matcher = pattern.matcher(customHTML);
+            while (matcher.find()) {
+                String inputString = matcher.group(0);
+                String newInputString = inputString;
+                
+                //get the name
+                Pattern patternName = Pattern.compile("name=\"([^\\\"]*)\"");
+                Matcher matcherName = patternName.matcher(inputString);
+                String name = "";
+                if (matcherName.find()) {
+                    name = matcherName.group(1);
+                }
+                
+                //get the type
+                Pattern patternType = Pattern.compile("type=\"([^\\\"]*)\"");
+                Matcher matcherType = patternType.matcher(inputString);
+                String type = "";
+                if (matcherType.find()) {
+                    type = matcherType.group(1);
+                }
+                
+                if (type.equalsIgnoreCase("checkbox") || type.equalsIgnoreCase("radio")) {
+                    //get the value
+                    Pattern patternValue = Pattern.compile("value=\"([^\\\"]*)\"");
+                    Matcher matcherValue = patternValue.matcher(inputString);
+                    String value = "";
+                    if (matcherValue.find()) {
+                        value = matcherValue.group(1);
+                    }
+                    
+                    Element element = new CheckBox();
+                    element.setProperty("id", name);
+                    element.setParent(this);
+                    String[] values = FormUtil.getElementPropertyValues(element, formData);
+                    
+                    Boolean checked = false;
+                    if (values != null && values.length > 0) {
+                        for (String v : values) {
+                            if (value.equals(v)) {
+                                checked = true;
+                            }
+                        }
+                    }
+                    
+                    newInputString = newInputString.replaceFirst("checked", "");
+                    newInputString = newInputString.replaceFirst("checked=\"true\"", "");
+                    newInputString = newInputString.replaceFirst("checked=\"checked\"", "");
+                    
+                    if (checked) {
+                        newInputString = newInputString.replaceFirst("value=\"", "checked=\"checked\" value=\"");
+                    }
+                    
+                } else if (type.equalsIgnoreCase("file") || type.equalsIgnoreCase("button") || type.equalsIgnoreCase("submit") || type.equalsIgnoreCase("reset") || type.equalsIgnoreCase("image")) {
+                    //ignore
+                } else {
+                    Element element = new TextField();
+                    element.setProperty("id", name);
+                    element.setParent(this);
+                    String value = FormUtil.getElementPropertyValue(element, formData);
+                    newInputString = newInputString.replaceFirst("value=\"[^\\\"]*\"", "value=\"" + StringUtil.escapeRegex(value) + "\"");
+                }
+                
+                customHTML = customHTML.replaceFirst(StringUtil.escapeRegex(inputString), StringUtil.escapeRegex(newInputString));
+            }
+            
+            //textarea
+            Pattern patternTextarea = Pattern.compile("<textarea[^>]*>.*?</textarea>", Pattern.DOTALL);
+            Matcher matcherTextarea = patternTextarea.matcher(customHTML);
+            while (matcherTextarea.find()) {
+                String textareaString = matcherTextarea.group(0);
+                String newTextareaString = textareaString;
+                        
+                //get the name
+                Pattern patternName = Pattern.compile("name=\"([^\\\"]*)\"");
+                Matcher matcherName = patternName.matcher(textareaString);
+                String name = "";
+                if (matcherName.find()) {
+                    name = matcherName.group(1);
+                }
+                
+                Element element = new TextField();
+                element.setProperty("id", name);
+                element.setParent(this);
+                String value = FormUtil.getElementPropertyValue(element, formData);
+                
+                newTextareaString = newTextareaString.replaceFirst(">.*?</", ">" + StringUtil.escapeRegex(value) + "</");
+                
+                customHTML = customHTML.replaceFirst(StringUtil.escapeRegex(textareaString), StringUtil.escapeRegex(newTextareaString));
+            }
+            
+            //Select Box
+            Pattern patternSelect = Pattern.compile("<select[^>]*>.*?</select>", Pattern.DOTALL);
+            Matcher matcherSelect = patternSelect.matcher(customHTML);
+            while (matcherSelect.find()) {
+                String selectString = matcherSelect.group(0);
+                String newSelectString = selectString;
+                
+                //get the name
+                Pattern patternName = Pattern.compile("name=\"([^\\\"]*)\"");
+                Matcher matcherName = patternName.matcher(selectString);
+                String name = "";
+                if (matcherName.find()) {
+                    name = matcherName.group(1);
+                }
 
-        Pattern pattern = Pattern.compile("<input [^>]* name=\\\"([^\\\"]*)\\\"");
-        Matcher matcher = pattern.matcher(customHTML);
-
-        while (matcher.find()) {
-            //get the name and create an dummy element with ID set
-            String name = matcher.group(1);
-            Element element = new TextField();
-            element.setProperty("id", name);
-
-            //get the value
-            String value = FormUtil.getElementPropertyValue(element, formData);
-
-            if (value != null) {
-                //remove value field created by user
-                Pattern pattern2 = Pattern.compile("name=\\\"" + name + "\\\"[.]* value=\\\"([^\\\"]*)\\\"");
-                Matcher matcher2 = pattern2.matcher(customHTML);
-                customHTML = matcher2.replaceFirst("name=\"" + name + "\"");
-
-                pattern2 = Pattern.compile("value=\\\"([^\\\"]*)\\\"[.]* name=\\\"" + name + "\\\"");
-                matcher2 = pattern2.matcher(customHTML);
-                customHTML = matcher2.replaceFirst("name=\"" + name + "\"");
-
-                //inject value into customHTML
-                pattern2 = Pattern.compile("name=\\\"" + name + "\\\"");
-                matcher2 = pattern2.matcher(customHTML);
-                customHTML = matcher2.replaceFirst("name=\"" + name + "\" value=\"" + value + "\"");
+                Element element = new SelectBox();
+                element.setProperty("id", name);
+                element.setParent(this);
+                String[] values = FormUtil.getElementPropertyValues(element, formData);
+                    
+                //get the option
+                Pattern patternOption = Pattern.compile("<option[^>]*>.*?</option>");
+                Matcher matcherOption = patternOption.matcher(selectString);
+                while (matcherOption.find()) {
+                    String optionString = matcherOption.group(0);
+                    String newOptionString = optionString;
+                    
+                    //get the value
+                    Pattern patternValue = Pattern.compile("value=\"([^\\\"]*)\"");
+                    Matcher matcherValue = patternValue.matcher(optionString);
+                    String value = "";
+                    if (matcherValue.find()) {
+                        value = matcherValue.group(1);
+                    }
+                    
+                    Boolean selected = false;
+                    if (values != null && values.length > 0) {
+                        for (String v : values) {
+                            if (value.equals(v)) {
+                                selected = true;
+                            }
+                        }
+                    }
+                    
+                    newOptionString = newOptionString.replaceFirst("selected", "");
+                    newOptionString = newOptionString.replaceFirst("selected=\"selected\"", "");
+                    newOptionString = newOptionString.replaceFirst("selected=\"true\"", "");
+                    
+                    if (selected) {
+                        newOptionString = newOptionString.replaceFirst("value=\"", "selected=\"selected\" value=\"");
+                    }
+                    newSelectString = newSelectString.replaceFirst(StringUtil.escapeRegex(optionString), newOptionString);
+                }
+                customHTML = customHTML.replaceFirst(StringUtil.escapeRegex(selectString), StringUtil.escapeRegex(newSelectString));
             }
         }
 
