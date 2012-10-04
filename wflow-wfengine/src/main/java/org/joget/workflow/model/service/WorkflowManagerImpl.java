22d5053cd180367de30a8371abc722e0a55636a2
owen <owen@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2012-10-04 07:23:39 +0000

    Fixed: wflow-wfengine - Show next assignment doesn't work when in parallel route. (#949) @3.0-SNAPSHOT Fixed: wflow-wfengine - Show next assignment doesn't work when next activity exist in subflow. @3.0-SNAPSHOT
    git-svn-id: http://dev.joget.org/svn/jw-community/branches/3.0-SNAPSHOT@585 1bff935d-2fe2-40b7-b00f-c2aca69a7612

--- wflow-wfengine/src/main/java/org/joget/workflow/model/service/WorkflowManagerImpl.java
+++ wflow-wfengine/src/main/java/org/joget/workflow/model/service/WorkflowManagerImpl.java
@@ -3015,10 +3015,18 @@ public class WorkflowManagerImpl implements WorkflowManager {
 
             sc = connect();
 
-            WfAssignment wfa = getSharkAssignmentByProcess(sc, processId);
-            String activityId = (wfa != null ? wfa.activity().key() : null);
-            ass = (activityId != null ? getAssignment(activityId) : null);
-
+            WfProcess wfProcess = sc.getProcess(processId);
+            
+            Shark shark = Shark.getInstance();
+            AdminMisc admin = shark.getAdminMisc();
+            WMSessionHandle sessionHandle = sc.getSessionHandle();
+            XPDLBrowser xpdl = shark.getXPDLBrowser();
+            
+            WfActivity[] activityList = wfProcess.get_sequence_step(0);
+            WorkflowActivity activity = getNextActivity(sessionHandle, wfProcess.manager(), admin, xpdl, wfProcess.key(), activityList);
+            if (activity != null) {
+                ass = getAssignment(activity.getId());
+            }
         } catch (Exception ex) {
             LogUtil.error(getClass().getName(), ex, "");
         } finally {
@@ -4605,19 +4613,12 @@ public class WorkflowManagerImpl implements WorkflowManager {
                             return getNextActivity(sessionHandle, mgr, admin, xpdl, wfProcess.key(), wfActivityTempList);
                         }
                     } else {
-                        List<String> users = getAssignmentResourceIds(mgr.name(), processId, activityId);
-
-                        if (users != null && users.size() > 0) {
-                            String currentUsername = getWorkflowUserManager().getCurrentUsername();
-                            for (String username : users) {
-                                if (username.equals(currentUsername)) {
-                                    WorkflowActivity activityStarted = new WorkflowActivity();
-                                    activityStarted.setId(activityId);
-                                    return activityStarted;
-                                }
-                            }
+                        WfAssignment ass = getSharkAssignment(connect(), activityId);
+                        if (ass != null) {
+                            WorkflowActivity activityStarted = new WorkflowActivity();
+                            activityStarted.setId(activityId);
+                            return activityStarted;
                         }
-                        return null;
                     }
                 }
             }
