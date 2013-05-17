21f20659e0d54905b5e6e424359fe6ba91ed20f1
owen <owen@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2013-05-17 07:58:46 +0000

    Fixed: wflow-core - Duplicate Value Validator - Not working in subform. (#1416)
    git-svn-id: http://dev.joget.org/svn/jw-community/trunk@806 1bff935d-2fe2-40b7-b00f-c2aca69a7612

--- wflow-core/src/main/java/org/joget/apps/form/service/FormUtil.java
+++ wflow-core/src/main/java/org/joget/apps/form/service/FormUtil.java
@@ -778,9 +778,14 @@ public class FormUtil implements ApplicationContextAware {
     public static boolean isElementPropertyValuesChanges(Element element, FormData formData, String[] updatedValues) {
         // get value
         String id = element.getPropertyString(FormUtil.PROPERTY_ID);
-
-        String primaryKeyValue = formData.getPrimaryKeyValue();
-        if (primaryKeyValue != null && !primaryKeyValue.equals(formData.getRequestParameter(FormUtil.FORM_META_ORIGINAL_ID))) {
+        
+        String primaryKeyValue = element.getPrimaryKeyValue(formData);
+        String uniqueId = "";
+        Form rootForm = findRootForm(element);
+        if (rootForm.getParent() != null) {
+            uniqueId = rootForm.getCustomParameterName();
+        }
+        if (primaryKeyValue != null && !primaryKeyValue.equals(formData.getRequestParameter(uniqueId + FormUtil.FORM_META_ORIGINAL_ID))) {
             return true;
         }
 
