81680178d3ddbf919ae1532b427ea9c1be0cf824
owen <owen@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2016-03-29 08:30:18 +0000

    Modified: wflow-core - Date Picker - Support for keyboard control.  @5.0-SNAPSHOT
    git-svn-id: https://dev.joget.org/svn/jw-community/trunk@2659 1bff935d-2fe2-40b7-b00f-c2aca69a7612

--- wflow-core/src/main/resources/resources/js/jquery.custom.datepicker.js
+++ wflow-core/src/main/resources/resources/js/jquery.custom.datepicker.js
@@ -4,7 +4,65 @@
             this.each(function(){
                 var element = $(this);
                 
+                o.onClose = function(selectedDate) {
+                    $(element).focus();
+                };
+                
                 $(element).datepicker(o);
+                
+                var show = function(element, evt) {
+                    $(element).datepicker( "show" );
+                    
+                    var tabbables = $("#ui-datepicker-div").find(':tabbable');
+                    var first = tabbables.filter(':first');
+                    var last  = tabbables.filter(':last');
+
+                    $("#ui-datepicker-div").off("keydown", ":tabbable");
+                    $("#ui-datepicker-div").on("keydown", ":tabbable", function(e) {
+                        var keyCode = e.keyCode || e.which;
+                        if (keyCode === 9) {
+                            var focusedElement = $(e.target);
+
+                            var isFirstInFocus = (first.get(0) === focusedElement.get(0));
+                            var isLastInFocus = (last.get(0) === focusedElement.get(0));
+
+                            var tabbingForward = !e.shiftKey;
+
+                            if (tabbingForward) {
+                                if (isLastInFocus) {
+                                    first.focus();
+                                    e.preventDefault();
+                                }
+                            } else {
+                                if (isFirstInFocus) {
+                                    last.focus();
+                                    e.preventDefault();
+                                }
+                            }
+                        } else if (keyCode == 27) {
+                            $(element).datepicker( "hide" );
+                            $(element).next("a.trigger").focus();
+                        }
+                    });
+                    
+                    var focused = false;
+                    while ($("#ui-datepicker-div").is(":visible") && !focused) {
+                        first.focus();
+                        focused = true;
+                    }
+                    
+                    evt.preventDefault();
+                };
+                
+                var a = $("<a>").attr("href","#");
+                $(element).next("img.ui-datepicker-trigger").wrap("<a class=\"trigger\" href=\"#\"></a>");
+                $(element).next("a.trigger").off("keydown").click(function(evt){
+                    show(element, evt);
+                }).on("keydown", function(evt){
+                    if (evt.keyCode == 13) {
+                        show(element, evt);
+                    }
+                });  
                 
                 if (o.startDateFieldId  !== undefined && o.startDateFieldId !== "") {
                     var startDate = FormUtil.getField(o.startDateFieldId);
