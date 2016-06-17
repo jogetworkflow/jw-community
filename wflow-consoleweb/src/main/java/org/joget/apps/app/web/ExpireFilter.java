caedccdcdc08767a3af8d294be74be518cc8b58b
owen <owen@1bff935d-2fe2-40b7-b00f-c2aca69a7612>
2016-06-17 06:05:12 +0000

    Modified: wflow-enterpriseweb - Support -Dresources.expires (Seconds) in JAVA_OPTS to set Expires header of web resources. #4169  @5.0-SNAPSHOT
    git-svn-id: https://dev.joget.org/svn/jw-community/trunk@2725 1bff935d-2fe2-40b7-b00f-c2aca69a7612

---
+++ wflow-consoleweb/src/main/java/org/joget/apps/app/web/ExpireFilter.java
@@ -0,0 +1,64 @@
+package org.joget.apps.app.web;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ExpireFilter implements Filter {
+    
+    public static final long DEFAULT_HEADER_CACHE_EXPIRY = 300000L; // 5 minutes
+    public static long expires = DEFAULT_HEADER_CACHE_EXPIRY;
+    
+    private final static List<String> EXTS = Arrays.asList(new String[]{"css", "less", "js", "jpeg", "jpg", "png", "gif", "ico", "otf", "eot", "svg", "ttf", "woff"});
+    
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String sysExpireStr = System.getProperty("resources.expires");
+        if (sysExpireStr != null && !sysExpireStr.trim().isEmpty()) {
+            try {
+                expires = Long.parseLong(sysExpireStr) * 1000;
+            } catch (Exception e) {
+                // ignore
+            }
+        }
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if ((request instanceof HttpServletRequest) && (response instanceof HttpServletResponse)) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            if (isWebResouces(httpRequest.getRequestURI())) {
+                httpResponse.addDateHeader("Expires", System.currentTimeMillis() + expires);
+            }
+        }
+        
+        // Continue
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        
+    }
+    
+    protected boolean isWebResouces (String url) {
+        if (url != null) {
+            if (url.endsWith(".")) {
+                url = url.substring(0, url.length() - 1);
+            }
+            if (url.contains(".")) {
+                String extension = url.substring(url.lastIndexOf(".") + 1);
+                if (EXTS.contains(extension)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
