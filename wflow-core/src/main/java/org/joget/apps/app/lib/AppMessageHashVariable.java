34e0e9e5c749815009cb4343203cd64200ffe285
owen <owen@joget.org>
2016-10-17 12:25:38 +0800

    Modified: wflow-core - AppMessageHashVariable - Retrieve app message from cache instead of make another call to db. @6.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/app/lib/AppMessageHashVariable.java
+++ wflow-core/src/main/java/org/joget/apps/app/lib/AppMessageHashVariable.java
@@ -1,24 +1,15 @@
 package org.joget.apps.app.lib;
 
-import org.joget.apps.app.dao.MessageDao;
-import org.joget.apps.app.model.AppDefinition;
 import org.joget.apps.app.model.DefaultHashVariablePlugin;
-import org.joget.apps.app.model.Message;
 import org.joget.apps.app.service.AppUtil;
-import org.springframework.context.ApplicationContext;
 
 public class AppMessageHashVariable extends DefaultHashVariablePlugin {
 
     @Override
     public String processHashVariable(String variableKey) {
-        AppDefinition appDef = (AppDefinition) getProperty("appDefinition");
-        if (appDef != null) {
-            ApplicationContext appContext = AppUtil.getApplicationContext();
-            MessageDao messageDao = (MessageDao) appContext.getBean("messageDao");
-            Message message = messageDao.loadByMessageKey(variableKey, AppUtil.getAppLocale(), appDef);
-            if (message != null) {
-                return message.getMessage();
-            }
+        String label = AppUtil.replaceAppMessage(variableKey);
+        if (label != null) {
+            return label;
         }
         return null;
     }
