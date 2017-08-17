jogetworkflow/jw-community

commit 1850f6d32b8b97774f5810f93a8b99605f030dc3

Fixed: wflow-consoleweb - Property Editor - AJAX call dependent on element's properties is not trigger immediately when the property is changed. @6.0-SNAPSHOT Modified: wflow-consoleweb - Property Editor - Reduced AJAX call if dependent values are same. @6.0-SNAPSHOT

wflow-consoleweb/src/main/webapp/js/jquery/jquery.propertyeditor.js
@@ -9,6 +9,7 @@ PropertyEditor.Util = {
     ajaxLoadingTimeoutConter: 0,
     resources: {},
     ajaxCalls: {},
+    prevAjaxCalls: {},
     types: {},
     validators: {},
     escapeHtmlTag: function(string){
@@ -287,9 +288,13 @@ PropertyEditor.Util = {
                 var selector = "";
                 var fieldId = fieldIds[i];
                 if (fieldId.indexOf(".") !== -1) {
-                    selector = "#" + field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id + " [name=\"" + fieldId.substring(fieldId.indexOf(".") + 1) + "\"]";
-                    if ($(field.editor).find(selector).length === 0) {
-                        selector = "[name=\""+field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id+"\"]";
+                    if (fieldId.indexOf(".properties") !== -1) {
+                        selector = ".property-editor-page[elementid=\""+field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id+"\"] .property-editor-property:not(.hidden) [name]";
+                    } else {
+                        selector = "#" + field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id + " [name=\"" + fieldId.substring(fieldId.indexOf(".") + 1) + "\"]";
+                        if ($(field.editor).find(selector).length === 0) {
+                            selector = "[name=\""+field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id+"\"]";
+                        }
                     }
                 } else {
                     selector = "[name=\""+field.editorObject.fields[fieldId].id+"\"]";
@@ -374,6 +379,11 @@ PropertyEditor.Util = {
                 ajaxUrl += param + "=" + escape(targetValue);
             }
         }
+        var prevAjaxUrl = PropertyEditor.Util.prevAjaxCalls[field.id];
+        if (prevAjaxUrl !== null && prevAjaxUrl !== undefined && prevAjaxUrl === ajaxUrl) {
+            return;
+        }
+        
         if (PropertyEditor.Util.ajaxCalls[ajaxUrl] === undefined || PropertyEditor.Util.ajaxCalls[ajaxUrl] === null) {
             PropertyEditor.Util.ajaxCalls[ajaxUrl] = [];
         }
@@ -383,6 +393,7 @@ PropertyEditor.Util = {
             mapping : mapping,
             reference : reference
         });
+        PropertyEditor.Util.prevAjaxCalls[field.id] = ajaxUrl;
         
         if (PropertyEditor.Util.ajaxCalls[ajaxUrl].length === 1) {
             if (method === undefined || method.toUpperCase() !== "POST") {
@@ -459,9 +470,13 @@ PropertyEditor.Util = {
             var selector = "";
             var fieldId = fieldIds[i];
             if (fieldId.indexOf(".") !== -1) {
-                selector = "#" + field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id + " [name=\"" + fieldId.substring(fieldId.indexOf(".") + 1) + "\"]";
-                if ($(field.editor).find(selector).length === 0) {
-                    selector = "[name=\""+field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id+"\"]";
+                if (fieldId.indexOf(".properties") !== -1) {
+                    selector = ".property-editor-page[elementid=\""+field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id+"\"] .property-editor-property:not(.hidden) [name]";
+                } else {
+                    selector = "#" + field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id + " [name=\"" + fieldId.substring(fieldId.indexOf(".") + 1) + "\"]";
+                    if ($(field.editor).find(selector).length === 0) {
+                        selector = "[name=\""+field.editorObject.fields[fieldId.substring(0, fieldId.indexOf("."))].id+"\"]";
+                    }
                 }
             } else {
                 selector = "[name=\""+field.editorObject.fields[fieldId].id+"\"]";
@@ -966,12 +981,6 @@ PropertyEditor.Model.Editor.prototype = {
         }
     },
     changePageCallback: function (pageId, scroll) {
-        //trigger change if the current page is property page of an element
-        var elementId = $(this.editor).find('.property-page-show.current').attr("elementid");
-        if (elementId !== undefined && elementId !== null) {
-            $(this.editor).find("#"+elementId).trigger("change");
-        }
-        
         $(this.editor).find('.property-page-hide, .property-type-hidden, .property-page-show').hide();
         $(this.editor).find('.property-page-show').removeClass("current");
         this.pages[pageId].show(scroll);

commit e4c87d0e84a2cc055ed6d3ab5db3fd84cdbf0465

Fixed: wflow-consoleweb - Regression from 1850f6d. Property Editor - Ajax options can't load correctly for multiple grid column selectbox.  @6.0-SNAPSHOT

wflow-consoleweb/src/main/webapp/js/jquery/jquery.propertyeditor.js
@@ -379,7 +379,7 @@ PropertyEditor.Util = {
                 ajaxUrl += param + "=" + escape(targetValue);
             }
         }
-        var prevAjaxUrl = PropertyEditor.Util.prevAjaxCalls[field.id];
+        var prevAjaxUrl = PropertyEditor.Util.prevAjaxCalls[field.id + "::" + reference];
         if (prevAjaxUrl !== null && prevAjaxUrl !== undefined && prevAjaxUrl === ajaxUrl) {
             return;
         }
@@ -393,7 +393,7 @@ PropertyEditor.Util = {
             mapping : mapping,
             reference : reference
         });
-        PropertyEditor.Util.prevAjaxCalls[field.id] = ajaxUrl;
+        PropertyEditor.Util.prevAjaxCalls[field.id + "::" + reference] = ajaxUrl;
         
         if (PropertyEditor.Util.ajaxCalls[ajaxUrl].length === 1) {
             if (method === undefined || method.toUpperCase() !== "POST") {
@@ -443,7 +443,7 @@ PropertyEditor.Util = {
                                     tempOptions = extra;
                                 }
                             }
-
+                            
                             calls[i].field.handleAjaxOptions(tempOptions, calls[i].reference);
                             calls[i].field.isDataReady = true;
                         }