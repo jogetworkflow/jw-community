jogetworkflow/jw-community

commit dc9d1c35e4364beb28d93b95d64bc4ad2535ec32

Fixed: wflow-core - User Notification - Assignment link is wrong when send to multiple users. @6.0-SNAPSHOT

wflow-core/src/main/java/org/joget/apps/app/lib/UserNotificationAuditTrail.java
@@ -147,7 +147,9 @@ public class UserNotificationAuditTrail extends DefaultAuditTrailPlugin implemen
                                 
                                 int count = 0;
                                 do {
-                                    wfAssignment = workflowManager.getAssignment(activityInstanceId);
+                                    try {
+                                        wfAssignment = workflowManager.getAssignment(activityInstanceId);
+                                    } catch (Exception ex) {}
                                     
                                     if (wfAssignment == null) {
                                         Thread.sleep(4000); //wait for assignment creation
@@ -190,17 +192,18 @@ public class UserNotificationAuditTrail extends DefaultAuditTrailPlugin implemen
                                     if (emailMessage != null && emailMessage.length() != 0) {
 
                                         String msg;
+                                        String tempLink = "";
                                         if ("true".equalsIgnoreCase(isHtml)) {
                                             if (urlName != null && urlName.length() != 0) {
-                                                link = "<a href=\"" + link + "\">" + urlName + "</a>";
+                                                tempLink = "<a href=\"" + link + "\">" + urlName + "</a>";
                                             } else {
-                                                link = "<a href=\"" + link + "\">" + link + "</a>";
+                                                tempLink = "<a href=\"" + link + "\">" + link + "</a>";
                                             }
-                                            msg = AppUtil.processHashVariable(emailMessage + "<br/><br/><br/>" + link, wfAssignment, null, replace);
+                                            msg = AppUtil.processHashVariable(emailMessage + "<br/><br/><br/>" + tempLink, wfAssignment, null, replace);
                                             msg = msg.replaceAll("\\n", "<br/>");
                                             email.setHtmlMsg(msg);
                                         } else {
-                                            msg = AppUtil.processHashVariable(emailMessage + "\n\n\n" + link, wfAssignment, null, replace);
+                                            msg = AppUtil.processHashVariable(emailMessage + "\n\n\n" + tempLink, wfAssignment, null, replace);
                                             email.setMsg(msg);
                                         }
                                     }

commit 3eff30c14d4597fcde59f8c97cdf800cad475498

Fixed: wflow-core - User Notification - Regression from dc9d1c3. Assignment link is missing when not send as HTML. @6.0-SNAPSHOT

wflow-core/src/main/java/org/joget/apps/app/lib/UserNotificationAuditTrail.java
@@ -196,7 +196,7 @@ public class UserNotificationAuditTrail extends DefaultAuditTrailPlugin implemen
                                     if (emailMessage != null && emailMessage.length() != 0) {
 
                                         String msg;
-                                        String tempLink = "";
+                                        String tempLink = link;
                                         if ("true".equalsIgnoreCase(isHtml)) {
                                             if (urlName != null && urlName.length() != 0) {
                                                 tempLink = "<a href=\"" + link + "\">" + urlName + "</a>";