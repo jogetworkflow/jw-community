jogetworkflow/jw-community

commit 86c680f4b9ed2e826f7e8b104d519e9a57385307

Modified: wflow-consoleweb/wflow-core - Datalist Column Formatter - Add implementation to customize the export result before/after a row rendering. @6.0-SNAPSHOT

wflow-core/src/main/java/org/joget/apps/datalist/model/DataList.java
@@ -3,9 +3,12 @@ package org.joget.apps.datalist.model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.displaytag.tags.TableTagParameters;
 import org.displaytag.util.ParamEncoder;
 import org.joget.apps.app.service.AppUtil;
@@ -15,6 +18,7 @@ import org.joget.commons.util.ResourceBundleUtil;
 import org.joget.commons.util.StringUtil;
 import org.joget.plugin.base.PluginManager;
 import org.joget.workflow.util.WorkflowUtil;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 
 public class DataList {
 
@@ -72,6 +76,7 @@ public class DataList {
     private boolean filterQueryBuild = false;
     private boolean disableQuickEdit = false;
     private boolean showDataWhenFilterSet = false;
+    private Map<String, String[]> requestParamMap = null;
 
     //Required when using session
     public void init() {
@@ -611,11 +616,23 @@ public class DataList {
     }
 
     public String[] getDataListParam(String paramName) {
-        HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
+        if (requestParamMap == null) {
+            requestParamMap = new HashMap<String, String[]>();
+            HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
+            Enumeration<String> e = request.getParameterNames();
+            while (e.hasMoreElements()) {
+                String pn = e.nextElement();
+                paramName = StringEscapeUtils.escapeHtml(pn);
+                String[] values = request.getParameterValues(pn);
+                requestParamMap.put(pn, values);
+            }
+        }
+        
         String param = getDataListEncodedParamName(paramName);
-        String[] values = request.getParameterValues(param);
+        String[] values = requestParamMap.get(param);
         if (isUseSession() && !(TableTagParameters.PARAMETER_EXPORTTYPE.equals(paramName) || PARAMETER_ACTION.equals(paramName) || paramName.startsWith(CHECKBOX_PREFIX))) {
             String sessionKey = "session_" + getId() + "_" + getSessionKeyPrefix() + "_" + param;
+            HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
             HttpSession session = request.getSession(true);
 
             if (values != null) {
@@ -791,5 +808,29 @@ public class DataList {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Retrieve current request map
+     * @return 
+     */
+    public Map<String, String[]> getRequestParamMap() {
+        return requestParamMap;
+    }
+
+    /**
+     * Set current request map
+     * @return 
+     */
+    public void setRequestParamMap(Map<String, String[]> requestParamMap) {
+        this.requestParamMap = requestParamMap;
+    }
+
+    /**
+     * Method to clear filter
+     */
+    public void clearFilter() {
+        filterQueryBuild = false;
+        dataListFilterQueryObjectList = new ArrayList<DataListFilterQueryObject>();
     }
 }

wflow-consoleweb/pom.xml
@@ -79,27 +79,6 @@
             <version>1.5</version>
         </dependency>
         <dependency>
-            <groupId>displaytag</groupId>
-            <artifactId>displaytag-export-poi</artifactId>
-            <version>1.2</version>
-            <exclusions>
-                <exclusion>
-                    <groupId>org.apache.poi</groupId>
-                    <artifactId>poi</artifactId>
-                </exclusion>
-            </exclusions>
-        </dependency>
-        <dependency>
-            <groupId>org.apache.poi</groupId>
-            <artifactId>poi</artifactId>
-            <version>3.15</version>
-        </dependency>
-        <dependency>
-            <groupId>org.apache.poi</groupId>
-            <artifactId>poi-ooxml</artifactId>
-            <version>3.15</version>
-        </dependency>
-        <dependency>
             <groupId>org.w3c.jigsaw</groupId>
             <artifactId>jigsaw</artifactId>
             <version>2.2.6</version>

wflow-consoleweb/src/main/java/org/joget/apps/displaytag/export/CustomExcelHssfView.java
@@ -1,35 +1,34 @@
 package org.joget.apps.displaytag.export;
 
 import java.io.OutputStream;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.PageContext;
 
-import org.apache.commons.lang.ObjectUtils;
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.poi.hssf.util.HSSFColor;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.Font;
-import org.apache.poi.xssf.streaming.SXSSFRow;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFRichTextString;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.displaytag.Messages;
 import org.displaytag.exception.BaseNestableJspTagException;
 import org.displaytag.exception.SeverityEnum;
 import org.displaytag.export.BinaryExportView;
-import org.displaytag.export.excel.ExcelHssfView;
 import org.displaytag.model.Column;
 import org.displaytag.model.ColumnIterator;
 import org.displaytag.model.HeaderCell;
 import org.displaytag.model.Row;
 import org.displaytag.model.RowIterator;
 import org.displaytag.model.TableModel;
+import org.displaytag.model.TableModelWrapper;
+import org.joget.apps.datalist.model.DataList;
+import org.joget.apps.datalist.model.DataListColumn;
+import org.joget.apps.datalist.model.DataListColumnFormat;
+import org.joget.apps.datalist.model.DataListExcelExportFormatter;
+import org.joget.apps.datalist.model.DataListExcelWriter;
 import org.joget.commons.util.LogUtil;
 
 public class CustomExcelHssfView implements BinaryExportView {
@@ -50,6 +49,26 @@ public class CustomExcelHssfView implements BinaryExportView {
      * decorate export?
      */
     private boolean decorated;
+    /**
+     * This is a utility class to create table
+     */
+    private DataListExcelWriter writer;
+    /**
+     * The datalist object of the current table
+     */
+    private DataList datalist;
+    /**
+     * A flag that indicate there is formatter to execute before row
+     */
+    private boolean isBeforeRow = false;
+    /**
+     * A flag that indicate there is formatter to execute after row
+     */
+    private boolean isAfterRow = false;
+    /**
+     * A map hold the column number and its formatter
+     */
+    private Map<Integer, DataListExcelExportFormatter> formatter = new HashMap<Integer, DataListExcelExportFormatter>();
 
     /**
      * @see org.displaytag.export.ExportView#setParameters(TableModel, boolean, boolean, boolean)
@@ -60,6 +79,34 @@ public class CustomExcelHssfView implements BinaryExportView {
         this.exportFull = exportFullList;
         this.header = includeHeader;
         this.decorated = decorateValues;
+        
+        PageContext pageContext = (new TableModelWrapper(tableModel)).getPageContext();
+        if (pageContext != null) {
+            datalist = (DataList) pageContext.findAttribute("dataList");
+            
+            if (datalist != null) {
+                DataListColumn[] columns = datalist.getColumns();
+                Collection<DataListColumnFormat> formats;
+                DataListExcelExportFormatter ef;
+                for (int i = 0; i < columns.length; i++) {
+                    formats = columns[i].getFormats();
+                    if (formats != null && !formats.isEmpty()) {
+                        for (DataListColumnFormat f : formats) {
+                            if (f instanceof DataListExcelExportFormatter) {
+                                ef = (DataListExcelExportFormatter) f;
+                                if (ef.isExcelBeforeRow()) {
+                                    isBeforeRow = true;
+                                }
+                                if (ef.isExcelAfterRow()) {
+                                    isAfterRow = true;
+                                }
+                                formatter.put(i, ef);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -77,33 +124,16 @@ public class CustomExcelHssfView implements BinaryExportView {
             wb_template = new XSSFWorkbook();
             wb = new SXSSFWorkbook(wb_template, 100, true); 
             SXSSFSheet sheet = (SXSSFSheet) wb.createSheet("-");
-            sheet.setRandomAccessWindowSize(100);
-            sheet.trackAllColumnsForAutoSizing();
-
-            int rowNum = 0;
-            int colNum = 0;
-
-            SXSSFRow xlsRow = null;
-            CellStyle style = null;
-            Font font = null;
+            writer = new DataListExcelWriter(wb, sheet);
+            
             Iterator iterator = null;
             HeaderCell headerCell = null;
             String columnHeader = null;
-            Cell cell = null;
             if (this.header) {
                 // Create an header row
-                xlsRow = (SXSSFRow) sheet.createRow(rowNum++);
-
-                style = wb.createCellStyle();
-                style.setFillPattern(CellStyle.FINE_DOTS);
-                style.setFillBackgroundColor(HSSFColor.BLUE_GREY.index);
-                font = wb.createFont();
-                font.setBoldweight(Font.BOLDWEIGHT_BOLD);
-                font.setColor(HSSFColor.WHITE.index);
-                style.setFont(font);
-
+                writer.createNewRow();
+                
                 iterator = this.model.getHeaderCellList().iterator();
-
                 while (iterator.hasNext()) {
                     headerCell = (HeaderCell) iterator.next();
 
@@ -112,48 +142,64 @@ public class CustomExcelHssfView implements BinaryExportView {
                     if (columnHeader == null) {
                         columnHeader = StringUtils.capitalize(headerCell.getBeanPropertyName());
                     }
-
-                    cell = xlsRow.createCell(colNum++);
-                    cell.setCellValue(new XSSFRichTextString(columnHeader));
-                    cell.setCellStyle(style);
+                    writer.addHeaderCell(columnHeader, null);
                 }
             }
 
             // get the correct iterator (full or partial list according to the exportFull field)
             RowIterator rowIterator = this.model.getRowIterator(this.exportFull);
-            // iterator on rows
-            
             Row row = null;
             ColumnIterator columnIterator = null;
             Column column = null;
             Object value = null;
-            int colCount = 0;
+            int col;
+            DataListExcelExportFormatter ef;
             while (rowIterator.hasNext()) {
                 row = rowIterator.next();
-                xlsRow = (SXSSFRow) sheet.createRow(rowNum++);
-                colNum = 0;
-
+                
+                if (isBeforeRow) {
+                    // iterator on columns
+                    columnIterator = row.getColumnIterator(this.model.getHeaderCellList());
+                    col = 0;
+                    while (columnIterator.hasNext()) {
+                        column = columnIterator.nextColumn();
+                        ef = formatter.get(col);
+                        if (ef != null && ef.isExcelBeforeRow()) {
+                            value = column.getValue(this.decorated);
+                            ef.excelBeforeRow(datalist, row.getObject(), value, writer);
+                        }
+                        col++;
+                    }
+                }
+                
+                writer.createNewRow();
                 // iterator on columns
                 columnIterator = row.getColumnIterator(this.model.getHeaderCellList());
-
                 while (columnIterator.hasNext()) {
                     column = columnIterator.nextColumn();
 
                     // Get the value to be displayed for the column
                     value = column.getValue(this.decorated);
-
-                    cell = xlsRow.createCell(colNum++);
-
-                    writeCell(value, cell);
+                    writer.addCell(value);
+                }
+                
+                if (isAfterRow) {
+                    // iterator on columns
+                    columnIterator = row.getColumnIterator(this.model.getHeaderCellList());
+                    col = 0;
+                    while (columnIterator.hasNext()) {
+                        column = columnIterator.nextColumn();
+                        ef = formatter.get(col);
+                        if (ef != null && ef.isExcelAfterRow()) {
+                            value = column.getValue(this.decorated);
+                            ef.excelAfterRow(datalist, row.getObject(), value, writer);
+                        }
+                        col++;
+                    }
                 }
             }
 
-            // adjust the column widths
-            colCount = 0;
-            while (colCount <= colNum) {
-                sheet.autoSizeColumn((short) colCount++);
-            }
-
+            writer.adjustColumnWidth();
             wb.write(out);
         } catch (Exception e) {
             LogUtil.error(CustomExcelHssfView.class.getName(), e, "");
@@ -171,46 +217,7 @@ public class CustomExcelHssfView implements BinaryExportView {
             }
         }
     }
-
-    /**
-     * Write the value to the cell. Override this method if you have complex data types that may need to be exported.
-     * @param value the value of the cell
-     * @param cell the cell to write it to
-     */
-    protected void writeCell(Object value, Cell cell) {
-        if (value instanceof Number) {
-            cell.setCellValue(((Number) value).doubleValue());
-        } else if (value instanceof Date) {
-            cell.setCellValue((Date) value);
-        } else if (value instanceof Calendar) {
-            cell.setCellValue((Calendar) value);
-        } else {
-            cell.setCellValue(new XSSFRichTextString(escapeColumnValue(value)));
-        }
-    }
-
-    // patch from Karsten Voges
-    /**
-     * Escape certain values that are not permitted in excel cells.
-     * @param rawValue the object value
-     * @return the escaped value
-     */
-    protected String escapeColumnValue(Object rawValue) {
-        if (rawValue == null) {
-            return null;
-        }
-        String returnString = ObjectUtils.toString(rawValue);
-        // escape the String to get the tabs, returns, newline explicit as \t \r \n
-        returnString = StringEscapeUtils.escapeJava(StringUtils.trimToEmpty(returnString));
-        // remove tabs, insert four whitespaces instead
-        returnString = StringUtils.replace(StringUtils.trim(returnString), "\\t", "    ");
-        // remove the return, only newline valid in excel
-        returnString = StringUtils.replace(StringUtils.trim(returnString), "\\r", " ");
-        // unescape so that \n gets back to newline
-        returnString = StringEscapeUtils.unescapeJava(returnString);
-        return returnString;
-    }
-
+    
     /**
      * Wraps IText-generated exceptions.
      * @author Fabrizio Giustina
@@ -228,7 +235,7 @@ public class CustomExcelHssfView implements BinaryExportView {
          * @param cause Previous exception
          */
         public ExcelGenerationException(Throwable cause) {
-            super(ExcelHssfView.class, Messages.getString("ExcelView.errorexporting"), cause); //$NON-NLS-1$
+            super(CustomExcelHssfView.class, Messages.getString("ExcelView.errorexporting"), cause); //$NON-NLS-1$
         }
 
         /**

wflow-consoleweb/src/main/java/org/joget/apps/displaytag/export/CustomPdfViewer.java
@@ -27,17 +27,22 @@ import com.lowagie.text.Cell;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
-import com.lowagie.text.Font;
-import com.lowagie.text.FontFactory;
 import com.lowagie.text.HeaderFooter;
 import com.lowagie.text.PageSize;
 import com.lowagie.text.Rectangle;
-import com.lowagie.text.Table;
-import com.lowagie.text.pdf.BaseFont;
-import com.lowagie.text.pdf.FontSelector;
 import com.lowagie.text.pdf.PdfWriter;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.jsp.PageContext;
+import org.displaytag.model.TableModelWrapper;
 import org.displaytag.util.TagConstants;
+import org.joget.apps.datalist.model.DataList;
+import org.joget.apps.datalist.model.DataListColumn;
+import org.joget.apps.datalist.model.DataListColumnFormat;
+import org.joget.apps.datalist.model.DataListPdfExportFormatter;
+import org.joget.apps.datalist.model.DataListPdfWriter;
 
 public class CustomPdfViewer implements BinaryExportView {
     /* split up the large pdf report into smaller parts */
@@ -60,15 +65,26 @@ public class CustomPdfViewer implements BinaryExportView {
      */
     private boolean decorated;
     /**
-     * This is the table, added as an Element to the PDF document. It contains all the data, needed to represent the
-     * visible table into the PDF
+     * This is a utility class to create table
      */
-    private Table tablePDF;
+    private DataListPdfWriter writer;
     /**
-     * The default font used in the document.
+     * The datalist object of the current table
      */
-    private FontSelector selector;
-    
+    private DataList datalist;
+    /**
+     * A flag that indicate there is formatter to execute before row
+     */
+    private boolean isBeforeRow = false;
+    /**
+     * A flag that indicate there is formatter to execute after row
+     */
+    private boolean isAfterRow = false;
+    /**
+     * A map hold the column number and its formatter
+     */
+    private Map<Integer, DataListPdfExportFormatter> formatter = new HashMap<Integer, DataListPdfExportFormatter>();
+
     private ResourceBundle bundle = null;
 
     /**
@@ -80,6 +96,34 @@ public class CustomPdfViewer implements BinaryExportView {
         this.exportFull = exportFullList;
         this.header = includeHeader;
         this.decorated = decorateValues;
+        
+        PageContext pageContext = (new TableModelWrapper(tableModel)).getPageContext();
+        if (pageContext != null) {
+            datalist = (DataList) pageContext.findAttribute("dataList");
+            
+            if (datalist != null) {
+                DataListColumn[] columns = datalist.getColumns();
+                Collection<DataListColumnFormat> formats;
+                DataListPdfExportFormatter ef;
+                for (int i = 0; i < columns.length; i++) {
+                    formats = columns[i].getFormats();
+                    if (formats != null && !formats.isEmpty()) {
+                        for (DataListColumnFormat f : formats) {
+                            if (f instanceof DataListPdfExportFormatter) {
+                                ef = (DataListPdfExportFormatter) f;
+                                if (ef.isPdfBeforeRow()) {
+                                    isBeforeRow = true;
+                                }
+                                if (ef.isPdfAfterRow()) {
+                                    isAfterRow = true;
+                                }
+                                formatter.put(i, ef);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /**
@@ -87,27 +131,12 @@ public class CustomPdfViewer implements BinaryExportView {
      * @throws BadElementException for errors during table initialization
      */
     protected void initTable() throws BadElementException, DocumentException, IOException{
-        tablePDF = new Table(this.model.getNumberOfColumns());
-        tablePDF.getDefaultCell().setVerticalAlignment(Element.ALIGN_TOP);
-        tablePDF.setCellsFitPage(true);
-        tablePDF.setWidth(100);
-
-        tablePDF.setPadding(2);
-        tablePDF.setSpacing(0);
-
+        writer = new DataListPdfWriter(this.model.getNumberOfColumns());
+        
         if (bundle == null) {
             //get displaytag ressource bundle
             bundle = ResourceBundle.getBundle(org.displaytag.properties.TableProperties.LOCAL_PROPERTIES, Locale.getDefault());
         }
-        
-        selector = new FontSelector();
-        selector.addFont(FontFactory.getFont(FontFactory.HELVETICA, 7, Font.NORMAL, new Color(0, 0, 0)));
-        selector.addFont(new Font(BaseFont.createFont("fonts/Droid-Sans/DroidSans.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED), 7, Font.NORMAL, new Color(0, 0, 0)));
-        selector.addFont(new Font(BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.NOT_EMBEDDED), 7, Font.NORMAL, new Color(0, 0, 0)));
-        selector.addFont(new Font(BaseFont.createFont("MSung-Light", "UniCNS-UCS2-H", BaseFont.NOT_EMBEDDED), 7, Font.NORMAL, new Color(0, 0, 0)));
-        selector.addFont(new Font(BaseFont.createFont("HeiseiMin-W3", "UniJIS-UCS2-H", BaseFont.NOT_EMBEDDED), 7, Font.NORMAL, new Color(0, 0, 0)));
-        selector.addFont(new Font(BaseFont.createFont("HYGoThic-Medium", "UniKS-UCS2-H", BaseFont.NOT_EMBEDDED), 7, Font.NORMAL, new Color(0, 0, 0)));
-        selector.addFont(new Font(BaseFont.createFont("fonts/THSarabun/THSarabun.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED), 10, Font.NORMAL, new Color(0, 0, 0)));
     }
 
     /**
@@ -127,7 +156,7 @@ public class CustomPdfViewer implements BinaryExportView {
         if (this.header) {
             generateHeaders();
         }
-        tablePDF.endHeaders();
+        writer.endHeaders();
         generateRows(document);
     }
 
@@ -144,7 +173,7 @@ public class CustomPdfViewer implements BinaryExportView {
             Document document = new Document(PageSize.A4.rotate(), 60, 60, 40, 40);
             document.addCreationDate();
 
-            HeaderFooter footer = new HeaderFooter(selector.process(TagConstants.EMPTY_STRING), true);
+            HeaderFooter footer = new HeaderFooter(writer.getSelector().process(TagConstants.EMPTY_STRING), true);
             footer.setBorder(Rectangle.NO_BORDER);
             footer.setAlignment(Element.ALIGN_CENTER);
 
@@ -173,12 +202,7 @@ public class CustomPdfViewer implements BinaryExportView {
             if (columnHeader == null) {
                 columnHeader = StringUtils.capitalize(headerCell.getBeanPropertyName());
             }
-
-            Cell hdrCell = getCell(columnHeader);
-            hdrCell.setBackgroundColor(new Color(210, 221, 231));
-            hdrCell.setHeader(true);
-            tablePDF.addCell(hdrCell);
-
+            writer.addHeaderCell(columnHeader, new Color(210, 221, 231));
         }
     }
 
@@ -194,52 +218,70 @@ public class CustomPdfViewer implements BinaryExportView {
         Column column = null;
         Object value = null;
         Cell cell = null;
+        Row row = null;
+        DataListPdfExportFormatter ef;
+        int col;
+        ColumnIterator columnIterator;
         while (rowIterator.hasNext()) {
-            Row row = rowIterator.next();
+            row = rowIterator.next();
+            
+            if (isBeforeRow) {
+                // iterator on columns
+                columnIterator = row.getColumnIterator(this.model.getHeaderCellList());
+                col = 0;
+                while (columnIterator.hasNext()) {
+                    column = columnIterator.nextColumn();
+                    ef = formatter.get(col);
+                    if (ef != null && ef.isPdfBeforeRow()) {
+                        value = column.getValue(this.decorated);
+                        ef.pdfBeforeRow(datalist, row.getObject(), value, writer);
+                    }
+                    col++;
+                }
+            }
 
             // iterator on columns
-            ColumnIterator columnIterator = row.getColumnIterator(this.model.getHeaderCellList());
-
+            columnIterator = row.getColumnIterator(this.model.getHeaderCellList());
             while (columnIterator.hasNext()) {
                 column = columnIterator.nextColumn();
 
                 // Get the value to be displayed for the column
                 value = column.getValue(this.decorated);
-                cell = getCell(ObjectUtils.toString(value));
-
+                
                 /* some eyecandy stuff */
                 if (rowCnt % 2 == 1) {
-                    cell.setBackgroundColor(new Color(220, 223, 225));
+                    writer.addCell(ObjectUtils.toString(value), new Color(220, 223, 225));
+                } else {
+                    writer.addCell(ObjectUtils.toString(value), null);
                 }
-                tablePDF.addCell(cell);
+            }
+            
+            if (isAfterRow) {
+                // iterator on columns
+                columnIterator = row.getColumnIterator(this.model.getHeaderCellList());
+                col = 0;
+                while (columnIterator.hasNext()) {
+                    column = columnIterator.nextColumn();
+                    ef = formatter.get(col);
+                    if (ef != null && ef.isPdfAfterRow()) {
+                        value = column.getValue(this.decorated);
+                        ef.pdfAfterRow(datalist, row.getObject(), value, writer);
+                    }
+                    col++;
+                }
             }
 
             /* split up the pdf document to prevent OutOfMemoryExceptions */
             if (rowCnt % FRAGMENT_SIZE == FRAGMENT_SIZE - 1) {
-                document.add(this.tablePDF);
+                document.add(writer.getTable());
                 initTable();
-                generateHeaders();
-                this.tablePDF.endHeaders();
             }
 
             rowCnt++;
         }
 
         /* add the rest of the table */
-        document.add(this.tablePDF);
-    }
-
-    /**
-     * Returns a formatted cell for the given value.
-     * @param value cell value
-     * @return Cell
-     * @throws BadElementException errors while generating content
-     */
-    private Cell getCell(String value) throws BadElementException {
-        Cell cell = new Cell(selector.process(StringUtils.trimToEmpty(value)));
-        cell.setVerticalAlignment(Element.ALIGN_TOP);
-        cell.setLeading(8);
-        return cell;
+        document.add(writer.getTable());
     }
 
     /**

commit b87a01e234018128b380d6a52a9f903fb9a0ba46

Fixed: wflow-core - Datalist - Regression from 86c680f. Parameters in redirect url is passed incorrectly. @6.0-SNAPSHOT

wflow-core/src/main/java/org/joget/apps/datalist/model/DataList.java
@@ -619,23 +619,16 @@ public class DataList {
     }
 
     public String[] getDataListParam(String paramName) {
-        if (requestParamMap == null) {
-            requestParamMap = new HashMap<String, String[]>();
-            HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
-            Enumeration<String> e = request.getParameterNames();
-            while (e.hasMoreElements()) {
-                String pn = e.nextElement();
-                paramName = StringEscapeUtils.escapeHtml(pn);
-                String[] values = request.getParameterValues(pn);
-                requestParamMap.put(pn, values);
-            }
-        }
-        
+        HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
         String param = getDataListEncodedParamName(paramName);
-        String[] values = requestParamMap.get(param);
+        String[] values = null; 
+        if (requestParamMap != null) {
+            values = requestParamMap.get(param);
+        } else {
+            values = request.getParameterValues(param);
+        }
         if (isUseSession() && !(TableTagParameters.PARAMETER_EXPORTTYPE.equals(paramName) || PARAMETER_ACTION.equals(paramName) || paramName.startsWith(CHECKBOX_PREFIX))) {
             String sessionKey = "session_" + getId() + "_" + getSessionKeyPrefix() + "_" + param;
-            HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
             HttpSession session = request.getSession(true);
 
             if (values != null) {