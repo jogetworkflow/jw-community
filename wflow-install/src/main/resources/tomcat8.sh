fffdaca3af4a184d0d4bf5dce4f08119e5ffbeb9
owen <owen@joget.org>
2017-12-18 10:49:43 +0800

    Modified: wflow-install - Startup Script - Added commented JAVA_OPTS (--add-modules java.se.ee) for java 9. @6.0-SNAPSHOT

--- wflow-install/src/main/resources/tomcat8.sh
+++ wflow-install/src/main/resources/tomcat8.sh
@@ -2,6 +2,9 @@
 
 export JAVA_OPTS="-XX:MaxPermSize=128m -Xmx512M -Dwflow.home=./wflow/ "
 
+#For Java 9
+#export JAVA_OPTS="-XX:MaxPermSize=128m -Xmx512M -Dwflow.home=./wflow/ --add-modules java.se.ee"
+
 apache-tomcat-8.5.23/bin/catalina.sh $*
  
 
