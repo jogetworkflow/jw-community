jogetworkflow/jw-community

commit a64bdfc9c518d088ca083ccfcec1b69bdaab308c

Fixed: wflow-core - Datalist - Auto convert newline to <br> tag for non html string. #5103 @5.0-SNAPSHOT

wflow-core/src/main/java/org/joget/apps/datalist/service/DataListDecorator.java
@@ -140,6 +140,10 @@ public class DataListDecorator extends CheckboxTableDecorator {
         if (!MediaTypeEnum.HTML.equals(tableModel.getMedia())) {
             text = StringUtil.stripAllHtmlTag(text, false);
             text = StringEscapeUtils.unescapeHtml(text);
+        } else {
+            if (text.equals(StringUtil.stripAllHtmlTag(text, false))) { //check it is not html text
+                text = StringUtil.escapeString(text, StringUtil.TYPE_NL2BR, null);
+            }
         }
 
         // handle links

commit 0c151f31f4e77aa7d6b9f0775ae327f6257c009e

Fixed: wflow-core - Datalist - Regression bug from a64bdfc. @5.0-SNAPSHOT

wflow-core/src/main/java/org/joget/apps/datalist/service/DataListDecorator.java
@@ -137,12 +137,14 @@ public class DataListDecorator extends CheckboxTableDecorator {
         String text = formatColumn(column, row, columnValue);
 
         // strip tags if media type is not HTML
-        if (!MediaTypeEnum.HTML.equals(tableModel.getMedia())) {
-            text = StringUtil.stripAllHtmlTag(text, false);
-            text = StringEscapeUtils.unescapeHtml(text);
-        } else {
-            if (text.equals(StringUtil.stripAllHtmlTag(text, false))) { //check it is not html text
-                text = StringUtil.escapeString(text, StringUtil.TYPE_NL2BR, null);
+        if (text != null) {
+            if (!MediaTypeEnum.HTML.equals(tableModel.getMedia())) {
+                text = StringUtil.stripAllHtmlTag(text, false);
+                text = StringEscapeUtils.unescapeHtml(text);
+            } else {
+                if (text.equals(StringUtil.stripAllHtmlTag(text, false))) { //check it is not html text
+                    text = StringUtil.escapeString(text, StringUtil.TYPE_NL2BR, null);
+                }
             }
         }
 