jogetworkflow/jw-community

commit e38439cb82e48a2f2aa8efe08e353926508b0c63

Fixed: wflow-core - Userview v5 Theme - Custom HTML before and after login form are not render. @5.0-SNAPSHOT

wflow-core/src/main/java/org/joget/apps/userview/model/UserviewV5Theme.java
@@ -221,10 +221,10 @@ public abstract class UserviewV5Theme extends UserviewTheme {
      */
     public String getLoginForm(Map<String, Object> data) {
         if (!data.containsKey("login_form_before")) {
-            data.put("login_form_before", this.userview.getSetting().getPropertyString("loginPageTop"));
+            data.put("login_form_before", getPropertyString("loginPageTop"));
         }
         if (!data.containsKey("login_form_after")) {
-            data.put("login_form_after", this.userview.getSetting().getPropertyString("loginPageBottom"));
+            data.put("login_form_after", getPropertyString("loginPageBottom"));
         }
         return UserviewUtil.getTemplate(this, data, "/templates/userview/login.ftl");
     }

commit cdde5e08984495c88be083f63af63f119e4ff545

Fixed: wflow-core - Userview v5 Theme - Regression bug from e38439cb. @5.0-SNAPSHOT

wflow-core/src/main/java/org/joget/apps/userview/model/UserviewV5Theme.java
@@ -221,10 +221,10 @@ public abstract class UserviewV5Theme extends UserviewTheme {
      */
     public String getLoginForm(Map<String, Object> data) {
         if (!data.containsKey("login_form_before")) {
-            data.put("login_form_before", getPropertyString("loginPageTop"));
+            data.put("login_form_before", this.userview.getSetting().getPropertyString("loginPageTop"));
         }
         if (!data.containsKey("login_form_after")) {
-            data.put("login_form_after", getPropertyString("loginPageBottom"));
+            data.put("login_form_after", this.userview.getSetting().getPropertyString("loginPageBottom"));
         }
         return UserviewUtil.getTemplate(this, data, "/templates/userview/login.ftl");
     }