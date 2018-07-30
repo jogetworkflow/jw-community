5a5a65fadf101c189db7ff57f8112053d40e8ed9
owen <owen@joget.org>
2018-07-30 09:57:20 +0800

    Fixed: wflow-core - Section - "Show as readonly when no permission?" causing options field cannot display value correctly. @6.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/form/service/FormUtil.java
+++ wflow-core/src/main/java/org/joget/apps/form/service/FormUtil.java
@@ -392,7 +392,7 @@ public class FormUtil implements ApplicationContextAware {
         if (formData == null) {
             formData = new FormData();
         }
-        if (element.isAuthorize(formData)) {
+        if (element.isAuthorize(formData) || (!element.isAuthorize(formData) && "true".equalsIgnoreCase(element.getPropertyString("permissionReadonly")))) {
             FormLoadBinder binder = (FormLoadBinder) element.getOptionsBinder();
             if (binder != null && !isAjaxOptionsSupported(element, formData)) {
                 String primaryKeyValue = (formData != null) ? element.getPrimaryKeyValue(formData) : null;
