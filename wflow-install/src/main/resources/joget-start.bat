9590acde2cf88ffce38bb8e527c00e82ab0ee459
owen <owen@joget.org>
2018-11-08 10:32:03 +0800

    Modified: wflow-install - Move glowroot under wflow directory. @7.0-SNAPSHOT

--- wflow-install/src/main/resources/joget-start.bat
+++ wflow-install/src/main/resources/joget-start.bat
@@ -9,8 +9,8 @@ ECHO.
 REM Start Tomcat
 set JAVA_HOME=.\jre1.8.0_112
 set CATALINA_HOME=.\apache-tomcat-8.5.23
-set JAVA_OPTS=-Xmx512M -Dwflow.home=./wflow/ -javaagent:./glowroot/glowroot.jar
-REM set JAVA_OPTS=-Xmx1024M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/  -javaagent:./glowroot/glowroot.jar
+set JAVA_OPTS=-Xmx512M -Dwflow.home=./wflow/ -javaagent:./wflow/glowroot/glowroot.jar
+REM set JAVA_OPTS=-Xmx1024M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/  -javaagent:./wflow/glowroot/glowroot.jar
 ECHO == Starting Tomcat from %CATALINA_HOME% ==
 ECHO.
 %CATALINA_HOME%\bin\catalina.bat run
--- wflow-install/src/main/resources/tomcat8-run.bat
+++ wflow-install/src/main/resources/tomcat8-run.bat
@@ -1,8 +1,8 @@
 set JAVA_HOME=.\jre1.8.0_112
 set CATALINA_HOME=.\apache-tomcat-8.5.23
 
-set JAVA_OPTS=-Xmx512M -Dwflow.home=./wflow/ -javaagent:./glowroot/glowroot.jar
-REM set JAVA_OPTS=-Xmx1024M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/ -javaagent:./glowroot/glowroot.jar
+set JAVA_OPTS=-Xmx512M -Dwflow.home=./wflow/ -javaagent:./wflow/glowroot/glowroot.jar
+REM set JAVA_OPTS=-Xmx1024M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/ -javaagent:./wflow/glowroot/glowroot.jar
 
 %CATALINA_HOME%\bin\startup.bat
 
