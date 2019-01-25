21923c8844fd696389a7a5533cee6d8ee70ff20e
owen <owen@joget.org>
2019-01-25 17:01:29 +0800

    Modified: wflow-install - Update to JRE 1.8.0_122. @5.0-SNAPSHOT

--- wflow-install/src/main/resources/joget-start.bat
+++ wflow-install/src/main/resources/joget-start.bat
@@ -7,7 +7,7 @@ CALL .\mysql-start.bat
 ECHO.
 
 REM Start Tomcat
-set JAVA_HOME=.\jre1.7.0_76
+set JAVA_HOME=.\jre1.8.0_112
 set CATALINA_HOME=.\apache-tomcat-8.0.20
 set JAVA_OPTS=-XX:MaxPermSize=128m -Xmx512M -Dwflow.home=./wflow/
 REM set JAVA_OPTS=-XX:MaxPermSize=128m -Xmx1024M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/
