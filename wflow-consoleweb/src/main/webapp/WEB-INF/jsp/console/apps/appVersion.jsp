jogetworkflow/jw-community

commit 3d29b76f3ee847f8539ea7425493a9f99dda84ca

Modified: wflow-consoleweb - Console UI - Block UI when form submission. @7.0-SNAPSHOT

wflow-consoleweb/src/main/webapp/WEB-INF/jsp/console/apps/appVersion.jsp
@@ -142,7 +142,7 @@
                 <div id="propertyEditor" class="pluginConfig menu-wizard-container">
 
                 </div>
-                <form id="propertiesForm" action="${pageContext.request.contextPath}/web/console/app/${appId}/${appVersion}/dev/submit" class="form" method="POST" style="display:none">
+                <form id="propertiesForm" action="${pageContext.request.contextPath}/web/console/app/${appId}/${appVersion}/dev/submit" class="form blockui" method="POST" style="display:none">
                     <input id="properties" name="properties" type="hidden" value=""/>
                     <input id="id" name="id" type="hidden" value="${appResource.id}"/>
                 </form>
@@ -232,7 +232,7 @@
                 <div id="propertyEditorGit" class="pluginConfig menu-wizard-container-git">
 
                 </div>
-                <form id="propertiesFormGit" action="${pageContext.request.contextPath}/web/console/app/${appId}/${appVersion}/dev/submit" class="form" method="POST" style="display:none">
+                <form id="propertiesFormGit" action="${pageContext.request.contextPath}/web/console/app/${appId}/${appVersion}/dev/submit" class="form" method="POST blockui" style="display:none">
                     <input id="propertiesGit" name="properties" type="hidden" value=""/>
                     <input id="id" name="id" type="hidden" value="${appResource.id}"/>
                 </form>

commit 4b9df359181ed8c11f166ec733909390108d7833

Fixed: wflow-consoleweb - Git Integration - HTTP Status 405 Method Not Allowed when submitting Git configuration form, regression from 3d29b76 @7.0-SNAPSHOT

wflow-consoleweb/src/main/webapp/WEB-INF/jsp/console/apps/appVersion.jsp
@@ -232,7 +232,7 @@
                 <div id="propertyEditorGit" class="pluginConfig menu-wizard-container-git">
 
                 </div>
-                <form id="propertiesFormGit" action="${pageContext.request.contextPath}/web/console/app/${appId}/${appVersion}/dev/submit" class="form" method="POST blockui" style="display:none">
+                <form id="propertiesFormGit" action="${pageContext.request.contextPath}/web/console/app/${appId}/${appVersion}/dev/submit" class="form blockui" method="POST" style="display:none">
                     <input id="propertiesGit" name="properties" type="hidden" value=""/>
                     <input id="id" name="id" type="hidden" value="${appResource.id}"/>
                 </form>