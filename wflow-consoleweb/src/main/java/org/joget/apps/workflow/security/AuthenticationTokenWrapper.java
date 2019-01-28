jogetworkflow/jw-community

commit 695796eae7d60b6e5d8931031f3f55b18f899134

Added: wflow-core, wflow-consoleweb - Delegated App Admin - Added delegated app admin feature @7.0-SNAPSHOT

wflow-consoleweb/src/main/java/org/joget/apps/workflow/security/AuthenticationTokenWrapper.java
@@ -1,0 +1,72 @@
+package org.joget.apps.workflow.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import javax.security.auth.Subject;
+import static org.joget.workflow.model.service.WorkflowUserManager.ROLE_ADMIN;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class AuthenticationTokenWrapper implements Authentication {
+ 
+    Authentication authentication;
+
+    public AuthenticationTokenWrapper(Authentication auth) {
+        this.authentication = auth;
+    }
+    
+    @Override
+    public String getName() {
+        return authentication.getName();
+    }
+
+    @Override
+    public boolean implies(Subject subject) {
+        return authentication.implies(subject);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection tempAuthorities = new ArrayList<>(authentication.getAuthorities());
+        
+        // check for sys admin role, add if not in db
+        GrantedAuthority ga = new SimpleGrantedAuthority(ROLE_ADMIN);
+        if (tempAuthorities.contains(ga) && !EnhancedWorkflowUserManager.isSysAdminRoleAvailable()) {
+            tempAuthorities.add(new SimpleGrantedAuthority(EnhancedWorkflowUserManager.ROLE_SYSADMIN));
+        }
+        
+        // check for app admin role, remove if app admin role configured for specific users
+        if (tempAuthorities.contains(ga) && EnhancedWorkflowUserManager.isAppAdminRoleRestricted()) {
+            tempAuthorities.remove(ga);
+        }
+
+        return tempAuthorities;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return authentication.getCredentials();
+    }
+
+    @Override
+    public Object getDetails() {
+        return authentication.getDetails();
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return authentication.getPrincipal();
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authentication.isAuthenticated();
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        authentication.setAuthenticated(isAuthenticated);
+    }
+    
+}

wflow-consoleweb/pom.xml
@@ -299,7 +299,7 @@
                     </dependency>                         
                 </dependencies>                
                 <configuration>
-                    <complianceLevel>1.6</complianceLevel>
+                    <complianceLevel>1.7</complianceLevel>
                     <weaveDependencies>
                         <weaveDependency>
                             <groupId>org.joget</groupId>

wflow-consoleweb/src/main/java/org/joget/apps/app/controller/ConsoleWebController.java
@@ -50,6 +50,7 @@ import org.joget.apps.app.model.PluginDefaultProperties;
 import org.joget.apps.app.model.UserviewDefinition;
 import org.joget.apps.app.model.DatalistDefinition;
 import org.joget.apps.app.model.ImportAppException;
+import org.joget.apps.app.service.AppDevUtil;
 import org.joget.apps.app.service.AppResourceUtil;
 import org.joget.apps.app.service.AppService;
 import org.joget.apps.app.service.AppUtil;
@@ -63,6 +64,7 @@ import org.joget.apps.form.service.FormService;
 import org.joget.apps.form.service.FormUtil;
 import org.joget.apps.generator.service.GeneratorUtil;
 import org.joget.apps.userview.service.UserviewService;
+import org.joget.apps.workflow.security.EnhancedWorkflowUserManager;
 import org.joget.commons.spring.model.ResourceBundleMessage;
 import org.joget.commons.spring.model.ResourceBundleMessageDao;
 import org.joget.commons.spring.model.Setting;
@@ -1400,8 +1402,20 @@ public class ConsoleWebController {
     }
 
     @RequestMapping("/console/app/(*:appId)/versioning")
-    public String consoleAppVersioning(ModelMap model, @RequestParam(value = "appId") String appId) {
-        model.addAttribute("appId", appId);
+    public String consoleAppVersioning(ModelMap map, @RequestParam(value = "appId") String appId) throws JSONException {
+        AppDefinition appDef = appService.getAppDefinition(appId, null);
+        map.addAttribute("appId", appDef.getId());
+        map.addAttribute("appVersion", appDef.getVersion());
+        map.addAttribute("appDefinition", appDef);
+
+        Properties props = AppDevUtil.getAppDevProperties(appDef);
+        String properties = "{}";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.accumulate(WorkflowUserManager.ROLE_ADMIN, props.getProperty(WorkflowUserManager.ROLE_ADMIN));
+        jsonObject.accumulate(EnhancedWorkflowUserManager.ROLE_ADMIN_GROUP, props.getProperty(EnhancedWorkflowUserManager.ROLE_ADMIN_GROUP));
+        jsonObject.accumulate("orgId", props.getProperty(EnhancedWorkflowUserManager.ROLE_ADMIN_ORG));
+        properties = jsonObject.toString(4);
+        map.addAttribute("properties", PropertyUtil.propertiesJsonLoadProcessing(properties));
         return "console/apps/appVersion";
     }
 
@@ -5284,4 +5298,30 @@ public class ConsoleWebController {
         }
         return jsonArr;
     }
+    
+    @RequestMapping(value = "/console/app/(*:appId)/(~:version)/dev/submit", method = RequestMethod.POST)
+    public String consoleDevSubmit(ModelMap map, String id, @RequestParam String appId, @RequestParam(required = false) String version, @RequestParam(required = false) String properties) throws JSONException, IOException {
+        AppDefinition appDef = appService.getAppDefinition(appId, version);
+        map.addAttribute("appId", appDef.getId());
+        map.addAttribute("appVersion", appDef.getVersion());
+        map.addAttribute("appDefinition", appDef);
+        
+        String oldProperties = "{}";        
+        properties = PropertyUtil.propertiesJsonStoreProcessing(oldProperties, properties);
+        Properties appProps = new Properties();
+        JSONObject jsonObject = new JSONObject(properties);
+        if (!jsonObject.isNull(WorkflowUserManager.ROLE_ADMIN)) {
+            appProps.setProperty(WorkflowUserManager.ROLE_ADMIN, jsonObject.getString(WorkflowUserManager.ROLE_ADMIN));
+        }
+        if (!jsonObject.isNull(EnhancedWorkflowUserManager.ROLE_ADMIN_GROUP)) {
+            appProps.setProperty(EnhancedWorkflowUserManager.ROLE_ADMIN_GROUP, jsonObject.getString(EnhancedWorkflowUserManager.ROLE_ADMIN_GROUP));
+        }
+        if (!jsonObject.isNull("orgId")) {
+            appProps.setProperty(EnhancedWorkflowUserManager.ROLE_ADMIN_ORG, jsonObject.getString("orgId"));
+        }
+        AppDevUtil.setAppDevProperties(appDef, appProps);
+        
+        return "console/dialogClose";
+    }    
+    
 }

wflow-consoleweb/src/main/java/org/joget/apps/workflow/security/WorkflowAuthenticationProvider.java
@@ -95,8 +95,9 @@ public class WorkflowAuthenticationProvider implements AuthenticationProvider, M
         // return result
         User user = directoryManager.getUserByUsername(username);
         UserDetails details = new WorkflowUserDetails(user);
-        UsernamePasswordAuthenticationToken result = new UsernamePasswordAuthenticationToken(username, password, gaList);
-        result.setDetails(details);
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password, gaList);
+        token.setDetails(details);
+        Authentication result = new AuthenticationTokenWrapper(token);
         return result;
     }
 

wflow-consoleweb/src/main/resources/console.properties
@@ -968,3 +968,8 @@ userview.cache.application=Application
 userview.cache.user=User
 userview.cache.duration=Duration (s)
 userview.cache.duration.desc=Duration in seconds to cache content
+
+console.app.dev.manage=Manage App Configuration
+console.app.dev.settings=App Configuration Management
+console.app.dev.admin.settings=Custom App Admin Role
+

wflow-consoleweb/src/main/webapp/WEB-INF/applicationContext.xml
@@ -97,7 +97,15 @@
         <security:form-login login-page="/web/login" username-parameter="j_username" password-parameter="j_password" login-processing-url="/j_spring_security_check" default-target-url="/index.jsp" authentication-failure-url="/web/login?login_error=1" />
         <security:logout logout-url="/j_spring_security_logout" logout-success-url="/web/login"/>
         <security:anonymous username="roleAnonymous"/>
-        
+
+        <!-- Web Console system admin paths -->
+        <security:intercept-url pattern="/web/console/directory/**" access="ROLE_SYSADMIN"/>
+        <security:intercept-url pattern="/web/console/monitor/**" access="ROLE_SYSADMIN"/>
+        <security:intercept-url pattern="/web/console/setting/**" access="ROLE_SYSADMIN"/>
+        <security:intercept-url pattern="/web/json/console/directory/**" access="ROLE_SYSADMIN"/>
+        <security:intercept-url pattern="/web/json/console/monitor/**" access="ROLE_SYSADMIN"/>
+        <security:intercept-url pattern="/web/json/console/setting/**" access="ROLE_SYSADMIN"/>
+                
         <!-- Secure by default -->
         <security:intercept-url pattern="/web/json/**" access="ROLE_ADMIN" />
         <security:intercept-url pattern="/web/console/**" access="ROLE_ADMIN"/>
@@ -116,10 +124,13 @@
         <security:custom-filter ref="workflowHttpAuthFilter" after="BASIC_AUTH_FILTER" /> 
     </security:http>
         
+    <bean id="workflowUserManager" class="org.joget.apps.workflow.security.EnhancedWorkflowUserManager">
+    </bean>                
+
     <bean id="authenticationProvider" class="org.joget.apps.workflow.security.WorkflowAuthenticationProvider">
         <property name="directoryManager" ref="directoryManager"/>
     </bean>
-
+                                                                                                
     <bean id="workflowHttpAuthFilter" class="org.joget.apps.workflow.security.WorkflowHttpAuthProcessingFilter">
         <property name="authenticationManager" ref="authenticationManager" />
         <property name="workflowUserManager" ref="workflowUserManager"/>

commit 9c176c19697d3d7318f2fb23c1189556134ab05c

Fixed: wflow-consoleweb - JSON API - Error clearing credentials in token when authenticating in JSON API calls, regression from 695796e @7.0-SNAPSHOT

wflow-consoleweb/src/main/java/org/joget/apps/workflow/security/AuthenticationTokenWrapper.java
@@ -1,9 +1,11 @@
 package org.joget.apps.workflow.security;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
 import javax.security.auth.Subject;
 import static org.joget.workflow.model.service.WorkflowUserManager.ROLE_ADMIN;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -67,6 +69,22 @@ public class AuthenticationTokenWrapper implements Authentication {
     @Override
     public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
         authentication.setAuthenticated(isAuthenticated);
+    }
+ 
+    public void clearCredentials() {
+        // no direct way in Spring Security, so use reflection to clear password in token
+        Field field = null;
+        try {
+            field = authentication.getClass().getDeclaredField("credentials");
+            field.setAccessible(true);
+            field.set(authentication, null);
+        } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException ex) {
+            throw new BadCredentialsException(ex.getMessage(), ex);
+        } finally {
+            if (field != null) {
+                field.setAccessible(false);
+            }
+        }
     }
     
 }

wflow-consoleweb/src/main/java/org/joget/apps/workflow/security/WorkflowHttpAuthProcessingFilter.java
@@ -247,19 +247,7 @@ public class WorkflowHttpAuthProcessingFilter extends UsernamePasswordAuthentica
 
                         auth = getAuthenticationManager().authenticate(authRequest);
 
-                        // no direct way in Spring Security 2, so use reflection to clear password in token
-                        Field field = null;
-                        try {
-                            field = auth.getClass().getDeclaredField("credentials");
-                            field.setAccessible(true);
-                            field.set(auth, null);
-                        } catch (Exception ex) {
-                            LogUtil.error(getClass().getName(), ex, "Error clearing credentials in token");
-                        } finally {
-                            if (field != null) {
-                                field.setAccessible(false);
-                            }
-                        }
+                        ((AuthenticationTokenWrapper)auth).clearCredentials();
                         
                         if (auth.isAuthenticated()) {
                             currentUser = directoryManager.getUserByUsername(username);