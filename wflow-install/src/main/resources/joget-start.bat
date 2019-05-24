79dd878259813843e3987ada5fb01dd167fe60b8
julian <julian@joget.org>
2019-05-24 12:55:42 +0800

    Modified: wflow-install - Installer - Increase default max heap space config in tomcat startup script to 768M @7.0-SNAPSHOT

--- wflow-install/src/main/resources/joget-start.bat
+++ wflow-install/src/main/resources/joget-start.bat
@@ -9,7 +9,7 @@ ECHO.
 REM Start Tomcat
 set JAVA_HOME=.\jre11.0.2
 set CATALINA_HOME=.\apache-tomcat-8.5.41
-set JAVA_OPTS=-Xmx512M -Dwflow.home=./wflow/ -javaagent:./wflow/glowroot/glowroot.jar
+set JAVA_OPTS=-Xmx768M -Dwflow.home=./wflow/ -javaagent:./wflow/glowroot/glowroot.jar
 REM set JAVA_OPTS=-Xmx1024M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/  -javaagent:./wflow/glowroot/glowroot.jar
 ECHO == Starting Tomcat from %CATALINA_HOME% ==
 ECHO.
