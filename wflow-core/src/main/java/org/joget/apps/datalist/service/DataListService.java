b7d68b9571e7c8e8bdebe4050d6d08d432060c0c
owen <owen@joget.org>
2019-09-18 17:08:23 +0800

    Fixed: wflow-core - Datalist Builder - Hidden action plugin is appear in the builder. @6.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/datalist/service/DataListService.java
+++ wflow-core/src/main/java/org/joget/apps/datalist/service/DataListService.java
@@ -13,6 +13,7 @@ import org.joget.apps.form.model.FormRow;
 import org.joget.commons.util.LogUtil;
 import org.joget.commons.util.StringUtil;
 import org.joget.commons.util.TimeZoneUtil;
+import org.joget.plugin.base.HiddenPlugin;
 import org.joget.plugin.base.Plugin;
 import org.joget.plugin.base.PluginManager;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -94,7 +95,7 @@ public class DataListService {
         Collection<DataListAction> list = new ArrayList<DataListAction>();
         Collection<Plugin> pluginList = pluginManager.list(DataListAction.class);
         for (Plugin plugin : pluginList) {
-            if (plugin instanceof DataListAction) {
+            if (plugin instanceof DataListAction && !(plugin instanceof HiddenPlugin)) {
                 list.add((DataListAction) plugin);
             }
         }
