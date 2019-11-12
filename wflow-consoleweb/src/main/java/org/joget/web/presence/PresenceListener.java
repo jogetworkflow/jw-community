931b80cbf08e041014fd8ace1214f80176084531
julian <julian@joget.org>
2019-11-12 10:55:40 +0800

    Fixed: wflow-consoleweb - JBoss EAP - 500 Error after logout or session timeout @7.0-SNAPSHOT

--- wflow-consoleweb/src/main/java/org/joget/web/presence/PresenceListener.java
+++ wflow-consoleweb/src/main/java/org/joget/web/presence/PresenceListener.java
@@ -3,6 +3,7 @@ package org.joget.web.presence;
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
+import org.joget.commons.util.LogUtil;
 
 @WebListener
 public class PresenceListener implements HttpSessionListener{
@@ -14,7 +15,11 @@ public class PresenceListener implements HttpSessionListener{
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
-        String sessionId = se.getSession().getId();
-        PresenceManager.leave(null, sessionId);
+        try {
+            String sessionId = se.getSession().getId();
+            PresenceManager.leave(null, sessionId);
+        } catch(Exception e) {
+            LogUtil.debug(getClass().getName(), e.getMessage());
+        }
     }
 }
