027bcd67061d01bc07eb126df6254aff7781b05a
owen <owen@joget.org>
2019-11-14 17:50:27 +0800

    Fixed: wflow-consoleweb/wflow-core - Form Section - Section with only hidden field having border and padding in some userview theme. #7050 @7.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/form/model/Section.java
+++ wflow-core/src/main/java/org/joget/apps/form/model/Section.java
@@ -71,6 +71,10 @@ public class Section extends Element implements FormBuilderEditable, FormContain
             }   
 
             String html = FormUtil.generateElementHtml(this, formData, template, dataModel);
+            
+            if (!html.contains("form-cell")) {
+                html = html.replaceFirst("form-section", "form-section form-section-empty");
+            }
             return html;
         } else {
             return "";
