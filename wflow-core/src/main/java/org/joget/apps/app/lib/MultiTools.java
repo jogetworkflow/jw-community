2c80cbb230ba88fa093b03ce7ba5fecd32cef0e8
owen <owen@joget.org>
2019-11-18 18:48:00 +0800

    Modified: wflow-core - Multi Tools - Support run tool in multithread. @7.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/app/lib/MultiTools.java
+++ wflow-core/src/main/java/org/joget/apps/app/lib/MultiTools.java
@@ -1,10 +1,14 @@
 package org.joget.apps.app.lib;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 import java.util.HashMap;
 import org.joget.apps.app.model.AppDefinition;
 import org.joget.apps.app.model.ProcessMappingInfo;
 import org.joget.apps.app.service.AppUtil;
+import org.joget.commons.util.LogUtil;
+import org.joget.commons.util.PluginThread;
 import org.joget.plugin.base.ApplicationPlugin;
 import org.joget.plugin.base.DefaultApplicationPlugin;
 import org.joget.plugin.base.Plugin;
@@ -12,7 +16,7 @@ import org.joget.plugin.base.PluginManager;
 import org.joget.plugin.property.model.PropertyEditable;
 
 public class MultiTools extends DefaultApplicationPlugin implements ProcessMappingInfo {
-    
+
     @Override
     public String getName() {
         return "Multi Tools";
@@ -27,7 +31,7 @@ public class MultiTools extends DefaultApplicationPlugin implements ProcessMappi
     public String getDescription() {
         return "Enable the use of multiple tools";
     }
-    
+
     @Override
     public String getLabel() {
         return "Multi Tools";
@@ -37,7 +41,7 @@ public class MultiTools extends DefaultApplicationPlugin implements ProcessMappi
     public String getClassName() {
         return this.getClass().getName();
     }
-    
+
     @Override
     public String getPropertyOptions() {
         AppDefinition appDef = AppUtil.getCurrentAppDefinition();
@@ -52,29 +56,58 @@ public class MultiTools extends DefaultApplicationPlugin implements ProcessMappi
     public Object execute(Map properties) {
         Object[] tools = (Object[]) getProperty("tools");
         if (tools != null && tools.length > 0) {
+            boolean waitAllFinish = "true".equalsIgnoreCase(getPropertyString("waitAllFinish"));
+            boolean runInMultiThread = "true".equalsIgnoreCase(getPropertyString("runInMultiThread"));
+
+            Thread newThread;
+            Collection<Thread> threads = new ArrayList<Thread>();
+        
             PluginManager pluginManager = (PluginManager) AppUtil.getApplicationContext().getBean("pluginManager");
             for (Object tool : tools) {
                 if (tool != null && tool instanceof Map) {
                     Map toolMap = (Map) tool;
                     if (toolMap != null && toolMap.containsKey("className") && !toolMap.get("className").toString().isEmpty()) {
                         String className = toolMap.get("className").toString();
-                        ApplicationPlugin p = (ApplicationPlugin)pluginManager.getPlugin(className);
+                        ApplicationPlugin p = (ApplicationPlugin) pluginManager.getPlugin(className);
 
                         if (p != null) {
-                            Map propertiesMap = new HashMap(properties);
-                            propertiesMap.putAll((Map)toolMap.get("properties"));
-                            ApplicationPlugin appPlugin = (ApplicationPlugin) p;
+                            final Map propertiesMap = new HashMap(properties);
+                            propertiesMap.putAll((Map) toolMap.get("properties"));
+                            final ApplicationPlugin appPlugin = (ApplicationPlugin) p;
 
                             if (appPlugin instanceof PropertyEditable) {
                                 ((PropertyEditable) appPlugin).setProperties(propertiesMap);
                             }
-                            appPlugin.execute(propertiesMap);
+
+                            if (runInMultiThread) {
+                                newThread = new PluginThread(new Runnable() {
+                                    public void run() {
+                                        appPlugin.execute(propertiesMap);
+                                    }
+                                });
+                                newThread.start();
+                                threads.add(newThread);
+                            } else {
+                                appPlugin.execute(propertiesMap);
+                            }
                         }
                     }
                 }
             }
+            
+            if(runInMultiThread && waitAllFinish){
+                //wait for all threads to finish before reaching to the end of this plugin.
+                //if do not wait, the tool will reach the end and workflow process will continue without waiting for the end of all threads' executions.
+                for(Thread thread : threads){
+                    try {
+                        thread.join();
+                    } catch (InterruptedException ex) {
+                        LogUtil.error(getClassName(), ex, "");
+                    }
+                }
+            }
         }
-        
+
         return null;
     }
 
