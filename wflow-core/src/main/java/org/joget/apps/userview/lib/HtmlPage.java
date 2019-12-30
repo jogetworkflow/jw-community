a7519a9138edb0b72108d9a43cc87c498010eeb6
julian <julian@joget.org>
2019-12-30 15:16:18 +0800

    Fixed: wflow-core, wflow-consoleweb - HTML Page - Too little padding, regression from 0060947b @7.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/userview/lib/HtmlPage.java
+++ wflow-core/src/main/java/org/joget/apps/userview/lib/HtmlPage.java
@@ -76,8 +76,6 @@ public class HtmlPage extends UserviewMenu {
             ".ql-editor h4," +
             ".ql-editor h5," +
             ".ql-editor h6 {" +
-            "  margin: 0;" +
-            "  padding: 0;" +
             "  counter-reset: list-1 list-2 list-3 list-4 list-5 list-6 list-7 list-8 list-9;" +
             "}" + 
             ".ql-editor blockquote {" +
