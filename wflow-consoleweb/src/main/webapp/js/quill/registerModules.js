09de07cd38eadc02790ead2f77bcb24e0f2c1115
owen <owen@joget.org>
2020-03-12 22:39:52 +0800

    Modified: wflow-consoleweb - Quill - Switch to use inline style. #7471 @7.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/js/quill/registerModules.js
+++ wflow-consoleweb/src/main/webapp/js/quill/registerModules.js
@@ -1,2 +1,7 @@
 window.Quill = Quill;
-Quill.register("modules/htmlEditButton", htmlEditButton);
+Quill.register(Quill.import('attributors/style/align'),true);
+Quill.register(Quill.import('attributors/style/background'),true);
+Quill.register(Quill.import('attributors/style/color'),true);
+Quill.register(Quill.import('attributors/style/direction'),true);
+Quill.register(Quill.import('attributors/style/font'),true);
+Quill.register(Quill.import('attributors/style/size'),true);
