345c5308c5f990c625c98f0c60a8763846300912
owen <owen@joget.org>
2020-03-27 11:49:15 +0800

    Modified: wflow-core - Multi tools - support tools run in thread in correct sequence. @7.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/app/lib/MultiTools.java
+++ wflow-core/src/main/java/org/joget/apps/app/lib/MultiTools.java
@@ -16,6 +16,7 @@ import org.joget.plugin.base.Plugin;
 import org.joget.plugin.base.PluginManager;
 import org.joget.plugin.property.model.PropertyEditable;
 import org.joget.workflow.model.WorkflowAssignment;
+import org.joget.workflow.model.service.WorkflowUserManager;
 
 public class MultiTools extends DefaultApplicationPlugin implements ProcessMappingInfo {
 
@@ -55,56 +56,93 @@ public class MultiTools extends DefaultApplicationPlugin implements ProcessMappi
     }
 
     @Override
-    public Object execute(Map properties) {
-        Object[] tools = (Object[]) getProperty("tools");
+    public Object execute(final Map properties) {
+        final Object[] tools = (Object[]) getProperty("tools");
         if (tools != null && tools.length > 0) {
             boolean waitAllFinish = "true".equalsIgnoreCase(getPropertyString("waitAllFinish"));
             boolean runInMultiThread = "true".equalsIgnoreCase(getPropertyString("runInMultiThread"));
+            boolean runInSingleThread = "single".equalsIgnoreCase(getPropertyString("runInMultiThread"));
 
             Thread newThread;
             Collection<Thread> threads = new ArrayList<Thread>();
+            
+            final WorkflowUserManager workflowUserManager = (WorkflowUserManager) AppUtil.getApplicationContext().getBean("workflowUserManager");
+            final String currentUser = workflowUserManager.getCurrentUsername();
         
-            PluginManager pluginManager = (PluginManager) AppUtil.getApplicationContext().getBean("pluginManager");
-            for (Object tool : tools) {
-                if (tool != null && tool instanceof Map) {
-                    Map toolMap = (Map) tool;
-                    if (toolMap != null && toolMap.containsKey("className") && !toolMap.get("className").toString().isEmpty()) {
-                        String className = toolMap.get("className").toString();
-                        ApplicationPlugin p = (ApplicationPlugin) pluginManager.getPlugin(className);
+            final PluginManager pluginManager = (PluginManager) AppUtil.getApplicationContext().getBean("pluginManager");
+                
+            if (runInSingleThread) {
+                newThread = new PluginThread(new Runnable() {
+                    public void run() {
+                        workflowUserManager.setCurrentThreadUser(currentUser);
+                        
+                        for (Object tool : tools) {
+                            if (tool != null && tool instanceof Map) {
+                                Map toolMap = (Map) tool;
+                                if (toolMap != null && toolMap.containsKey("className") && !toolMap.get("className").toString().isEmpty()) {
+                                    String className = toolMap.get("className").toString();
+                                    ApplicationPlugin p = (ApplicationPlugin) pluginManager.getPlugin(className);
 
-                        if (p != null) {
-                            final Map propertiesMap = new HashMap(properties);
-                            propertiesMap.putAll(AppPluginUtil.getDefaultProperties((Plugin) p, (Map) toolMap.get("properties"), (AppDefinition) properties.get("appDef"), (WorkflowAssignment) properties.get("workflowAssignment")));
-                            final ApplicationPlugin appPlugin = (ApplicationPlugin) p;
+                                    if (p != null) {
+                                        Map propertiesMap = new HashMap(properties);
+                                        propertiesMap.putAll(AppPluginUtil.getDefaultProperties((Plugin) p, (Map) toolMap.get("properties"), (AppDefinition) properties.get("appDef"), (WorkflowAssignment) properties.get("workflowAssignment")));
+                                        ApplicationPlugin appPlugin = (ApplicationPlugin) p;
 
-                            if (appPlugin instanceof PropertyEditable) {
-                                ((PropertyEditable) appPlugin).setProperties(propertiesMap);
-                            }
+                                        if (appPlugin instanceof PropertyEditable) {
+                                            ((PropertyEditable) appPlugin).setProperties(propertiesMap);
+                                        }
 
-                            if (runInMultiThread) {
-                                newThread = new PluginThread(new Runnable() {
-                                    public void run() {
                                         appPlugin.execute(propertiesMap);
                                     }
-                                });
-                                newThread.start();
-                                threads.add(newThread);
-                            } else {
-                                appPlugin.execute(propertiesMap);
+                                }
+                            }
+                        }
+                    }
+                });
+                newThread.start();
+            } else {
+                for (Object tool : tools) {
+                    if (tool != null && tool instanceof Map) {
+                        Map toolMap = (Map) tool;
+                        if (toolMap != null && toolMap.containsKey("className") && !toolMap.get("className").toString().isEmpty()) {
+                            String className = toolMap.get("className").toString();
+                            ApplicationPlugin p = (ApplicationPlugin) pluginManager.getPlugin(className);
+
+                            if (p != null) {
+                                final Map propertiesMap = new HashMap(properties);
+                                propertiesMap.putAll(AppPluginUtil.getDefaultProperties((Plugin) p, (Map) toolMap.get("properties"), (AppDefinition) properties.get("appDef"), (WorkflowAssignment) properties.get("workflowAssignment")));
+                                final ApplicationPlugin appPlugin = (ApplicationPlugin) p;
+
+                                if (appPlugin instanceof PropertyEditable) {
+                                    ((PropertyEditable) appPlugin).setProperties(propertiesMap);
+                                }
+
+                                if (runInMultiThread) {
+                                    newThread = new PluginThread(new Runnable() {
+                                        public void run() {
+                                            workflowUserManager.setCurrentThreadUser(currentUser);
+                                            appPlugin.execute(propertiesMap);
+                                        }
+                                    });
+                                    newThread.start();
+                                    threads.add(newThread);
+                                } else {
+                                    appPlugin.execute(propertiesMap);
+                                }
                             }
                         }
                     }
                 }
-            }
-            
-            if(runInMultiThread && waitAllFinish){
-                //wait for all threads to finish before reaching to the end of this plugin.
-                //if do not wait, the tool will reach the end and workflow process will continue without waiting for the end of all threads' executions.
-                for(Thread thread : threads){
-                    try {
-                        thread.join();
-                    } catch (InterruptedException ex) {
-                        LogUtil.error(getClassName(), ex, "");
+
+                if(runInMultiThread && waitAllFinish){
+                    //wait for all threads to finish before reaching to the end of this plugin.
+                    //if do not wait, the tool will reach the end and workflow process will continue without waiting for the end of all threads' executions.
+                    for(Thread thread : threads){
+                        try {
+                            thread.join();
+                        } catch (InterruptedException ex) {
+                            LogUtil.error(getClassName(), ex, "");
+                        }
                     }
                 }
             }
