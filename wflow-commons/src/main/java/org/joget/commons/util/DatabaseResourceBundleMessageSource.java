aee9d2f7ee771cf53221b2763d906457eed08d94
owen <owen@joget.org>
2020-06-11 19:12:26 +0800

    Fixed: wflow-commons - I18N - Message with arguments are not able to pick up custom message added in Manage Messages. #7878 @7.0-SNAPSHOT

--- wflow-commons/src/main/java/org/joget/commons/util/DatabaseResourceBundleMessageSource.java
+++ wflow-commons/src/main/java/org/joget/commons/util/DatabaseResourceBundleMessageSource.java
@@ -1,14 +1,17 @@
 package org.joget.commons.util;
 
+import java.text.MessageFormat;
+import java.util.HashMap;
 import org.joget.commons.spring.model.ResourceBundleMessage;
 import org.joget.commons.spring.model.ResourceBundleMessageDao;
 import java.util.Locale;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Map;
 import org.springframework.context.support.ResourceBundleMessageSource;
 
 public class DatabaseResourceBundleMessageSource extends ResourceBundleMessageSource {
 
     private ResourceBundleMessageDao resourceBundleMessageDao;
+    private final Map<String, MessageFormat> cachedMessageFormats = new HashMap<String, MessageFormat>();
 
     public ResourceBundleMessageDao getResourceBundleMessageDao() {
         return resourceBundleMessageDao;
@@ -33,6 +36,27 @@ public class DatabaseResourceBundleMessageSource extends ResourceBundleMessageSo
             return resourceBundleMessage.getMessage();
         } else {
             return super.resolveCodeWithoutArguments(code, locale);
+        }
+    }
+    
+    @Override
+    protected MessageFormat resolveCode(String code, Locale locale) {
+        String localeToUse = locale.toString();
+        
+        ResourceBundleMessage resourceBundleMessage = null;
+        try {
+            resourceBundleMessage = resourceBundleMessageDao.getMessage(code, localeToUse);
+        } catch (Exception e) {
+            //LogUtil.error(getClass().getName(), null, "Error retrieving resource bundle message for " + code);
+        }
+
+        if (resourceBundleMessage != null) {
+            if (!cachedMessageFormats.containsKey(resourceBundleMessage.getMessage() + ":" + locale)) {
+                cachedMessageFormats.put(resourceBundleMessage.getMessage() + ":" + locale, createMessageFormat(resourceBundleMessage.getMessage(), locale));
+            }
+            return cachedMessageFormats.get(resourceBundleMessage.getMessage() + ":" + locale);
+        } else {
+            return super.resolveCode(code, locale);
         }
     }
 }
