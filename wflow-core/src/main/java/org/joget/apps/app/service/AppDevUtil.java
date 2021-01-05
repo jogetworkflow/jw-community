21cd2f86d82efbcc7b61690e785e8356dfa5ce2b
derekchentc <62978237+derekchentc@users.noreply.github.com>
2021-01-05 12:47:01 +0800

    Fixed: wflow-core - Git to Joget sync failed for Resources files to wflow folder. #8674 @7.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/app/service/AppDevUtil.java
+++ wflow-core/src/main/java/org/joget/apps/app/service/AppDevUtil.java
@@ -1509,6 +1509,19 @@ public class AppDevUtil {
             LogUtil.info(AppDevUtil.class.getName(), "Change detected (" + latestDate + " vs " + appLastModifiedDate + "), init sync for app " + appDef);
             // sync app
             updatedAppDef = appDefinitionDao.syncAppDefinition(appDef.getAppId(), appDef.getVersion());
+            String sourcePath = SetupManager.getBaseDirectory() + File.separator + "app_src" + File.separator + appDef.getAppId() + File.separator + appDef.getAppId() + "_" + appDef.getVersion() + File.separator + "resources" + File.separator;
+            String targetDirName = AppResourceUtil.getBaseDirectory() + appDef.getAppId() + File.separator + appDef.getVersion();
+
+            File sourceDir = new File(sourcePath);
+            if (sourceDir.exists()) {
+                File targetDir = new File(targetDirName);
+                // remove existing directory
+                FileUtils.deleteDirectory(targetDir);
+                // copy directory
+                targetDir.mkdirs();
+                FileUtils.copyDirectory(sourceDir, targetDir, true);
+            }
+           
             LogUtil.info(AppDevUtil.class.getName(), "Sync complete for app " + appDef);
         }
         return updatedAppDef;
