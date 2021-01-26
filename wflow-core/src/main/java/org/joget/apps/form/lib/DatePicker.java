4428774f8e282c47d92467773759c455b673f0ec
derekchentc <62978237+derekchentc@users.noreply.github.com>
2021-01-26 12:56:29 +0800

    Revert "Fixed: wflow-core - Import Menu bug when start date and end data validates each other. @7.0-SNAPSHOT"
    This reverts commit b3aec8e32f7b365eb1370c2ed6704436423ff342.

--- wflow-core/src/main/java/org/joget/apps/form/lib/DatePicker.java
+++ wflow-core/src/main/java/org/joget/apps/form/lib/DatePicker.java
@@ -405,24 +405,21 @@ public class DatePicker extends Element implements FormBuilderPaletteElement, Pw
                 if ("utcdateTime".equalsIgnoreCase(getPropertyString("datePickerType"))) {
                     dataFormat = UTC_DATEFORMAT;
                 }
-                
-                boolean inDisplayFormat = DateUtil.validateDateFormat(value, displayFormat);
-                if (!inDisplayFormat) {
-                    if (!displayFormat.equals(dataFormat)) {
-                        SimpleDateFormat data = new SimpleDateFormat(dataFormat);
-                        SimpleDateFormat display = new SimpleDateFormat(displayFormat);
-                        if ("utcdateTime".equalsIgnoreCase(getPropertyString("datePickerType"))) {
-                            String id = getPropertyString(FormUtil.PROPERTY_ID);
-                            if (FormUtil.PROPERTY_DATE_CREATED.equals(id) || FormUtil.PROPERTY_DATE_MODIFIED.equals(id)) {
-                                data.setTimeZone(TimeZone.getDefault());
-                            } else {
-                                data.setTimeZone(TimeZone.getTimeZone("UTC"));
-                            }
-                            display.setTimeZone(getUserTZ());
+                    
+                if (!displayFormat.equals(dataFormat)) {
+                    SimpleDateFormat data = new SimpleDateFormat(dataFormat);
+                    SimpleDateFormat display = new SimpleDateFormat(displayFormat);
+                    if ("utcdateTime".equalsIgnoreCase(getPropertyString("datePickerType"))) {
+                        String id = getPropertyString(FormUtil.PROPERTY_ID);
+                        if (FormUtil.PROPERTY_DATE_CREATED.equals(id) || FormUtil.PROPERTY_DATE_MODIFIED.equals(id)) {
+                            data.setTimeZone(TimeZone.getDefault());
+                        } else {
+                            data.setTimeZone(TimeZone.getTimeZone("UTC"));
                         }
-                        Date date = data.parse(value);
-                        value = display.format(date);
+                        display.setTimeZone(getUserTZ());
                     }
+                    Date date = data.parse(value);
+                    value = display.format(date);
                 }
             } catch (Exception e) {
             }
