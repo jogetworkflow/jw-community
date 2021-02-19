aa92a109a7de55e73d95fac5fd30ba18fcdeadc5
owen <owen@joget.org>
2021-02-19 11:53:48 +0800

    Modified: wflow-core - Universal/Progressive Theme - Disable zoom in effect of login form in Safari mobile. #8768 @7.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/plugin/enterprise/UniversalTheme.java
+++ wflow-core/src/main/java/org/joget/plugin/enterprise/UniversalTheme.java
@@ -127,7 +127,11 @@ public class UniversalTheme extends UserviewV5Theme implements PluginWebSupport 
     public String getMetas(Map<String, Object> data) {
 
         String meta = super.getMetas(data) + "\n";
-        meta += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
+        if ((Boolean) data.get("is_login_page")) {
+            meta += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\">\n";
+        } else {
+            meta += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
+        }
         meta += "<meta name=\"msapplication-tap-highlight\" content=\"no\"/>\n";
         return meta;
     }
