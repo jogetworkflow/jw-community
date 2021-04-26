85f8285dcd0474d60eacdbd88704ef359a404332
owen <owen@joget.org>
2021-04-26 15:28:31 +0800

    Modified: wflow-enterpriseweb - Revert SetupManager changes. @8.0-SNAPSHOT

--- wflow-commons/src/main/java/org/joget/commons/util/SetupManagerHelper.java
+++
@@ -1,9 +0,0 @@
-package org.joget.commons.util;
-
-import java.util.Map;
-import org.joget.commons.spring.model.Setting;
-
-public interface SetupManagerHelper {
-    
-    public void checkSettingChanges(Map<String, Setting> settingMap);
-}
