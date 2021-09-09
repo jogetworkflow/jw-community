aa4ec6c5db4bb5a4f1ccf8bf20617febe2fbdca1
owen <owen@joget.org>
2021-09-09 23:26:05 +0800

    Modified: wflow-core - Datalist - Enhance datalist text field filter type to support "Starts with", "Ends with", "Exact", "Larger than" and "Smaller than" search. @8.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/datalist/lib/TextFieldDataListFilterType.java
+++ wflow-core/src/main/java/org/joget/apps/datalist/lib/TextFieldDataListFilterType.java
@@ -1,5 +1,8 @@
 package org.joget.apps.datalist.lib;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -14,30 +17,37 @@ import org.joget.apps.userview.model.PwaOfflineResources;
 
 public class TextFieldDataListFilterType extends DataListFilterTypeDefault implements PwaOfflineResources {
 
+    @Override
     public String getName() {
         return "Text Field Data List Filter Type";
     }
 
+    @Override
     public String getVersion() {
         return "5.0.0";
     }
 
+    @Override
     public String getDescription() {
         return "Data List Filter Type - Text Field";
     }
 
+    @Override
     public String getLabel() {
         return "Text Field";
     }
 
+    @Override
     public String getClassName() {
         return this.getClass().getName();
     }
 
+    @Override
     public String getPropertyOptions() {
         return AppUtil.readPluginResource(getClass().getName(), "/properties/datalist/textFieldDataListFilterType.json", null, true, null);
     }
 
+    @Override
     public String getTemplate(DataList datalist, String name, String label) {
         PluginManager pluginManager = (PluginManager) AppUtil.getApplicationContext().getBean("pluginManager");
         Map dataModel = new HashMap();
@@ -45,27 +55,90 @@ public class TextFieldDataListFilterType extends DataListFilterTypeDefault imple
         dataModel.put("label", label);
         dataModel.put("value", getValue(datalist, name, getPropertyString("defaultValue")));
         dataModel.put("contextPath", WorkflowUtil.getHttpServletRequest().getContextPath());
+        
+        String jsTimeoutName = datalist.getDataListEncodedParamName(DataList.PARAMETER_FILTER_PREFIX+name);
+        jsTimeoutName = jsTimeoutName.replace(".", "");
+        jsTimeoutName = jsTimeoutName.replace("-", "");
+        jsTimeoutName = jsTimeoutName.replace("_", "");
+        dataModel.put("jsTimeoutName", jsTimeoutName);
+        
+        String filterSelection = getPropertyString("filterSelection");
+        if(filterSelection.contains("String")){
+            dataModel.put("filterSelectionString", "true");
+        }
+        if(filterSelection.contains("Number")){
+            dataModel.put("filterSelectionNumeric", "true");
+        }
+        dataModel.put("searchType", getValue(datalist, name + "_searchType", "any"));
+        
         return pluginManager.getPluginFreeMarkerTemplate(dataModel, getClassName(), "/templates/textFieldDataListFilterType.ftl", null);
     }
 
+    @Override
     public DataListFilterQueryObject getQueryObject(DataList datalist, String name) {
         DataListFilterQueryObject queryObject = new DataListFilterQueryObject();
         String value = getValue(datalist, name, getPropertyString("defaultValue"));
+        String searchType = getValue(datalist, name + "_searchType", "any");
+        
         if (datalist != null && datalist.getBinder() != null && value != null && !value.isEmpty()) {
-            String cname = datalist.getBinder().getColumnName(name);
+            Set<String> searchField = new HashSet<String>();
+            searchField.add(name);
             
-            //support aggregate function
-            if (cname.toLowerCase().contains("count(")
+            if (!getPropertyString("searchFields").isEmpty()) {
+                String[] fields = getPropertyString("searchFields").split(";");
+                searchField.addAll(Arrays.asList(fields));
+            }
+            
+            String query = "";
+            Collection<String> values = new ArrayList<String>();
+            
+            for (String f : searchField) {
+                if (!query.isEmpty()) {
+                    query += " OR ";
+                }
+                
+                String cname = datalist.getBinder().getColumnName(f);
+                boolean isAggregate = (cname.toLowerCase().contains("count(")
                     || cname.toLowerCase().contains("sum(")
                     || cname.toLowerCase().contains("avg(")
                     || cname.toLowerCase().contains("min(")
-                    || cname.toLowerCase().contains("max(")) {
-                queryObject.setQuery(cname + " = ?");
-                queryObject.setValues(new String[]{value});
-            } else {
-                queryObject.setQuery("lower(" + cname + ") like lower(?)");
-                queryObject.setValues(new String[]{'%' + value + '%'});
+                    || cname.toLowerCase().contains("max("));
+                
+                if(searchType.equalsIgnoreCase("largerThan") || searchType.equalsIgnoreCase("smallerThan") || isAggregate){
+                    if (isAggregate) {
+                        query += cname;
+                    } else {
+                        query += "cast(" + cname + " as big_decimal)";
+                    }
+                    
+                    if (searchType.equalsIgnoreCase("largerThan")) {
+                        query += " >= "; 
+                    } else if (searchType.equalsIgnoreCase("smallerThan")) {
+                        query += " <= "; 
+                    } else {
+                        query += " = "; 
+                    }
+                    
+                    query += "cast(? as big_decimal)";
+                    values.add(value);
+                } else if (searchType.equalsIgnoreCase("startsWith") || searchType.equalsIgnoreCase("endsWith") || searchType.equalsIgnoreCase("any")) {
+                    query += "lower(" + cname + ") like lower(?)";
+                    
+                    if (searchType.equalsIgnoreCase("startsWith")) {
+                        values.add(value + "%");
+                    } else if (searchType.equalsIgnoreCase("endsWith")) {
+                        values.add("%" + value);
+                    } else {
+                        values.add("%" + value + "%");
+                    }
+                } else if (searchType.equalsIgnoreCase("exact")) {
+                    query += cname + " = ?";
+                    values.add(value);
+                }
             }
+
+            queryObject.setQuery("(" + query + ")");
+            queryObject.setValues(values.toArray(new String[0]));
             
             return queryObject;
         }
--- wflow-core/src/main/resources/templates/textFieldDataListFilterType.ftl
+++ wflow-core/src/main/resources/templates/textFieldDataListFilterType.ftl
@@ -1,7 +1,42 @@
 <script type="text/javascript" src="${contextPath}/plugin/org.joget.apps.datalist.lib.TextFieldDataListFilterType/js/jquery.placeholder.min.js"></script>
+<#if (filterSelectionNumeric! == 'true' || filterSelectionString! == 'true')>
+    <select id="${name!?replace(".", "_")}_searchType" name="${name!}_searchType" style="display:none">
+        <option <#if searchType! == 'any'>selected</#if> value="any">@@datalist.tfdlft.filter.any@@</option>
+        
+        <#if filterSelectionNumeric! == 'true'>
+        <option <#if searchType! == 'largerThan'>selected</#if> value="largerThan">@@datalist.tfdlft.filter.largerThan@@</option>
+        <option <#if searchType! == 'smallerThan'>selected</#if> value="smallerThan">@@datalist.tfdlft.filter.smallerThan@@</option>
+        </#if>
+        
+        <#if filterSelectionString! == 'true'>
+        <option <#if searchType! == 'startsWith'>selected</#if> value="startsWith">@@datalist.tfdlft.filter.startsWith@@</option>
+        <option <#if searchType! == 'endsWith'>selected</#if> value="endsWith">@@datalist.tfdlft.filter.endsWith@@</option>
+        <option <#if searchType! == 'exact'>selected</#if> value="exact">@@datalist.tfdlft.filter.exact@@</option>
+        </#if>
+    </select>
+</#if>
 <input id="${name!?replace(".", "_")}" name="${name!}" type="text" size="10" value="${value!?html}" placeholder="${label!?html}"/>
 <script type="text/javascript">
     $(document).ready(function(){
+        var searchTypeTimeout${jsTimeoutName!};
+        
         $('#${name!?replace(".", "_")}').placeholder();
+
+        if ($('#${name!?replace(".", "_")}_searchType').length > 0) {
+            if ($("#${name!?replace(".", "_")}_searchType").val() !== "any") {
+                $('#${name!?replace(".", "_")}_searchType').show();
+            }
+            $('#${name!?replace(".", "_")}, #${name!?replace(".", "_")}_searchType').focus(function(e){
+                clearTimeout(searchTypeTimeout${jsTimeoutName!});
+                $('#${name!?replace(".", "_")}_searchType').show();
+            });
+            $('#${name!?replace(".", "_")}, #${name!?replace(".", "_")}_searchType').blur(function(e){
+                searchTypeTimeout${jsTimeoutName!} = setTimeout( function(){
+                    if ($("#${name!?replace(".", "_")}_searchType").val() === "any") {
+                        $("#${name!?replace(".", "_")}_searchType").hide();
+                    }
+                }, 1000);
+            });
+        }
     });
 </script>
