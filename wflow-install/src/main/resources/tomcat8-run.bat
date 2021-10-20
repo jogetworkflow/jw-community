6d927389a4a2c13a8572e4f092d610f0d8b05e71
owen <owen@joget.org>
2021-10-20 11:14:34 +0800

    Modified: wflow-install - Upgraded to Apache Tomcat 8.5.72 @7.0-SNAPSHOT

--- wflow-install/src/main/resources/tomcat8-run.bat
+++ wflow-install/src/main/resources/tomcat8-run.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=.\jre11.0.2
-set CATALINA_HOME=.\apache-tomcat-8.5.65
+set CATALINA_HOME=.\apache-tomcat-8.5.72
 
 set JAVA_OPTS=-Xmx768M -Dwflow.home=./wflow/
 REM set JAVA_OPTS=-Xmx1024M -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/
--- wflow-install/src/main/resources/tomcat8-stop.bat
+++ wflow-install/src/main/resources/tomcat8-stop.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=.\jre11.0.2
-set CATALINA_HOME=.\apache-tomcat-8.5.65
+set CATALINA_HOME=.\apache-tomcat-8.5.72
 
 %CATALINA_HOME%\bin\shutdown.bat
 
