97a580b2650bec2e4a26706b7cca90ac5f9db854
owen <owen@joget.org>
2021-12-22 10:05:05 +0800

    Fixed: wflow-commons - Hash Variable required HttpServletRequest object can't work in PluginThread. 7.0-SNAPSHOT

--- wflow-commons/src/main/java/org/joget/commons/util/PluginThread.java
+++ wflow-commons/src/main/java/org/joget/commons/util/PluginThread.java
@@ -1,15 +1,30 @@
 package org.joget.commons.util;
 
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
 /**
  * Thread implementation to used by plugin
  */
 public final class PluginThread extends Thread {
     
     private final String profile;
+    private final HttpServletRequest request;
     
     public PluginThread(Runnable r) {
         super(r);
         profile = DynamicDataSourceManager.getCurrentProfile();
+        ServletRequestAttributes sra = null;
+        try {
+            sra = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes());
+        } catch (IllegalStateException e) {
+        }
+        if (sra != null) {
+            request = sra.getRequest();
+        } else {
+            request = null;
+        }
     }
     
     private void setProfile() {
@@ -19,6 +34,15 @@ public final class PluginThread extends Thread {
     @Override
     public void run() {
         setProfile();
+        
+        if (request != null) {
+            RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        }
+        
         super.run();
+        
+        if (request != null) {
+            RequestContextHolder.resetRequestAttributes();
+        }
     }
 }
