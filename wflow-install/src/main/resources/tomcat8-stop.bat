23d650bc8f7d3ce9a294065114d8453b25adb8c5
owen <owen@joget.org>
2022-04-04 11:54:02 +0800

    Modified: wflow-install - Update Apache Tomcat to 8.5.78 for CVE-2022-22965. @6.0-SNAPSHOT

--- wflow-install/src/main/resources/tomcat8-stop.bat
+++ wflow-install/src/main/resources/tomcat8-stop.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=.\jre1.8.0_112
-set CATALINA_HOME=.\apache-tomcat-8.5.41
+set CATALINA_HOME=.\apache-tomcat-8.5.78
 
 %CATALINA_HOME%\bin\shutdown.bat
 
--- wflow-install/src/main/resources/tomcat8.sh
+++ wflow-install/src/main/resources/tomcat8.sh
@@ -5,6 +5,6 @@ export JAVA_OPTS="-XX:MaxPermSize=128m -Xmx512M -Dwflow.home=./wflow/ -javaagent
 #For Java 9
 #export JAVA_OPTS="-XX:MaxPermSize=128m -Xmx512M -Dwflow.home=./wflow/ -javaagent:./wflow/aspectjweaver-1.8.5.jar --add-modules java.se.ee"
 
-apache-tomcat-8.5.41/bin/catalina.sh $*
+apache-tomcat-8.5.78/bin/catalina.sh $*
  
 
