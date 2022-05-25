b46172d6c0e48cab144b90a36c4c3032f9eaa37b
owen <owen@joget.org>
2022-05-25 11:55:56 +0800

    Modified: wflow-commons, wflow-plugin-base - To support DX7 plugin using hibernate4. @8.0-SNAPSHOT

---
+++ wflow-commons/src/main/java/org/springframework/orm/hibernate4/LocalSessionFactoryBean.java
@@ -0,0 +1,23 @@
+package org.springframework.orm.hibernate4;
+
+import java.util.Properties;
+
+public class LocalSessionFactoryBean extends org.springframework.orm.hibernate5.LocalSessionFactoryBean {
+    
+    @Override
+    public void setHibernateProperties(Properties hibernateProperties) {
+        
+        // set properties required for hibernate 5
+        if (!hibernateProperties.containsKey("hibernate.enable_lazy_load_no_trans")) {
+            hibernateProperties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
+        }
+        if (!hibernateProperties.containsKey("hibernate.allow_update_outside_transaction")) {
+            hibernateProperties.setProperty("hibernate.allow_update_outside_transaction", "true");
+        }
+        if (!hibernateProperties.containsKey("hibernate.transaction.coordinator_class")) {
+            hibernateProperties.setProperty("hibernate.transaction.coordinator_class", "jta");
+        }
+        
+        super.setHibernateProperties(hibernateProperties);
+    }
+}
