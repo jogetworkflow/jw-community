jogetworkflow/jw-community

commit 3dd80796629702bd7901db2e36960a4273bbddfa

Fixed: wflow-consoleweb - Process Builder - Correction of 7b5062cc. @8.0-SNAPSHOT

wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
@@ -79,7 +79,12 @@ ProcessBuilder = {
             CustomBuilder.Builder.bindEvent("nodeAdditionalSelected nodeAdditionalAdded nodeAdditionalRemoved nodeAdditionalModeChanged", ProcessBuilder.refreshConnections);
             
             $(window).off('hashchange');
-            $(window).on('hashchange', ProcessBuilder.viewProcess);
+            $(window).on('hashchange', function(){
+                var id = window.location.hash.replace("#", "");
+                if ((ProcessBuilder.currentProcessData.properties === undefined || (ProcessBuilder.currentProcessData.properties !== undefined && id !== ProcessBuilder.currentProcessData.properties.id))) {
+                    ProcessBuilder.viewProcess();
+                }
+            });
 
             var deferreds = [];
             
@@ -278,29 +283,27 @@ ProcessBuilder = {
         var self = CustomBuilder.Builder;
         var id = window.location.hash.replace("#", "");
         
-        if ((ProcessBuilder.currentProcessData.properties === undefined || (ProcessBuilder.currentProcessData.properties !== undefined && id !== ProcessBuilder.currentProcessData.properties.id))) {
-            if (id !== "") {
-                ProcessBuilder.generateProcessData(id);
-                
-                if (ProcessBuilder.currentProcessData !== undefined && ProcessBuilder.currentProcessData.properties !== undefined) {
-                    CustomBuilder.Builder.load(ProcessBuilder.currentProcessData, function(){
-                        ProcessBuilder.validate();
-                        
-                        setTimeout(function(){
-                            if (ProcessBuilder.preSelect !== "") {
-                                var node = self.frameBody.find("[data-cbuilder-id='"+ProcessBuilder.preSelect+"']");
-                                self.selectNode(node);
-                                ProcessBuilder.preSelect = "";
-                            }
-                            if (ProcessBuilder.view !== "") {
-                                $("[data-cbuilder-view='"+ProcessBuilder.view+"']").trigger("click");
-                            }
-                        }, 1000);
-                    });
-                }
-            } else {
-                window.location.hash = "process1";
+        if (id !== "") {
+            ProcessBuilder.generateProcessData(id);
+
+            if (ProcessBuilder.currentProcessData !== undefined && ProcessBuilder.currentProcessData.properties !== undefined) {
+                CustomBuilder.Builder.load(ProcessBuilder.currentProcessData, function(){
+                    ProcessBuilder.validate();
+
+                    setTimeout(function(){
+                        if (ProcessBuilder.preSelect !== "") {
+                            var node = self.frameBody.find("[data-cbuilder-id='"+ProcessBuilder.preSelect+"']");
+                            self.selectNode(node);
+                            ProcessBuilder.preSelect = "";
+                        }
+                        if (ProcessBuilder.view !== "") {
+                            $("[data-cbuilder-view='"+ProcessBuilder.view+"']").trigger("click");
+                        }
+                    }, 1000);
+                });
             }
+        } else {
+            window.location.hash = "process1";
         }
     },
     

commit 760be4ef4c456f0238ad3ac2059c0da934c7b5d6

Fixed: wflow-consoleweb - Process Builder - Delete process is broken caused by 3dd80796. @8.0-SNAPSHOT

wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
@@ -83,7 +83,9 @@ ProcessBuilder = {
             $(window).off('hashchange');
             $(window).on('hashchange', function(){
                 var id = window.location.hash.replace("#", "");
-                if ((ProcessBuilder.currentProcessData.properties === undefined || (ProcessBuilder.currentProcessData.properties !== undefined && id !== ProcessBuilder.currentProcessData.properties.id))) {
+                
+                //when no current process data or current process data is not match with the id in URL hash
+                if (ProcessBuilder.currentProcessData === null || ProcessBuilder.currentProcessData === undefined || (ProcessBuilder.currentProcessData.properties === undefined || (ProcessBuilder.currentProcessData.properties !== undefined && id !== ProcessBuilder.currentProcessData.properties.id))) {
                     ProcessBuilder.viewProcess();
                 }
             });
@@ -285,10 +287,11 @@ ProcessBuilder = {
         var self = CustomBuilder.Builder;
         var id = window.location.hash.replace("#", "");
         
-        if (id !== "") {
+        //if id not empty or id is empty but there is remaining process in package. 
+        //The generateProcessData method will redirect to the first remaining process when id is empty
+        if (id !== "" || $("#processes_list option").length > 0) {
             ProcessBuilder.generateProcessData(id);
-
-            if (ProcessBuilder.currentProcessData !== undefined && ProcessBuilder.currentProcessData.properties !== undefined) {
+            if (ProcessBuilder.currentProcessData !== undefined && ProcessBuilder.currentProcessData !== null && ProcessBuilder.currentProcessData.properties !== undefined) {
                 CustomBuilder.Builder.load(ProcessBuilder.currentProcessData, function(){
                     ProcessBuilder.validate();
 
@@ -304,7 +307,7 @@ ProcessBuilder = {
                     }, 1000);
                 });
             }
-        } else {
+        } else { //only redirect to `process1` when there is totally no process in package
             window.location.hash = "process1";
         }
     },