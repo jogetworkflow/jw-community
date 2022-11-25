ed4800c459f6f45127e0a2a554a16d87d63852cd
fina <fina.luthfiyani@kinnarastudio.com>
2022-11-25 18:22:19 +0700

    change color theme

--- wflow-consoleweb/src/main/java/org/joget/apps/ext/ConsoleWebPlugin.java
+++ wflow-consoleweb/src/main/java/org/joget/apps/ext/ConsoleWebPlugin.java
@@ -111,7 +111,7 @@ public class ConsoleWebPlugin extends ExtDefaultPlugin implements PluginWebSuppo
         MessageSource messageSource = (MessageSource)AppUtil.getApplicationContext().getBean("messageSource");
         Locale locale = LocaleContextHolder.getLocale();
         String revision = messageSource.getMessage("console.footer.label.revision", null, "", locale);
-        String footer = "&copy; Joget DX - Joget Inc. All Rights Reserved. " + revision;
+        String footer = "&copy; Kecak Workflow - Joget Inc. All Rights Reserved. " + revision;
         return footer;
     }
     
