0d2e8d55b77ff2d7df1eef13d06bddf6eb428e25
owen <owen@joget.org>
2022-12-05 08:38:27 +0800

    Fixed: wflow-wfengine - Workflow Manager - Process completed checking run even when activity on created and running state. T1513 @7.0-SNAPSHOT

--- wflow-wfengine/src/main/java/org/joget/workflow/shark/WorkflowEventAuditManager.java
+++ wflow-wfengine/src/main/java/org/joget/workflow/shark/WorkflowEventAuditManager.java
@@ -32,7 +32,9 @@ public class WorkflowEventAuditManager implements EventAuditManagerInterface {
     }
 
     public void persist(WMSessionHandle arg0, StateEventAuditPersistenceObject arg1) throws EventAuditException {
-        ((WorkflowManager) WorkflowUtil.getApplicationContext().getBean("workflowManager")).internalRemoveProcessOnComplete(arg1.getProcessId());
+        if (arg1 != null && arg1.getNewState().contains("closed")) { //only need when activity status is closed
+            ((WorkflowManager) WorkflowUtil.getApplicationContext().getBean("workflowManager")).internalRemoveProcessOnComplete(arg1.getProcessId());
+        }
     }
 
     public boolean restore(WMSessionHandle arg0, AssignmentEventAuditPersistenceObject arg1) throws EventAuditException {
