32b9888579215ebeaea2faba3f288d4b7db265cb
owen <owen@joget.org>
2022-12-05 18:41:06 +0800

    Fixed: wflow-core - Date Picker - Date picker can't work with date limit when the date format is without day syntax. T1673 @7.0-SNAPSHOT

--- wflow-core/src/main/resources/resources/js/jquery.custom.datepicker.js
+++ wflow-core/src/main/resources/resources/js/jquery.custom.datepicker.js
@@ -4,6 +4,7 @@
             this.each(function(){
                 var element = $(this);
                 var elementParent = element.parent();
+                var value = $(element).val();
 
                 if (!/iPhone|iPod|iPad/.test(navigator.userAgent)) {
                     o.beforeShow = function(input, inst) {
@@ -163,6 +164,18 @@
                         $(element).next("img.ui-datepicker-trigger").wrap("<a class=\"trigger\" href=\"#\"></a>");
                     }
                 }
+                
+                if ((o.datePickerType !== "dateTime" && o.datePickerType !== "timeOnly") //only apply for datepicker
+                        && o.dateFormat.indexOf("d") === -1  //when the format is without a day syntax
+                        && value !== null && value !== undefined && value !== "") { //there is a default value
+                    //use setTimeout to make sure if there is set other field as min/max, the field are ready before setting this value
+                    setTimeout(function(){
+                        //set the value again with a first day of the month, 
+                        //just simple add the day syntax and 01 value and it seem to work with all formats.
+                        //it is ok to set a date over the min/max date limit, the field will auto adjust to the min/max date value if it is invalid/over the limit
+                        $(element).datepicker("setDate", $.datepicker.parseDate('dd/'+ o.dateFormat, '01/'+value));
+                    }, 1);
+                }
             });
         }
     });
@@ -171,6 +184,10 @@
         var value = $(element).val();
         if (value === "" && $(target).datetimepicker("option", type) === null) {
             return;
+        }
+        if ($.datepicker._getInst($(element)[0]) !== undefined) {
+            //use to make sure the value use as min/max are always a valid date if the element itself is a datapicker field
+            value = $(element).datepicker("getDate"); 
         }
         $(target).next(".trigger").remove();
         if (o.datePickerType === "dateTime") {
