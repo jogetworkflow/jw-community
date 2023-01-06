72585acf0d01d0cf59bfe0520931de82738d5afe
owen <owen@joget.org>
2023-01-06 20:30:02 +0800

    Fixed: wflow-consoleweb - UI Builder - Linked a delete style sheet. @8.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/js/ubuilder.core.js
+++ wflow-consoleweb/src/main/webapp/js/ubuilder.core.js
@@ -42,7 +42,6 @@ UserviewBuilder = {
             }
         }, function() {
             CustomBuilder.Builder.setHead('<link data-datalist-style href="' + CustomBuilder.contextPath + '/css/datalist8.css" rel="stylesheet" />');
-            CustomBuilder.Builder.setHead('<link data-datalist-card-style href="' + CustomBuilder.contextPath + '/css/datalist_cardlayout.css" rel="stylesheet" />');
             CustomBuilder.Builder.setHead('<link data-form-style href="' + CustomBuilder.contextPath + '/css/form8.css" rel="stylesheet" />');
             CustomBuilder.Builder.setHead('<link data-userview-style href="' + CustomBuilder.contextPath + '/css/userview8.css" rel="stylesheet" />');
             CustomBuilder.Builder.setHead('<link data-ubuilder-style href="' + CustomBuilder.contextPath + '/css/ubuilder.css" rel="stylesheet" />');
@@ -53,7 +52,6 @@ UserviewBuilder = {
             $("#iframe_screenshot").on("load", function(){
                 var frameHead = $(UserviewBuilder.screenshotFrame.contentWindow.document).find("head");
                 frameHead.append('<link data-datalist-style href="' + CustomBuilder.contextPath + '/css/datalist8.css" rel="stylesheet" />');
-                frameHead.append('<link data-datalist-card-style href="' + CustomBuilder.contextPath + '/css/datalist_cardlayout.css" rel="stylesheet" />');
                 frameHead.append('<link data-form-style href="' + CustomBuilder.contextPath + '/css/form8.css" rel="stylesheet" />');
                 frameHead.append('<link data-userview-style href="' + CustomBuilder.contextPath + '/css/userview8.css" rel="stylesheet" />');
                 frameHead.append('<link data-ubuilder-style href="' + CustomBuilder.contextPath + '/css/ubuilder.css" rel="stylesheet" />');
