c8f29626f8a8aa028d23f24a966b50040f62ff7b
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2023-01-24 16:17:50 +0700

    enhance SetupManager
    add backward compatibility for AuthTokenService

--- wflow-core/src/main/java/org/joget/apps/app/service/AuthTokenService.java
+++ wflow-core/src/main/java/org/joget/apps/app/service/AuthTokenService.java
@@ -2,6 +2,11 @@ package org.joget.apps.app.service;
 
 import org.joget.commons.util.SetupManager;
 
+/**
+ * {@link Deprecated}
+ *
+ * Use {@link org.kecak.apps.app.service.AuthTokenService}
+ */
 @Deprecated
 public class AuthTokenService extends org.kecak.apps.app.service.AuthTokenService {
     public AuthTokenService(SetupManager setupManager) {
