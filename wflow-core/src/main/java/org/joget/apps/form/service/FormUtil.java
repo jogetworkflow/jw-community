ff6d28751ea9e56d2b0fcf58a1f6bb8563fae263
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2023-01-27 19:36:54 +0700

    fix versioning

--- wflow-core/src/main/java/org/joget/apps/form/service/FormUtil.java
+++ wflow-core/src/main/java/org/joget/apps/form/service/FormUtil.java
@@ -133,7 +133,6 @@ public class FormUtil implements ApplicationContextAware {
     public static final String FORM_ERRORS_PARAM = "_FORM_ERRORS";
     public static final String FORM_RESULT_LOAD_ALL_DATA = "FORM_RESULT_LOAD_ALL_DATA";
     public static final String PROPERTY_ORG_ID = "orgId";
-
     public static final String PROPERTY_DELETED = "deleted";
     static ApplicationContext appContext;
     
