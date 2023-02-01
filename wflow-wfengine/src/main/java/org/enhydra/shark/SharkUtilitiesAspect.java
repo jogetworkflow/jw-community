jogetworkflow/jw-community

commit fb6a54893e7ccc9a87b88e570afffa10f19228eb

Modified: wflow-core, wflow-wfengine - Performance - Added caching to reduce repeated database calls within the same HTTP request @8.0-SNAPSHOT

wflow-wfengine/src/main/java/org/enhydra/shark/SharkUtilitiesAspect.java
@@ -299,4 +299,77 @@ public class SharkUtilitiesAspect {
 
         return object;
     }
+
+    @Pointcut("execution(* org.enhydra.shark.SharkUtilities.getProcess(..))")
+    private void getProcessMethod() {
+    }
+
+    @Around("org.enhydra.shark.SharkUtilitiesAspect.getProcessMethod()")
+    public Object getProcess(ProceedingJoinPoint pjp) throws Throwable {
+        Object[] args = pjp.getArgs();
+        WMSessionHandle shandle = (WMSessionHandle) args[0];
+        String processId = (String) args[1];
+        int mode = (int) args[2];
+        
+        String key = "processId_" + processId + "_" + mode;
+        Object result = WorkflowUtil.readRequestCache(key);
+        if (result != null) {
+            return result;
+        } else {
+            result = pjp.proceed();
+            if (result != null) {
+                WorkflowUtil.writeRequestCache(key, result);
+            }
+        }        
+        return result;
+    }
+
+    @Pointcut("execution(* org.enhydra.shark.SharkUtilities.getProcessMgr(..))")
+    private void getProcessMgrMethod() {
+    }
+
+    @Around("org.enhydra.shark.SharkUtilitiesAspect.getProcessMgrMethod()")
+    public Object getProcessMgr(ProceedingJoinPoint pjp) throws Throwable {
+        Object[] args = pjp.getArgs();
+        WMSessionHandle shandle = (WMSessionHandle) args[0];
+        String name = (String) args[1];
+
+        String key = "processMgr_" + name;
+        Object result = WorkflowUtil.readRequestCache(key);
+        if (result != null) {
+            return result;
+        } else {
+            result = pjp.proceed();
+            if (result != null) {
+                WorkflowUtil.writeRequestCache(key, result);
+            }
+        }        
+        return result;
+    }
+
+    @Pointcut("execution(* org.enhydra.shark.SharkUtilities.getActivity(..))")
+    private void getActivityMethod() {
+    }
+
+    @Around("org.enhydra.shark.SharkUtilitiesAspect.getActivityMethod()")
+    public Object getActivity(ProceedingJoinPoint pjp) throws Throwable {
+        Object[] args = pjp.getArgs();
+        WMSessionHandle shandle = (WMSessionHandle) args[0];
+        String processId = (String) args[1];
+        String activityId = (String) args[2];
+        int mode = (int) args[3];
+        
+        String key = "activityId_" + activityId + "_" + mode;
+        Object result = WorkflowUtil.readRequestCache(key);
+        if (result != null) {
+            return result;
+        } else {
+            result = pjp.proceed();
+            if (result != null) {
+                WorkflowUtil.writeRequestCache(key, result);
+            }
+        }        
+        return result;
+    }
+    
 }

wflow-core/src/main/java/org/joget/apps/workflow/security/EnhancedWorkflowUserManager.java
@@ -103,6 +103,13 @@ public class EnhancedWorkflowUserManager extends WorkflowUserManager {
     
     @Override
     public Collection<String> getCurrentRoles() {
+        String username = WorkflowUtil.getCurrentUsername();
+        String key = "userRole_" + username;
+        Collection<String> result = (Collection<String>)WorkflowUtil.readRequestCache(key);
+        if (result != null) {
+            return result;
+        }
+        
         Collection<String> roles = super.getCurrentRoles();
 
         // check for sys admin role, add if not in db
@@ -128,6 +135,8 @@ public class EnhancedWorkflowUserManager extends WorkflowUserManager {
             }
         }
         
+        WorkflowUtil.writeRequestCache(key, roles);
+
         return roles;
     }
 

wflow-wfengine/src/main/java/org/enhydra/shark/repositorypersistence/CustomDODSRepositoryPersistenceManager.java
@@ -12,6 +12,7 @@ import org.enhydra.shark.repositorypersistence.data.CustomXPDLDataQuery;
 import org.enhydra.shark.repositorypersistence.data.CustomXPDLQuery;
 import org.enhydra.shark.repositorypersistence.data.XPDLDO;
 import org.enhydra.shark.repositorypersistence.data.XPDLDataDO;
+import org.joget.workflow.util.WorkflowUtil;
 
 public class CustomDODSRepositoryPersistenceManager extends DODSRepositoryPersistenceManager {
 
@@ -163,6 +164,11 @@ public class CustomDODSRepositoryPersistenceManager extends DODSRepositoryPersis
         DBQuery dbQuery = null;
         ResultSet rs = null;
         try {
+            String key = "xpdlCurrentVersion_" + xpdlId;
+            result = (String)WorkflowUtil.readRequestCache(key);
+            if (result != null) {
+                return result;
+            }
             
             DBTransaction dbt = getDBTransaction();
             CustomXPDLQuery query = new CustomXPDLQuery(dbt);
@@ -181,6 +187,8 @@ public class CustomDODSRepositoryPersistenceManager extends DODSRepositoryPersis
                 }
             }
             query.getQueryBuilder().close();
+            
+            WorkflowUtil.writeRequestCache(key, result);
         } catch (Exception ex) {
             
         } finally {

wflow-wfengine/src/main/java/org/joget/workflow/util/WorkflowUtil.java
@@ -379,13 +379,20 @@ public class WorkflowUtil implements ApplicationContextAware {
      * @return 
      */
     public static Map<String, Collection<String>> getReplacementUsers(String username) {
+        String key = "replacementUsers_" + username;
+        Map<String, Collection<String>> result = (Map<String, Collection<String>>)WorkflowUtil.readRequestCache(key);
+        if (result != null) {
+            return result;
+        }
+
         try {
             WorkflowHelper workflowMapper = (WorkflowHelper) appContext.getBean("workflowHelper");
-            return workflowMapper.getReplacementUsers(username);
+            result = workflowMapper.getReplacementUsers(username);
+            WorkflowUtil.writeRequestCache(key, result);
         } catch (Exception e) {
             LogUtil.error(WorkflowUtil.class.getName(), e, "Error retrieve absence users replaced by" + username);
         }
-        return null;
+        return result;
     }
     
     /**
@@ -416,4 +423,26 @@ public class WorkflowUtil implements ApplicationContextAware {
             FileStore.updateFileSizeLimit();
         }
     }
+    
+    public static final String REQUEST_CACHE_KEY_PREFIX = "RequestCacheKey_";
+    
+    public static Object readRequestCache(String key) {
+        Object cachedObject = null;
+        HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
+        if (request != null) {
+            String attributeName = REQUEST_CACHE_KEY_PREFIX + key;
+            cachedObject = request.getAttribute(attributeName);            
+        }
+        return cachedObject;
+    }
+    
+    public static void writeRequestCache(String key, Object value) {
+        HttpServletRequest request = WorkflowUtil.getHttpServletRequest();
+        if (request != null) {
+            String attributeName = REQUEST_CACHE_KEY_PREFIX + key;
+            request.setAttribute(attributeName, value);            
+        }        
+    }
+    
+    
 }

commit ba3c630f557037a0bc426d2ceb464bc02c1d29b5

Fixed: wflow-wfengine - Regression - Outdated process and activity data retrieved from cache caused by fb6a5489 @8.0-SNAPSHOT

wflow-wfengine/src/main/java/org/enhydra/shark/SharkUtilitiesAspect.java
@@ -320,15 +320,18 @@ public class SharkUtilitiesAspect {
         String processId = (String) args[1];
         int mode = (int) args[2];
         
-        String key = "processId_" + processId + "_" + mode;
+        String key = "processId_" + processId;
+        if (mode == 1) {
+            // write mode, clear cache and proceed without caching
+            WorkflowUtil.writeRequestCache(key, null);
+            return pjp.proceed();
+        }
         Object result = WorkflowUtil.readRequestCache(key);
         if (result != null) {
             return result;
         } else {
             result = pjp.proceed();
-            if (result != null) {
-                WorkflowUtil.writeRequestCache(key, result);
-            }
+            WorkflowUtil.writeRequestCache(key, result);
         }        
         return result;
     }
@@ -368,15 +371,18 @@ public class SharkUtilitiesAspect {
         String activityId = (String) args[2];
         int mode = (int) args[3];
         
-        String key = "activityId_" + activityId + "_" + mode;
+        String key = "activityId_" + activityId;
+        if (mode == 1) {
+            // write mode, clear cache and proceed without caching
+            WorkflowUtil.writeRequestCache(key, null);
+            return pjp.proceed();
+        }
         Object result = WorkflowUtil.readRequestCache(key);
         if (result != null) {
             return result;
         } else {
             result = pjp.proceed();
-            if (result != null) {
-                WorkflowUtil.writeRequestCache(key, result);
-            }
+            WorkflowUtil.writeRequestCache(key, result);
         }        
         return result;
     }