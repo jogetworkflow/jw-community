e52dae0517ee3309b639210ca59eca48e2de1b35
owen <owen@joget.org>
2023-05-07 15:05:56 +0800

    Fixed: wflow-consoleweb - Plugin Resources - "charset=UTF-8" should not add to content-type of binary file. @8.0-SNAPSHOT

---
+++ wflow-consoleweb/src/main/java/org/joget/apps/app/web/CustomCharacterEncodingFilter.java
@@ -0,0 +1,45 @@
+package org.joget.apps.app.web;
+
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+public class CustomCharacterEncodingFilter extends CharacterEncodingFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String url = request.getRequestURL().toString();
+        
+        //to support old url ends with .
+        if (url.endsWith(".")) {
+            url = url.substring(0, url.length() - 1);
+        }
+        
+        //get last part of url
+        if (url.contains("/")) {
+            url = url.substring(url.lastIndexOf("/"));
+        }
+        if (url.contains(".") && !(url.endsWith(".js") || url.endsWith(".css") ||
+                url.endsWith(".html") || url.endsWith(".txt") || url.endsWith(".xml") ||
+                url.endsWith(".json"))) { 
+            //possible an application file
+            String ct = request.getSession().getServletContext().getMimeType(url);
+            if (ct != null && ct.startsWith("application/") && 
+                    !(ct.contains("xml") || ct.contains("json") || 
+                    ct.contains("javascript") || ct.contains("yaml"))) {
+                //do not set the Character Encoding
+                filterChain.doFilter(request, response);
+            } else {
+                super.doFilterInternal(request, response, filterChain);
+            }
+        } else {
+            super.doFilterInternal(request, response, filterChain);
+        }
+    }
+}
