2c9c58b260f9aff34a9c97c9bb8e0b75c1558d8f
owen <owen@joget.org>
2023-06-25 11:07:42 +0800

    Modified: jw-community - Update AspectJ to 1.9.7. @7.1-SNAPSHOT

--- wflow-install/src/main/resources/tomcat8.sh
+++ wflow-install/src/main/resources/tomcat8.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export JAVA_OPTS="-XX:MaxPermSize=128m -Xmx768M -Dwflow.home=./wflow/ -javaagent:./wflow/aspectjweaver-1.8.5.jar"
+export JAVA_OPTS="-XX:MaxPermSize=128m -Xmx768M -Dwflow.home=./wflow/ -javaagent:./wflow/aspectjweaver-1.9.7.jar"
 
 #For Java 9
-#export JAVA_OPTS="-XX:MaxPermSize=128m -Xmx768M -Dwflow.home=./wflow/ -javaagent:./wflow/aspectjweaver-1.8.5.jar --add-modules java.se.ee"
+#export JAVA_OPTS="-XX:MaxPermSize=128m -Xmx768M -Dwflow.home=./wflow/ -javaagent:./wflow/aspectjweaver-1.9.7.jar --add-modules java.se.ee"
 
 apache-tomcat-9.0.71/bin/catalina.sh $*
  
