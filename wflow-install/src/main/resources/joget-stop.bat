a85c7a41714aeaeeb769ed4beb621cdd6a866184
owen <owen@joget.org>
2023-07-12 21:37:15 +0800

    Modified: jw-community - Update Apache Tomcat & Apache Tomcat JDBC to 9.0.76. @7.1-SNAPSHOT

--- wflow-install/src/main/resources/joget-stop.bat
+++ wflow-install/src/main/resources/joget-stop.bat
@@ -8,7 +8,7 @@ ECHO.
 
 REM Stop Tomcat
 set JAVA_HOME=.\jre11.0.13
-set CATALINA_HOME=.\apache-tomcat-9.0.74
+set CATALINA_HOME=.\apache-tomcat-9.0.76
 ECHO == Stopping Tomcat from %CATALINA_HOME% ==
 ECHO.
 %CATALINA_HOME%\bin\shutdown.bat
--- wflow-install/src/main/resources/tomcat8-stop.bat
+++ wflow-install/src/main/resources/tomcat8-stop.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=.\jre11.0.13
-set CATALINA_HOME=.\apache-tomcat-9.0.74
+set CATALINA_HOME=.\apache-tomcat-9.0.76
 
 %CATALINA_HOME%\bin\shutdown.bat
 
