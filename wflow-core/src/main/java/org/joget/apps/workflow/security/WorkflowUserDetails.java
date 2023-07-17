30aec1ef4d377e601573c6d8c5748bce21b651b9
owen <owen@joget.org>
2023-07-17 09:31:42 +0800

    Fixed: wflow-directory - Reduce unnecessary calls to retrieve user & role data. 7.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/workflow/security/WorkflowUserDetails.java
+++ wflow-core/src/main/java/org/joget/apps/workflow/security/WorkflowUserDetails.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+import org.joget.directory.model.service.ExtUserDetails;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-public class WorkflowUserDetails implements UserDetails {
+public class WorkflowUserDetails implements ExtUserDetails {
 
     private User user;
     private Collection<GrantedAuthority> authorities = null;
