98cdd4a067974d20f61d36b7df1060f2d9dcefef
owen <owen@joget.org>
2023-10-22 21:32:27 +0800

    Fixed: wflow-core - Naming Convention Health Check - Prevent parsing empty string to JSON object. @8.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/governance/lib/NamingConventionCheck.java
+++ wflow-core/src/main/java/org/joget/governance/lib/NamingConventionCheck.java
@@ -108,7 +108,7 @@ public class NamingConventionCheck extends GovHealthCheckAbstract implements Gov
                 
                 // get JSON
                 String json = formDef.getJson();
-                if (json != null) {
+                if (json != null && !json.isEmpty()) {
                     try {
                         Form form = (Form) formService.createElementFromJson(json, false);
                         Collection<String> tempColumnList = new HashSet<String>();
