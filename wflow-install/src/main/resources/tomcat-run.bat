5e1970fbc31f2c34b7d17de0b025e42e4d122022
owen <owen@joget.org>
2024-03-14 15:55:08 +0800

    Fixed: jw-community - Security - Upgrade Apache Tomcat to 9.0.86. @8.0-SNAPSHOT

--- wflow-install/src/main/resources/tomcat-run.bat
+++ wflow-install/src/main/resources/tomcat-run.bat
@@ -1,7 +1,7 @@
 chcp 65001
 
 set JAVA_HOME=.\jre11.0.18
-set CATALINA_HOME=.\apache-tomcat-9.0.85
+set CATALINA_HOME=.\apache-tomcat-9.0.86
 
 set JAVA_OPTS=-Xmx768M -Dfile.encoding=UTF-8 -Dwflow.home=./wflow/
 REM set JAVA_OPTS=-Xmx1024M -Dfile.encoding=UTF-8 -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/
