03bf13e82700a42954bf9a97eb43ba5be81d5531
irfnfhmi <irfan.fahmi@joget.com>
2024-04-25 13:59:06 +0800

    Modified: wflow-consoleweb - UI Builder - Modified hide label field on page component. @8.1-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/js/ubuilder.core.js
+++ wflow-consoleweb/src/main/webapp/js/ubuilder.core.js
@@ -974,23 +974,45 @@ UserviewBuilder = {
                 if (elementObj.properties.label === undefined) {
                     elementObj.properties.label = component.label;
                 }
-                
+                UserviewBuilder.updateLabelProperty(component);
                 UserviewBuilder.renderElementAjax(element, elementObj, component, callback, "menu");
                 if (UserviewBuilder.selectedMenu !== null && UserviewBuilder.selectedMenu.properties.id === elementObj.properties.id) {
                     UserviewBuilder.showMenuSnapshot();
                 }
             }
-        } else {
+        } else { 
             if (elementObj.className === "org.joget.apps.userview.model.UserviewPage") {
                 UserviewBuilder.renderUserviewPage(element, elementObj, component, callback);
             } else if (elementObj.className === "menu-component") {
                 UserviewBuilder.renderMenuComponent(element, elementObj, component, callback);
             } else {
+                // Hide label field if UserviewBuilder mode is "page"
+                if (typeof UserviewBuilder !== "undefined" && UserviewBuilder.mode === "page") {
+                    UserviewBuilder.updateLabelProperty(component);
+                }
                 UserviewBuilder.renderElementAjax(element, elementObj, component, callback, "component");
             }
         }
     },
     
+    /*
+     * Hide label on page component
+     */
+    updateLabelProperty : function (component) {
+        for (var i in component.propertyOptions) {
+            for (var r in component.propertyOptions[i].properties) {         
+                if (component.propertyOptions[i].properties[r].name === "label") {
+                    if (UserviewBuilder.mode === "userview") {
+                        component.propertyOptions[i].properties[r].type = 'icon-textfield';
+                    } else {
+                        component.propertyOptions[i].properties[r].type = 'hidden';
+                    }
+                    break;
+                }
+            }
+        }
+    },
+
     /*
      * Retrieve element template using ajax and render element. Called from UserviewBuilder.renderElement
      */
