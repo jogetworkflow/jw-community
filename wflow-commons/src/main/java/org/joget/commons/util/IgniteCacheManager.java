3fd933c51f1fa3e4593027651680c1a726478aa4
julian <julian@joget.org>
2024-05-09 13:16:55 +0800

    Modified: wflow-commons, wflow-consoleweb - Ignite - Added options for replicated mode, asynchronous cache operations and cluster discovery using Kubernetes or static IP @8.0-SNAPSHOT

--- wflow-commons/src/main/java/org/joget/commons/util/IgniteCacheManager.java
+++ wflow-commons/src/main/java/org/joget/commons/util/IgniteCacheManager.java
@@ -1,11 +1,18 @@
 package org.joget.commons.util;
 
+import java.util.Arrays;
 import java.util.Collection;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cluster.ClusterState;
+import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.kubernetes.configuration.KubernetesConnectionConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.kubernetes.TcpDiscoveryKubernetesIpFinder;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 import org.hibernate.cache.spi.RegionFactory;
 import org.hibernate.query.Query;
 
@@ -16,6 +23,11 @@ import org.hibernate.query.Query;
 public class IgniteCacheManager {
     
     public static final String SYSTEM_PROPERTY_IGNITE_CACHE = "wflow.ignite";
+    public static final String SYSTEM_PROPERTY_IGNITE_K8S_NAMESPACE = "wflow.igniteK8sNamespace";
+    public static final String SYSTEM_PROPERTY_IGNITE_K8S_SERVICE = "wflow.igniteK8sService";
+    public static final String SYSTEM_PROPERTY_IGNITE_STATIC_IP = "wflow.igniteStaticIp";
+    public static final String SYSTEM_PROPERTY_IGNITE_REPLICATED = "wflow.igniteReplicated";
+    public static final String SYSTEM_PROPERTY_IGNITE_MAX_ASYNC = "wflow.igniteMaxAsync";
     private static boolean started = false;
     static IgniteConfiguration igniteCfg;
 
@@ -24,18 +36,73 @@ public class IgniteCacheManager {
     }
     
     public IgniteCacheManager(IgniteConfiguration igniteCfg) {
+        initIgniteCache(igniteCfg);
+    }
+
+    /**
+     * Initialize the Ignite cache based on the IgniteConfiguration.
+     * If within a Kubernetes environment and system properties 
+     * SYSTEM_PROPERTY_IGNITE_K8S_NAMESPACE and SYSTEM_PROPERTY_IGNITE_K8S_SERVICE are defined, 
+     * will run as a client node and KubernetesConfiguration will be used.
+     * If the system property SYSTEM_PROPERTY_IGNITE_STATIC_IP is defined, 
+     * will run as a client node static IP discovery will be used.
+     * Otherwise, it will run embedded as a server node.
+     * @param igniteCfg 
+     */
+    public static void initIgniteCache(IgniteConfiguration igniteCfg) {
         if (!HostManager.isVirtualHostEnabled() && IgniteCacheManager.isIgniteCacheEnabled()) {
             // set ignite work directory
             String workDirectory = SetupManager.getBaseDirectory() + "/ignite/work";
-            igniteCfg.setWorkDirectory(workDirectory);            
-            Ignition.setClientMode(false);
+            igniteCfg.setWorkDirectory(workDirectory);
+            
+            // detect environment
+            boolean isKubernetesEnv = System.getenv("KUBERNETES_SERVICE_HOST") != null;
+            String k8sNamespace = System.getProperty(SYSTEM_PROPERTY_IGNITE_K8S_NAMESPACE);
+            String k8sService = System.getProperty(SYSTEM_PROPERTY_IGNITE_K8S_SERVICE);
+            String staticIp = System.getProperty(SYSTEM_PROPERTY_IGNITE_STATIC_IP);
+            if (isKubernetesEnv && k8sNamespace != null && k8sService != null) {
+                // get k8s namespace and service name
+                KubernetesConnectionConfiguration k8sConfig = new KubernetesConnectionConfiguration();
+                k8sConfig.setNamespace(k8sNamespace);
+                k8sConfig.setServiceName(k8sService);
+
+                // set k8s ip finder
+                TcpDiscoveryKubernetesIpFinder k8sIpFinder = new TcpDiscoveryKubernetesIpFinder(k8sConfig);
+                TcpDiscoverySpi discoverySpi = new TcpDiscoverySpi();
+                discoverySpi.setIpFinder(k8sIpFinder);
+                igniteCfg.setDiscoverySpi(discoverySpi);
+
+                // set client mode
+                igniteCfg.setClientMode(true);
+                Ignition.setClientMode(true);
+                LogUtil.info(IgniteCacheManager.class.getName(), "Running Ignite client node to cluster in Kubernetes namespace " + k8sNamespace + ", service name " + k8sService);
+            } else if (staticIp != null) {
+                // use static ip
+                Collection<String> addresses = Arrays.asList(staticIp.split(",", -1));
+                TcpDiscoveryVmIpFinder staticIpFinder = new TcpDiscoveryVmIpFinder();
+                staticIpFinder.setAddresses(addresses);
+                TcpDiscoverySpi discoverySpi = new TcpDiscoverySpi();
+                discoverySpi.setIpFinder(staticIpFinder);
+                igniteCfg.setDiscoverySpi(discoverySpi);
+
+                // set client mode
+                igniteCfg.setClientMode(true);
+                Ignition.setClientMode(true);
+                LogUtil.info(IgniteCacheManager.class.getName(), "Running Ignite client node to cluster using static IP " + staticIp);
+            } else {
+                // use embedded mode
+                Ignition.setClientMode(false);
+                LogUtil.info(IgniteCacheManager.class.getName(), "Running Ignite server node in embedded mode");
+            }
             IgniteCacheManager.igniteCfg = igniteCfg;            
+            
+            // start cluster
             Ignite ignite = Ignition.getOrStart(igniteCfg);
             ignite.cluster().state(ClusterState.ACTIVE);
             started = true;
         }
     }
-
+    
     /**
      * Checks to see whether the Ignite cache is enabled.
      * @return true if the cache is enabled.
@@ -103,4 +170,46 @@ public class IgniteCacheManager {
         query.setCacheRegion(cacheRegionName);
         return query;
     }
+    
+    /**
+     * Sets the Ignite cache mode based on configured system property.
+     * @param cacheConfig 
+     */
+    public static void setCacheMode(CacheConfiguration cacheConfig) {
+        // set cache mode i.e. PARTITIONED or REPLICATED
+        boolean useReplicatedCacheMode = Boolean.parseBoolean(System.getProperty(SYSTEM_PROPERTY_IGNITE_REPLICATED));
+        if (useReplicatedCacheMode) {
+            cacheConfig.setCacheMode(CacheMode.REPLICATED);
+        }
+        // set max concurrent asynchronous operations
+        int maxAsync = CacheConfiguration.DFLT_MAX_CONCURRENT_ASYNC_OPS;
+        String maxAsyncStr = System.getProperty(SYSTEM_PROPERTY_IGNITE_MAX_ASYNC);
+        if (maxAsyncStr != null) {
+            try {
+                maxAsync = Integer.parseInt(maxAsyncStr);
+            } catch(NumberFormatException e) {
+                // ignore
+            }
+        }
+        cacheConfig.setMaxConcurrentAsyncOperations(maxAsync);
+    }
+
+    /**
+     * Return the timeout for asynchronous cache gets and puts in milliseconds.
+     * Configurable via system property wflow.igniteAsyncTimeout.
+     * @return
+     */
+    public static long getConfigAsyncTimeout() {
+        long timeout = 0L;
+        String timeoutStr = System.getProperty(IgniteJdbcCacheManager.SYSTEM_PROPERTY_IGNITE_CACHE_ASYNC_TIMEOUT);
+        if (timeoutStr != null) {
+            try {
+                timeout = Long.parseLong(timeoutStr);
+            } catch (NumberFormatException e) {
+                // ignore
+            }
+        }
+        return timeout;
+    }
+    
 }
