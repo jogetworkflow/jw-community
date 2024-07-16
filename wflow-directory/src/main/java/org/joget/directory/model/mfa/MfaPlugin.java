ece43160e9356ccdfc17dd90c5adcef70d129694
chunzhe <chunzhe.chee@joget.com>
2024-07-16 14:28:23 +0800

    Modified: wflow-enterprise-plugins - MFA Plugin API - Included HttpServletRequest parameter in login and register methods to allow more flexibility when developing plugins.

--- wflow-directory/src/main/java/org/joget/directory/model/mfa/MfaPlugin.java
+++ wflow-directory/src/main/java/org/joget/directory/model/mfa/MfaPlugin.java
@@ -2,32 +2,36 @@ package org.joget.directory.model.mfa;
 
 import org.joget.directory.model.mfa.exception.MfaExecutionException;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
 public interface MfaPlugin {
 
     /**
      * Process MFA login request.
      * This method will be called by the MfaManager to delegate MFA login to the plugin.
      *
-     * @param username username of the Joget user
-     * @param data     the data submitted by the form
+     * @param username the username of the request user
+     * @param request  the HTTP request of the current session
      * @param secret   the secret (if applicable) for the plugin to validate the data with
-     * @return true if the validation passes; false if it fails
+     * @return true if MFA verification success; false if undefined error. Consider throwing {@link MfaExecutionException}
+     * with custom message if error.
      * @throws MfaExecutionException when the processing fails such as invalid data, secret, etc.
      *                               The exception message will be shown in a banner above the MFA login page.
      */
-    boolean login(String username, String data, String secret) throws MfaExecutionException;
+    boolean login(String username, HttpServletRequest request, String secret) throws MfaExecutionException;
 
     /**
      * Process MFA registration request.
      * This method will be called by the MfaManager to delegate MFA registration to the plugin.
      *
-     * @param username username of the Joget user
-     * @param data     the data submitted by the form
+     * @param username the username of the request user
+     * @param request  the HTTP request of the current session
      * @return string representation of secret associated with username to be saved to database for future login calls
      * @throws MfaExecutionException when the processing fails such as invalid username, data, etc.
      *                               The exception message will be shown in a banner above the MFA register page.
      */
-    String register(String username, String data) throws MfaExecutionException;
+    String register(String username, HttpServletRequest request) throws MfaExecutionException;
 
     /**
      * Gets the (friendly) display name of the plugin. Shown in MFA list and user profile.
@@ -39,14 +43,22 @@ public interface MfaPlugin {
     /**
      * Get the login HTML of this plugin to be shown to the user
      *
+     * @param username the username of the request user
+     * @param request  the HTTP request of the current session
      * @return the HTML in string
+     * @throws MfaExecutionException when a fatal error occurs such as processing fails, etc.
+     *                               The exception message will be shown in a banner above the MFA register page.
      */
-    String getLoginTemplate();
+    String getLoginTemplate(String username, HttpServletRequest request) throws MfaExecutionException;
 
     /**
      * Get the register HTML of this plugin to be shown to the user
      *
+     * @param username the username of the request user
+     * @param request  the HTTP request of the current session
      * @return the HTML in string
+     * @throws MfaExecutionException when a fatal error occurs such as processing fails, etc.
+     *                               The exception message will be shown in a banner above the MFA register page.
      */
-    String getRegisterTemplate();
+    String getRegisterTemplate(String username, HttpServletRequest request) throws MfaExecutionException;
 }
