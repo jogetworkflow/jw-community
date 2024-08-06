e1ca6becf7297c064b4ade08372799225667c3f0
SeanChristanto <sean.christanto@joget.com>
2024-08-06 11:57:37 +0700

    Modified: wflow-consoleweb - DX8 Color Admin - Added default colors for sidebar and header @8.1-SNAPSHOT

--- wflow-core/src/main/java/org/joget/plugin/enterprise/Dx8ColorAdminTheme.java
+++ wflow-core/src/main/java/org/joget/plugin/enterprise/Dx8ColorAdminTheme.java
@@ -45,4 +45,26 @@ public class Dx8ColorAdminTheme extends AjaxUniversalTheme {
     public String getPropertyOptions() {
         return AppUtil.readPluginResource(getClass().getName(), "/properties/userview/dx8ColorAdminTheme.json", null, true, null);
     }
+
+    @Override
+    protected String generateColorDesign() {
+        String color = "--header-default-bgcolor:#ffffff;";
+        color += "--header-button-default-color: #20252a;";
+        color += "--header-link-default-color: #20252a;";
+
+        //For sidebar
+        color += "--sidebar-default-bgcolor:#2D353C;";
+        color += "--sidebar-menu-default-bgcolor:#2D353C;";
+        color += "--sidebar-menu-default-color:#FFFFFF;";
+        color += "--sidebar-icon-default-color:#ffffff;";
+
+        color += "--sidebar-active-link-default-bgcolor:#20252a;";
+        color += "--sidebar-active-link-default-color:#FFFFFF;";
+        color += "--sidebar-active-icon-default-color:#00ACAC;";
+        color += "--sidebar-badge-default-bgcolor: #00ACAC;";
+        color += "--sidebar-badge-default-color: #FFFFFF;";
+        color += "--sidebar-scrollbar-thumb-default-color: #999999;";
+
+        return color;
+    }
 }
