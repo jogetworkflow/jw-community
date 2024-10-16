b1b8cf5a0c11ae002d58f6231b257445289c3ed3
julian <julian@joget.org>
2024-10-16 16:02:15 +0800

    Fixed: wflow-plugin-base, wflow-consoleweb - System Logs - Upgrade to Jakarta WebSocket 2.1 for Jakarta EE 10 @9.0-SNAPSHOT

--- wflow-consoleweb/src/main/java/org/joget/apps/app/web/WebSocketPluginConfigurator.java
+++ wflow-consoleweb/src/main/java/org/joget/apps/app/web/WebSocketPluginConfigurator.java
@@ -1,9 +1,9 @@
 package org.joget.apps.app.web;
 
 import jakarta.servlet.http.HttpSession;
-import javax.websocket.HandshakeResponse;
-import javax.websocket.server.HandshakeRequest;
-import javax.websocket.server.ServerEndpointConfig;
+import jakarta.websocket.HandshakeResponse;
+import jakarta.websocket.server.HandshakeRequest;
+import jakarta.websocket.server.ServerEndpointConfig;
 
 /**
  * Configurator for plugin websocket endpoint to make sure httpSession is available
