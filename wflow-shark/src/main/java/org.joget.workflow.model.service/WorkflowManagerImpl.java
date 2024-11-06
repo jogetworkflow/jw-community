9a50e1610b2e5580ca6807212a769bdedcbf4311
julian <julian@joget.org>
2024-11-06 15:26:10 +0800

    Fixed: wflow-core - Inbox - Error retrieving inbox data "Cannot compare left expression of type 'java.lang.Boolean' with right expression of type 'java.lang.Integer'" after Hibernate 6.6 upgrade @9.0-SNAPSHOT

--- wflow-shark/src/main/java/org.joget.workflow.model.service/WorkflowManagerImpl.java
+++ wflow-shark/src/main/java/org.joget.workflow.model.service/WorkflowManagerImpl.java
@@ -6648,11 +6648,7 @@ public class WorkflowManagerImpl implements WorkflowManager {
             }
         }
 
-        if (driver.equals("org.postgresql.Driver")) {
-            conds += " AND ass.IsValid IS TRUE";
-        } else {
-            conds += " AND ass.IsValid = 1";
-        }
+        conds += " AND ass.IsValid IS TRUE";
 
         queryObj.setOperator("AND");
         queryObj.setQuery(conds);
