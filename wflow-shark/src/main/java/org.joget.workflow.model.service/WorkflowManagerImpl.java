dec9501b3868a1509ff1d629a20a6c7c0dcf84dc
julian <julian@joget.org>
2024-11-07 17:16:40 +0800

    Fixed: wflow-shark - Process Archiving - WorkflowManagerImpl throws "jakarta.persistence.EntityExistsException: A different object with the same identifier value was already associated with the session" @9.0-SNAPSHOT

--- wflow-shark/src/main/java/org.joget.workflow.model.service/WorkflowManagerImpl.java
+++ wflow-shark/src/main/java/org.joget.workflow.model.service/WorkflowManagerImpl.java
@@ -6468,6 +6468,10 @@ public class WorkflowManagerImpl implements WorkflowManager {
                     }
                 
                     if ("STARTED".equals(statusObj.getString("state"))) {
+                        //retrieve the latest status
+                        result = setupDao.find("WHERE property = ?", new String[]{WorkflowManager.ARCHIVE_SETTING}, null, null, null, null);
+                        status = (result.isEmpty()) ? null : result.iterator().next();
+                        
                         SharkConnection sc = null;
                         try {
                             sc = connect();
@@ -6498,9 +6502,6 @@ public class WorkflowManagerImpl implements WorkflowManager {
                                 ea.deleteProcesses(sessionHandle, pIds.toArray(new String[0]));
                                 LogUtil.debug(WorkflowManagerImpl.class.getName(), "Migrated " + pIds.size() + " processes. " + pIds.toString());
                                 
-                                //retrieve the value again to update completed count, in case there is status changed
-                                result = setupDao.find("WHERE property = ?", new String[]{WorkflowManager.ARCHIVE_SETTING}, null, null, null, null);
-                                status = (result.isEmpty()) ? null : result.iterator().next();
                                 try {
                                     statusObj = new JSONObject(status.getValue());
                                 } catch (Exception e) {
