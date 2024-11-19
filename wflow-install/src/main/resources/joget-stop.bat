da57562d8c8fa8c313ab961e8bbe37511ce1a3dd
owen <owen@joget.org>
2024-11-19 12:22:01 +0800

    Fixed: jw-community - Security - Update Apache Tomcat to 11.0.1 for CVE-2024-52318. @9.0-SNAPSHOT

--- wflow-install/src/main/resources/joget-stop.bat
+++ wflow-install/src/main/resources/joget-stop.bat
@@ -8,7 +8,7 @@ ECHO.
 
 REM Stop Tomcat
 set JAVA_HOME=.\jre21.0.5
-set CATALINA_HOME=.\apache-tomcat-11.0.0
+set CATALINA_HOME=.\apache-tomcat-11.0.1
 ECHO == Stopping Tomcat from %CATALINA_HOME% ==
 ECHO.
 %CATALINA_HOME%\bin\shutdown.bat
--- wflow-install/src/main/resources/tomcat-stop.bat
+++ wflow-install/src/main/resources/tomcat-stop.bat
@@ -1,5 +1,5 @@
 set JAVA_HOME=.\jre21.0.5
-set CATALINA_HOME=.\apache-tomcat-11.0.0
+set CATALINA_HOME=.\apache-tomcat-11.0.1
 
 %CATALINA_HOME%\bin\shutdown.bat
 
