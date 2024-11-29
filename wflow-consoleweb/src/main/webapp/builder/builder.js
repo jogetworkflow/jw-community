614d75b7d5476e5ef65befa7baced517db0880f8
SeanChristanto <sean.christanto@joget.com>
2024-11-29 15:42:20 +0700

    Modified: wflow-consoleweb - Admin UI Revamp - Modified how the drag icon and its style @9.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/builder/builder.js
+++ wflow-consoleweb/src/main/webapp/builder/builder.js
@@ -3659,7 +3659,7 @@ _CustomBuilder = {
         $("#builder-quick-nav").prepend('<div class="backToApp"><a class="builder-link" href="'+CustomBuilder.contextPath+'/web/console/app'+CustomBuilder.appPath+'/builders"  target="_self" title="'+get_cbuilder_msg("abuilder.title")+'"><i class="far fa-edit"></i></a></div>');
         
         $("#builder-quick-nav div#dragIcon").remove();
-        $("<div id='dragIcon' style='text-align:center;width:35px;' title='"+get_cbuilder_msg("abuilder.dragTooltipMessage")+"'><i class='fas fa-grip-vertical'></i></div>").insertAfter("#builder-quick-nav .backToApp");
+        $("<div id='dragIcon' style='text-align:center;width:35px;font-size:24px;color:#aaa' title='"+get_cbuilder_msg("abuilder.dragTooltipMessage")+"'><i class='las la-braille'></i></div>").insertAfter("#builder-quick-nav .backToApp");
 
         CustomBuilder.builderTypes = [];
         for (var i in data) {
