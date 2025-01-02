ad35784638f938b533e24719b810f8b40ae30d4d
julian <julian@joget.org>
2025-01-02 19:49:48 +0800

    Modified: wflow-core - In-Memory Cache - Hibernate L2 cache not working for AppDefinition collections @9.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/app/dao/AbstractVersionedObjectDao.java
+++ wflow-core/src/main/java/org/joget/apps/app/dao/AbstractVersionedObjectDao.java
@@ -25,9 +25,6 @@ public abstract class AbstractVersionedObjectDao<T extends AbstractVersionedObje
     public T loadByUid(T uid) {
         Session session = findSession();
         T result = (T) session.get(getEntityName(), uid);
-        if (result != null) {
-            session.refresh(result);
-        }
         return result;
     }
 
@@ -41,9 +38,6 @@ public abstract class AbstractVersionedObjectDao<T extends AbstractVersionedObje
         Collection<T> versions = findLatestVersions(id, null, null, null, null, 0, 1);
         if (versions != null && !versions.isEmpty()) {
             result = versions.iterator().next();
-        }
-        if (result != null) {
-            findSession().refresh(result);
         }
         return result;
     }
@@ -74,9 +68,6 @@ public abstract class AbstractVersionedObjectDao<T extends AbstractVersionedObje
             if (resultList != null && !resultList.isEmpty()) {
                 result = resultList.iterator().next();
             }
-        }
-        if (result != null) {
-            findSession().refresh(result);
         }
         return result;
     }
