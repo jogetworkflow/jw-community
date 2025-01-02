0639bcd31b5a57330d388dbf55cc86a54d663fa7
julian <julian@joget.org>
2025-01-02 19:50:24 +0800

    Modified: wflow-shark - In-Memory Cache - Hibernate L2 cache mistakenly cleared when calling WorkflowManager inbox methods @9.0-SNAPSHOT

--- wflow-shark/src/main/java/org/enhydra/shark/SharkUtilitiesAspect.java
+++ wflow-shark/src/main/java/org/enhydra/shark/SharkUtilitiesAspect.java
@@ -474,7 +474,7 @@ public class SharkUtilitiesAspect {
         }
     }
         
-    @Pointcut("execution(* org.joget.workflow.model.service.WorkflowManager.*(..)) && !(execution(* org.joget..*.get*(..)) || execution(* org.joget..*.set*(..)) || execution(* org.joget..*.is*(..)) || execution(* org.joget..*.internal*(..)) || execution(* org.joget..*.*connect*(..)) || execution(* org.joget..*.*Variable*(..)))")
+    @Pointcut("execution(* org.joget.workflow.model.service.WorkflowManager.*(..)) && !(execution(* org.joget..*.get*(..)) || execution(* org.joget..*.set*(..)) || execution(* org.joget..*.is*(..)) || execution(* org.joget..*.internal*(..)) || execution(* org.joget..*.*connect*(..)) || execution(* org.joget..*.*Variable*(..)) || execution(* org.joget..*.*Inbox*(..)))")
     private void clearWorkflowSessionFactoryCacheMethods() {
     }
 
