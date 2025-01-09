ae11d26ddd212c7c8ce013c22cc21c62ddf147a5
julian <julian@joget.org>
2025-01-09 16:18:37 +0800

    Fixed: wflow-core, wflow-consoleweb - App Composer - Newly created builder items do not appear @9.0-SNAPSHOT

--- wflow-consoleweb/src/main/java/org/joget/apps/app/controller/ConsoleWebController.java
+++ wflow-consoleweb/src/main/java/org/joget/apps/app/controller/ConsoleWebController.java
@@ -3749,7 +3749,7 @@ public class ConsoleWebController {
             }        
         }
 
-        AppDefinition appDef = appService.getAppDefinition(appId, version);
+        AppDefinition appDef = appDefinitionDao.loadVersion(appId, AppUtil.convertVersionToLong(version));
         if (appDef == null) {
             return result;
         }
