afbe36b2945d753688f68c5d1458d7e6a7fda0ee
chunzhe <chunzhe.chee@joget.com>
2025-03-05 18:41:57 +0800

    Fixed: wflow-plugin-archetype - src folder not generating due to misconfigured archetype-metadata.xml. @9.0-SNAPSHOT

--- wflow-plugin-archetype/create-plugin.sh
+++ wflow-plugin-archetype/create-plugin.sh
@@ -4,18 +4,10 @@ plugin=$2
 label=$3
 description=$4
 version=$5
-if [ -z $package ]; then 
+if [ -z "$package" ] || [ -z "$plugin" ] || [ -z "$label" ] || [ -z "$description" ] || [ -z "$version" ]; then
     echo "Usage: create-plugin.sh (package) (plugin name) (plugin label) (plugin description) (joget version)"
-elif [ -z $plugin ]; then 
-    echo "Usage: create-plugin.sh $package (plugin name) (plugin label) (plugin description) (joget version)"
-elif [ -z "$label" ]; then 
-    echo "Usage: create-plugin.sh $package $plugin (plugin label) (plugin description) (joget version)"
-elif [ -z "$description" ]; then 
-    echo "Usage: create-plugin.sh $package $plugin \"$label\" (plugin description) (joget version)"
-elif [ -z $version ]; then 
-    echo "Usage: create-plugin.sh $package $plugin \"$label\" \"$description\" (joget version)"
-else 
-    clear
-    mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=${version} -DgroupId=$package -DartifactId=$plugin -Dlabel="$label" -Ddescription="$description"
+    exit 1
+else
+    mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=${version} -DgroupId="$package" -DartifactId="$plugin" -Dlabel="$label" -Ddescription="$description"
+    exit $?
 fi
-exit 1
