6bae55737784e45d755d2bfde80fab77cd74ebc1
chunzhe <chunzhe.chee@joget.com>
2025-03-05 18:41:57 +0800

    Fixed: wflow-plugin-archetype - src folder not generating due to misconfigured archetype-metadata.xml. @9.0-SNAPSHOT

--- wflow-plugin-archetype/create-plugin.bat
+++ wflow-plugin-archetype/create-plugin.bat
@@ -7,12 +7,10 @@ if ""%4"" == """" goto usage
 if ""%5"" == """" goto usage
 
 :create
-mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=%5 -DgroupId=%1 -DartifactId=%2 -Dlabel="$3" -Ddescription="$4"
+mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=%5 -DgroupId=%1 -DartifactId=%2 -Dlabel=%3 -Ddescription=%4
 goto end
 
 :usage
 echo   Usage: create-plugin (package) (plugin name) (plugin label) (plugin description) (joget version)
 
 :end
-
-
