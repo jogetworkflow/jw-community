7e33d0b4bb5fb5f57ebb8540cfe53ec97a6a9981
owen <owen@joget.org>
2025-03-06 16:11:13 +0800

    Fixed: wflow-consoleweb - File Upload - Progress bar not working when upload field in form grid. T5583 @7.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/basesw.js
+++ wflow-consoleweb/src/main/webapp/basesw.js
@@ -38,8 +38,13 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    // ignore web console and presence URLs
-    if (event.request.url.indexOf('/web/console/') > 0 || event.request.url.indexOf('/web/json/console/') > 0 || event.request.url.indexOf('/web/presence') > 0) {
+    let isFileUpload = event.request.method === 'POST' 
+            && event.request.headers.get('X-Requested-With') 
+            && event.request.headers.get('X-Requested-With').indexOf("XMLHttpRequest") !== -1; //for file upload progress bar
+    
+    // ignore web console and presence URLs & file upload
+    if (event.request.url.indexOf('/web/console/') > 0 || event.request.url.indexOf('/web/json/console/') > 0 || event.request.url.indexOf('/web/presence') > 0
+            || isFileUpload) {
         return;
     }
     
