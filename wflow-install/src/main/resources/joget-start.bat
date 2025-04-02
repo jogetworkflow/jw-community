d4d6424de192d67a28f510dacd9b182421edf6e0
chunzhe <chunzhe.chee@joget.com>
2025-04-02 14:44:30 +0800

    Fixed: jw-community - Security - Update Apache Tomcat to 11.0.5 for CVE-2025-24813. T5913 @9.0-SNAPSHOT

--- wflow-install/src/main/resources/joget-start.bat
+++ wflow-install/src/main/resources/joget-start.bat
@@ -10,7 +10,7 @@ ECHO.
 
 REM Start Tomcat
 set JAVA_HOME=.\jre21.0.5
-set CATALINA_HOME=.\apache-tomcat-11.0.2
+set CATALINA_HOME=.\apache-tomcat-11.0.5
 set JAVA_OPTS=-Xmx768M -Dfile.encoding=UTF-8 --add-opens=java.base/java.nio=ALL-UNNAMED -Dwflow.home=./wflow/
 REM set JAVA_OPTS=-Xmx1024M -Dfile.encoding=UTF-8 --add-opens=java.base/java.nio=ALL-UNNAMED -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,suspend=n,server=y,address=5115 -Dwflow.home=./wflow/
 ECHO == Starting Tomcat from %CATALINA_HOME% ==
--- wflow-install/src/main/resources/joget-stop.bat
+++ wflow-install/src/main/resources/joget-stop.bat
@@ -8,7 +8,7 @@ ECHO.
 
 REM Stop Tomcat
 set JAVA_HOME=.\jre21.0.5
-set CATALINA_HOME=.\apache-tomcat-11.0.2
+set CATALINA_HOME=.\apache-tomcat-11.0.5
 ECHO == Stopping Tomcat from %CATALINA_HOME% ==
 ECHO.
 %CATALINA_HOME%\bin\shutdown.bat
