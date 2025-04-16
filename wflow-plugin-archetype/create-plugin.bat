1156bf1edbd2be6159b7fe42c421101cebacd1c3
chunzhe <chunzhe.chee@joget.com>
2025-04-16 12:40:31 +0800

    Fixed: wflow-plugin-archetype - create-plugin.bat - wrong syntax used in quoting variables. @9.0-SNAPSHOT

--- wflow-plugin-archetype/create-plugin.bat
+++ wflow-plugin-archetype/create-plugin.bat
@@ -7,7 +7,7 @@ if ""%4"" == """" goto usage
 if ""%5"" == """" goto usage
 
 :create
-mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=%5 -DgroupId=%1 -DartifactId=%2 -Dlabel="%3" -Ddescription="%4"
+mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=%5 -DgroupId=%1 -DartifactId=%2 -Dlabel=%3 -Ddescription=%4
 goto end
 
 :usage
