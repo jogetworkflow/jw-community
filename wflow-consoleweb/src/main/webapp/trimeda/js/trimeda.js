52f3257b82db7422c2b66a280f4710312e59d405
sean.christanto <sean.christanto@joget.com>
2025-04-28 10:57:09 +0800

    Fixed: wflow-consoleweb - Trimeda Theme - Fixed styles issues. @8.2-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/trimeda/js/trimeda.js
+++ wflow-consoleweb/src/main/webapp/trimeda/js/trimeda.js
@@ -85,7 +85,7 @@ $(document).ready(function() {
                 $("body").find("ul#category-container > li.category").each(function() {
                     if (!$(this).hasClass("first") && prevMenu !== null) {
                         if ($("body").hasClass("rtl")) {
-                            gap = ($(prevMenu).offset().left + $(prevMenu).outerWidth()) - $(this).offset().left;
+                            gap = $(prevMenu).offset().left - $(this).offset().left + $(this).outerWidth();
                         } else {
                             gap = $(this).offset().left - ($(prevMenu).offset().left + $(prevMenu).outerWidth());
                         }
