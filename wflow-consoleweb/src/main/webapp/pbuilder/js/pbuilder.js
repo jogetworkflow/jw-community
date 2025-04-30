1fbb96c01215fbbf6eaedfbfe35dc9a3714cfcc9
owen <owen@joget.org>
2025-04-30 10:44:48 +0800

    Fixed: wflow-consoleweb - Process Builder - Marketplace link not render correctly. @9.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
+++ wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
@@ -6969,7 +6969,7 @@ ProcessBuilder = {
             function(returnedData){
                 ProcessBuilder.availableTools = {};
                 for (e in returnedData) {
-                    if (returnedData[e].value !== "") {
+                    if (returnedData[e].value !== "" && returnedData[e].marketplace == undefined) {
                         ProcessBuilder.availableTools[returnedData[e].value] = returnedData[e];
                     }
                 }
