3f2e2f8c4c0e319a80c695d1fa5718a83afb8508
julian <julian@joget.org>
2025-05-06 22:02:07 +0800

    Fixed: wflow-shark - Event Streams - Initialization does not run after first configuration @9.0-SNAPSHOT

--- wflow-shark/src/main/java/org/joget/workflow/model/service/WorkflowManagerImpl.java
+++ wflow-shark/src/main/java/org/joget/workflow/model/service/WorkflowManagerImpl.java
@@ -244,6 +244,9 @@ public class WorkflowManagerImpl implements SharkWorkflowManager {
                     }
                 });
 
+                // init event stream manager
+                initEventStreamManager();
+                
                 initialized = true;
             } catch (Exception ex) {
                 LogUtil.error(getClass().getName(), ex, "");
@@ -3229,14 +3232,15 @@ public class WorkflowManagerImpl implements SharkWorkflowManager {
         
         synchronized(this) {
             if (eventStreamManager == null) {
-                eventStreamManager = EventStreamManager.getEventStreamManager();
-                LogUtil.info(getClass().getName(), "EventStreamManager discovered: " + eventStreamManager);
-                if (eventStreamManager.isEnabled()) {
+                EventStreamManager evStreamManager = EventStreamManager.getEventStreamManager();
+                if (evStreamManager != null && evStreamManager.isEnabled()) {
+                    LogUtil.info(getClass().getName(), "EventStreamManager discovered: " + evStreamManager);
+
                     // register event stream message listener for process
                     String eventStreamProcessTopic = EVENT_STREAM_TOPIC_PROCESS_START;
                     String eventStreamGroup = EVENT_STREAM_GROUP_WORKFLOW;
-                    eventStreamManager.createTopic(eventStreamProcessTopic);
-                    eventStreamManager.listen(eventStreamProcessTopic, eventStreamGroup, (String topic, String key, Map data) -> {
+                    evStreamManager.createTopic(eventStreamProcessTopic);
+                    evStreamManager.listen(eventStreamProcessTopic, eventStreamGroup, (String topic, String key, Map data) -> {
                         transactionTemplate.executeWithoutResult((TransactionStatus status) -> {
                             // start process
                             String processDefId = (String)data.get("processDefId");
@@ -3252,8 +3256,8 @@ public class WorkflowManagerImpl implements SharkWorkflowManager {
 
                     // register event stream message listener for assignment
                     String eventStreamAssignmentTopic = EVENT_STREAM_TOPIC_ASSIGNMENT_COMPLETE;
-                    eventStreamManager.createTopic(eventStreamAssignmentTopic);
-                    eventStreamManager.listen(eventStreamAssignmentTopic, eventStreamGroup, (String topic, String key, Map data) -> {
+                    evStreamManager.createTopic(eventStreamAssignmentTopic);
+                    evStreamManager.listen(eventStreamAssignmentTopic, eventStreamGroup, (String topic, String key, Map data) -> {
                         transactionTemplate.executeWithoutResult((TransactionStatus status) -> {
                             // complete assignment
                             String activityId = (String)data.get("activityId");
@@ -3261,7 +3265,9 @@ public class WorkflowManagerImpl implements SharkWorkflowManager {
                         });
                     });
                     LogUtil.info(getClass().getName(), "Registered event stream listener for " + EVENT_STREAM_TOPIC_ASSIGNMENT_COMPLETE);
-
+                    eventStreamManager = evStreamManager;
+                } else {
+                    eventStreamManager = null;
                 }
             }
         }
