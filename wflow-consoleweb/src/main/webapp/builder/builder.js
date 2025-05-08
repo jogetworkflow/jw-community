7afaa54c12f1e247c4a242affea86c6d8f385566
owen <owen@joget.org>
2025-05-08 15:29:39 +0800

    Fixed: wflow-core - Builder - Error toast messages are hard to read due to auto hide. #184 T4187 @9.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/builder/builder.js
+++ wflow-consoleweb/src/main/webapp/builder/builder.js
@@ -310,6 +310,11 @@ _CustomBuilder = {
             $("#builder_loader i.fa-stack-1x").attr("class", data.builderIcon + " fa-stack-1x");
             $("#save-btn").removeClass("unsaved");
             
+            //remove all toast messages that not auto hided.
+            $('.toast').each(function(){
+                $(this).toast("hide");
+            });
+            
             if (CustomBuilder.builderType === data.builderType && CustomBuilder.systemTheme === data.systemTheme && CustomBuilder.builderType !== "app") {
                 CustomBuilder.id = data.id;
                 CustomBuilder.appId = data.appId;
@@ -1410,7 +1415,14 @@ _CustomBuilder = {
                 type = "secondary";
                 delay = 1500;
             } else if (type === "danger") {
-                delay = 10000;
+                delay = 0;
+                
+                //show max 5 toasts only, hide the oldest
+                if ($("#builder-message").find(".toast").length > 4) {
+                    $("#builder-message").find(".toast").slice(4).each(function(){
+                        $(this).toast("hide");
+                    });
+                }
             }
             var toast = $('<div id="'+id+'" role="alert" aria-live="assertive" aria-atomic="true" class="toast alert-dismissible toast-'+type+'" data-autohide="true">\
                 '+message+'\
@@ -1418,11 +1430,17 @@ _CustomBuilder = {
               </div>');
             
             $("#builder-message").removeClass('center');
-            $("#builder-message").append(toast);
+            $("#builder-message").prepend(toast); //add to the top
             if (center) {
                 $("#builder-message").addClass('center');
             }
-            $('#'+id).toast({delay : delay});
+            var option = {};
+            if (delay > 0) {
+                option.delay = delay;
+            } else {
+                option.autohide = false
+            }
+            $('#'+id).toast(option);
             $('#'+id).toast("show");
             $('#'+id).on('hidden.bs.toast', function () {
                 $('#'+id).remove();
@@ -3043,6 +3061,7 @@ _CustomBuilder = {
                 width = parseInt($("#right-panel").css("min-width").replace("px", ""));
             } catch (e){}
         }
+        $("body").css("--builder-right-panel-width", width + 'px');
         
         if (width > 680) {
             $("#right-panel, #right-panel .property-editor-container").addClass("wider");
