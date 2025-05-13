ea0ad236e0a18b618ac28907d87dad7b3989a5a5
shimmarasan-joget <shimmarasan.kumar@joget.com>
2025-05-14 01:49:42 +0800

    Fixed: wflow-consoleweb - Process Builder - not able to paste Start, End, or Route elements after copying. #939 @9.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
+++ wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
@@ -8257,6 +8257,7 @@ ProcessBuilder = {
         let gap = 150;
         ProcessBuilder.updatePasteElement = false;
         if (copiedObj.type !== 'lane') {
+            if (!copiedObj.text) copiedObj.text = {}; // Ensure "text" property exists. If not, initialize
             if (element.properties.className === 'participant') {
                 xPosition = parseInt(copiedObj.x);
                 copiedObj.x = xPosition + gap;
