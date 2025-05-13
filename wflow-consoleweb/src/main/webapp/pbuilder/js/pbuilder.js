eb52b5e395cb9c7d7844ea61ccecb294beef55d9
shimmarasan-joget <shimmarasan.kumar@joget.com>
2025-05-14 02:13:59 +0800

    Fixed: wflow-consoleweb - Process Builder - Auto Layout, Highlight Mode, and Navigator buttons (specific to Process) appearing in other builders. #939 @9.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
+++ wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
@@ -7681,6 +7681,7 @@ ProcessBuilder = {
      */            
     unloadBuilder : function() {
         $("#process-selector, .toolzoom-buttons, #listviewer-btn, #xpdl-btn").remove();
+        $("#auto-layout, #hightlight, #navigator").parent(".toolbar-group").remove();
         $("#launch-btn").parent().remove();
         $(window).off('hashchange');        
     },
