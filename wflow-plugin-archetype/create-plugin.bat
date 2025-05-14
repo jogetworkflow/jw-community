64082e577958838722d31ec9e75c31ad6f3283c1
chunzhe <chunzhe.chee@joget.com>
2025-05-14 10:26:26 +0800

    Modified: wflow-plugin-archetype - Added plugin name and description to new plugins. #877 @8.2-SNAPSHOT

--- wflow-plugin-archetype/create-plugin.bat
+++ wflow-plugin-archetype/create-plugin.bat
@@ -3,14 +3,14 @@
 if ""%1"" == """" goto usage
 if ""%2"" == """" goto usage
 if ""%3"" == """" goto usage
+if ""%4"" == """" goto usage
+if ""%5"" == """" goto usage
 
 :create
-mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=%3 -DgroupId=%1 -DartifactId=%2
+mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=%5 -DgroupId=%1 -DartifactId=%2 -Dlabel=%3 -Ddescription=%4
 goto end
 
 :usage
-echo   Usage: create-plugin (package) (pluginName) (jogetVersion)
+echo   Usage: create-plugin (package) (plugin name) (plugin label) (plugin description) (joget version)
 
 :end
-
-
--- wflow-plugin-archetype/create-plugin.sh
+++ wflow-plugin-archetype/create-plugin.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 package=$1
 plugin=$2
-version=$3
-if [ -z $package ]; then 
-    echo "Usage: create-plugin.sh (package) (plugin name) (joget version)"
-elif [ -z $plugin ]; then 
-    echo "Usage: create-plugin.sh $package (plugin name) (joget version)"
-elif [ -z $version ]; then 
-    echo "Usage: create-plugin.sh $package $plugin (joget version)"
-else 
-    clear
-    mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=${version} -DgroupId=$package -DartifactId=$plugin
+label=$3
+description=$4
+version=$5
+if [ -z "$package" ] || [ -z "$plugin" ] || [ -z "$label" ] || [ -z "$description" ] || [ -z "$version" ]; then
+    echo "Usage: create-plugin.sh (package) (plugin name) (plugin label) (plugin description) (joget version)"
+    exit 1
+else
+    mvn archetype:generate -DarchetypeGroupId=org.joget -DarchetypeArtifactId=wflow-plugin-archetype -DarchetypeVersion=${version} -DgroupId="$package" -DartifactId="$plugin" -Dlabel="$label" -Ddescription="$description"
+    exit $?
 fi
-exit 1
