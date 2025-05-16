3fce71ca7e2f79e007adece43475b31806d4f33c
shimmarasan-joget <shimmarasan.kumar@joget.com>
2025-05-16 12:07:53 +0800

    Fixed: wflow-consoleweb - Process Builder - List View not showing after clicking the button. #977 @9.0-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
+++ wflow-consoleweb/src/main/webapp/pbuilder/js/pbuilder.js
@@ -7274,7 +7274,7 @@ ProcessBuilder = {
         
         var id = obj.properties.id;
         if (self.selectedEl) {
-            var selectedData = self.selectedEl[0].data;
+            var selectedData = $(self.selectedEl).data("data");
             if (selectedData.properties.id === id) {
                 $(detailsDiv).find(".cbuilder-node-details-list").addClass("active");
                 var listId = $(list).attr("id");
