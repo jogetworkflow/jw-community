baf623e6d6477f7ab2137f5aa32a6dfca11d0012
allen.zhang <allen@valuprosys.com>
2025-05-26 15:54:15 +0800

    Fixed: NPE for WebSocketPluginConfigurator. httpSession

--- wflow-consoleweb/src/main/java/org/joget/apps/app/web/WebSocketPluginConfigurator.java
+++ wflow-consoleweb/src/main/java/org/joget/apps/app/web/WebSocketPluginConfigurator.java
@@ -15,7 +15,10 @@ public class WebSocketPluginConfigurator extends ServerEndpointConfig.Configurat
         // Get the HttpSession from the HandshakeRequest
         HttpSession httpSession = (HttpSession) request.getHttpSession();
 
-        // Store the HttpSession as a user property in the WebSocket session
-        config.getUserProperties().put(HttpSession.class.getName(), httpSession);
+        if (httpSession != null) {
+            // Store the HttpSession as a user property in the WebSocket session
+            config.getUserProperties().put(HttpSession.class.getName(), httpSession);
+        }
+
     }
 }
