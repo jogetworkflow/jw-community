155ac3c12ca7b90ce84ea803340617ced6fb2b3e
owen <owen@joget.org>
2025-07-09 09:22:32 +0800

    Revert "Fixed: wflow-wfengine - Workflow - Failed to recover stuck route & subflow during startup. #1033 @8.2-SNAPSHOT"
    This reverts commit 1fd2e03d1db73ea9d815d3e5606f331d0642b756.

--- wflow-wfengine/src/main/java/org/joget/workflow/shark/model/dao/WorkflowAssignmentDao.java
+++ wflow-wfengine/src/main/java/org/joget/workflow/shark/model/dao/WorkflowAssignmentDao.java
@@ -812,16 +812,15 @@ public class WorkflowAssignmentDao extends AbstractSpringDao {
     }
     
     /**
-     * Retrieve stuck subflow, tools & route which having "open.*" status and does no having assignment during startup.
-     * Stuck subflow does not having performer
+     * Only stuck tools having "open.running" status during startup
      * @return 
      */
     public Collection<Object[]> getStuckTools() {
         Session session = findSession();
-        String query = "SELECT e.processDefId, e.processId, e.activityId FROM SharkActivity e LEFT JOIN SharkAssignment a ON a.activity.activityId = e.activityId WHERE e.state.name like ?1 AND e.performer IS NULL AND a.id IS NULL";
+        String query = "SELECT e.processDefId, e.processId, e.activityId FROM SharkActivity e WHERE e.state.name = ?1";
         Query q = session.createQuery(query);
 
-        q.setParameter(1, "open.%");
+        q.setParameter(1, "open.running");
         return q.list();
     }
     
