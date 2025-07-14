ea0e65f9145f2e1b3920eaf84310d77244db12ba
anis.yusra <anis.yusra@joget.com>
2025-07-14 17:26:41 +0800

    Fixed : wflow-core - UI/Log Viewer - Fixed hardcoded references to "jw" path #1123. @8.2-SNAPSHOT

--- wflow-core/src/main/resources/resources/themes/universal/sw.js
+++ wflow-core/src/main/resources/resources/themes/universal/sw.js
@@ -133,7 +133,7 @@ self.addEventListener('install', function (event) {
     event.waitUntil(
         currentPageUrlPromise.then((url) => {
             self.skipWaiting();
-            if (url !== "/jw/web/login") {
+            if (url !== contextPath + "/web/login") {
                 caches.delete(appCacheName)
                     .then(function(){
                         caches.open(appCacheName)
