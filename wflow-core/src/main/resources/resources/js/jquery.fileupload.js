aa41c678d56b146d1a633574ed7ba7e30b92f670
Aditya <aditya.sood@joget.com>
2025-07-22 23:04:05 +0530

    Fixed: wflow-core - File Upload - Delete actual file during overwrite or removal #859 @8.2-SNAPSHOT

--- wflow-core/src/main/resources/resources/js/jquery.fileupload.js
+++ wflow-core/src/main/resources/resources/js/jquery.fileupload.js
@@ -155,7 +155,7 @@
                             // Go through the list of all <ul> elements created
                             $(target).find(".form-fileupload-value").find("li").each(function() {
                                 // Check each <ul> element's input value with current file name
-                                if (!$(this).is($(file.previewElement)) && ($(this).find("input[name$='_path']").val().split("\\").pop() === resp.filename || $(this).find("input[name$='_path']").val().split("\\").pop() === resp.newFilename)) {
+                                if (!$(this).is($(file.previewElement)) && ($(this).find("input[name$='_path']").val().split(/[\\\/]/).pop() === resp.filename || $(this).find("input[name$='_path']").val().split(/[\\\/]/).pop() === resp.newFilename)) {
                                     // If the same, remove the previous instance
                                     $(this).remove();
                                 }
