5670ec745838e5411cff2f95becb66f002932a3b
chunzhe <chunzhe.chee@joget.com>
2025-07-25 15:50:22 +0800

    Fixed: wflow-core - FormDataAuditTrail - Implement Serializable interface. #1200 @9.0-SNAPSHOT

--- wflow-core/src/main/java/org/joget/apps/form/model/FormDataAuditTrail.java
+++ wflow-core/src/main/java/org/joget/apps/form/model/FormDataAuditTrail.java
@@ -1,8 +1,9 @@
 package org.joget.apps.form.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class FormDataAuditTrail {
+public class FormDataAuditTrail implements Serializable {
     private String id;
     private String appId;
     private String appVersion;
