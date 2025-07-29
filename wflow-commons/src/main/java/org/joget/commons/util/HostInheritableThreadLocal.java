82784af5730478ca1da28becee2d4d0fd73bbcab
owen <owen@joget.org>
2025-07-29 11:23:14 +0800

    Fixed: wflow-commons - Security - Improve HostManager security check. #1207 @8.2-SNAPSHOT

---
+++ wflow-commons/src/main/java/org/joget/commons/util/HostInheritableThreadLocal.java
@@ -0,0 +1,24 @@
+package org.joget.commons.util;
+
+public final class HostInheritableThreadLocal<T> extends InheritableThreadLocal<T> {
+    
+    @Override
+    public void set(T value) {
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkPermission(new HostPermission("HostInheritableThreadLocal"));
+        }
+        
+        super.set(value);
+    }
+    
+    @Override
+    public void remove() {
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkPermission(new HostPermission("HostInheritableThreadLocal"));
+        }
+        
+        super.remove();
+    }
+}
