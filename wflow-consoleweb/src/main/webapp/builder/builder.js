f509bfb44d8ff951d5a52fe9d8e1c8a8b9a89a61
SeanChristanto <sean.christanto@joget.com>
2026-07-09 15:27:55 +0700

    Fixed: wflow-consoleweb - Builder - Fixed RangeError after a successful save. T7119 #1598 @9.1-SNAPSHOT

--- wflow-consoleweb/src/main/webapp/builder/builder.js
+++ wflow-consoleweb/src/main/webapp/builder/builder.js
@@ -1608,12 +1608,12 @@ window._CustomBuilder = {
                     data: params,
                     cache: false,
                     processData: false,
+                    dataType: 'json',
                     contentType: false,
                     beforeSend: function (request) {
                        request.setRequestHeader(ConnectionManager.tokenName, ConnectionManager.tokenValue);
                     },
-                    success:function(data) {
-                        var d = JSON.decode(data);
+                    success:function(d) {
                         if(d.success == true){
                             $("#save-btn").removeClass("unsaved");
                             $("#save-btn-toolbar").removeClass("unsaved");
